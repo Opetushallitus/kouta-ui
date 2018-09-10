9660541bd51b7759770bcb79af3e82ad0fa7b8a8
Joona Haavisto <joona.haavisto@gofore.com>
2018-09-10 17:09:57 +0300

    React app runko

---
+++ src/main/app/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
+
+ReactDOM.render((
+    <BrowserRouter basename={'/kouta'}>
+
+        <h1>Koulutustarjonta</h1>
+
+    </BrowserRouter>
+), document.getElementById('root'));
+

55a709ea908fb932c5f74e3ef3190784af6cc7d0
Heikki Kupiainen <heikki.kupiainen@metamatic.net>
2018-09-26 15:52:46 +0300

    koulutuksen julkaiseminen: created status bar component to show status, user and edit history

--- src/main/app/src/views/koulutus-publication/KoulutusPublicationView.js
+++ src/main/app/src/views/koulutus-publication/KoulutusPublicationView.js
@@ -1,7 +1,18 @@
 import React, {Component} from 'react';
+import {StatusBar} from './StatusBar';
+import {MultiStepBar} from './MultiStepBar';
+import {Accordion} from './Accordion';
+import {FooterActionButtons} from './FooterActionButtons';
 
 export class KoulutusPublicationView extends Component {
 
-  render = () => <h1>Julkaise uusi koulutus (placeholder)</h1>
+  render = () => (
+      <div className={"koulutus-publication-view"}>
+        <StatusBar/>
+        <MultiStepBar/>
+        <Accordion/>
+        <FooterActionButtons/>
+      </div>
+  )
 
 }
--- src/main/app/src/views/koulutus-publication/StatusBar.js
+++ src/main/app/src/views/koulutus-publication/StatusBar.js
@@ -1,5 +1,27 @@
 import React, {Component} from 'react';
 
 export class StatusBar extends Component {
-  render = () => <h1>Status Bar</h1>
+
+  render = () => (
+    <div className={"status-bar"}>
+      <h1 className={"header"}>Julkaise koulutus</h1>
+      <div className={"status-box progress-box"}>
+        <div className={"icon"}>
+          <i class="far fa-star"/>
+        </div>
+        <div className={"info"}>
+          <div className={"status-text"}>Tila: Kesken</div>
+          <div className={"link-text"}>Vaihda tila</div>
+        </div>
+      </div>
+      <div className={"status-box history-box"}>
+        <div className={"icon fas fa-info"}/>
+        <div className={"info"}>
+          <div className={"status-text"}>Muokattu viimeksi:</div>
+          <div className={"status-text"}>10.09.2018 9:15 Matti Meikäläinen</div>
+          <span className={"link-text"}>Näytä versiohistoria</span>
+        </div>
+      </div>
+    </div>
+  )
 }

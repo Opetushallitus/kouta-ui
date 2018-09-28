69a5888f3ee4ee70565ded0ade4a0c7d00051131
Joona Haavisto <joona.haavisto@gofore.com>
2018-09-28 15:35:10 +0300

    Korjattu configuraatiota

--- src/main/java/fi/oph/kouta/UrlConfiguration.java
+++ src/main/java/fi/oph/kouta/UrlConfiguration.java
@@ -11,7 +11,7 @@ public class UrlConfiguration extends OphProperties {
     @Autowired
     public UrlConfiguration(Environment environment) {
         addFiles("/kouta-ui-oph.properties");
-        this.addOverride("host-tarjonta", environment.getRequiredProperty("host.host-tarjonta"));
+        this.addOverride("host-virkailija", environment.getRequiredProperty("host.host-virkailija"));
         this.frontProperties.setProperty("kouta-backend.base-url", this.require("kouta-backend.base-url"));
         this.frontProperties.setProperty("kouta-backend.koulutus", this.require("kouta-backend.koulutus"));
         this.frontProperties.setProperty("kouta-backend.toteutus", this.require("kouta-backend.toteutus"));

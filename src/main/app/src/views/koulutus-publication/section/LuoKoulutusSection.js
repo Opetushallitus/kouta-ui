6d00193ba14ca4c3ef48ad7251ad1a37b16af1c9
Heikki Kupiainen <heikki.kupiainen@metamatic.net>
2018-09-28 13:37:12 +0300

    updated 'luo koulutus' section to use dedicated components

--- src/main/app/src/views/koulutus-publication/section/LuoKoulutusSection.js
+++ src/main/app/src/views/koulutus-publication/section/LuoKoulutusSection.js
@@ -1,6 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {AbstractSection} from './AbstractSection';
+import {SelectorButton} from '../../../components/SelectorButton';
+import {FilterList} from '../../../components/FilterList';
+
 
 export class LuoKoulutusSection extends AbstractSection {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   getHeader = () => "1 Luo koulutus";
+
+  getSelectorButtonOptions = () => [
+    {
+      text: 'Luo uusi koulutus',
+      action: () => this.createNewKoulutus()
+    },
+    {
+      text: 'Käytä aikaisemmin luodun koulutuksen tietoja'
+    }
+  ];
+
+  /*
+  <SelectorButton label={"Luo uusi"} text={"LUO UUSI"} instruction={"Lorem ipsum lorem ipsum"} options={this.getSelectorButtonOptions()}/>
+          <FilterList options={this.state.koodisto}/>
+   */
+  renderContent = () => {
+    return (
+        <div className={"content"}>
+         <SelectorButton/>
+         <FilterList/>
+        </div>
+    )
+  }
 }

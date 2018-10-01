d0169b79e628c6fd7ddff55ea9c6ab608106dc07
Heikki Kupiainen <heikki.kupiainen@metamatic.net>
2018-10-01 18:27:48 +0300

    implemented koulutus selector filter list component for finding koulutus from a huge list

--- src/main/app/src/components/FilterList.js
+++
@@ -1,8 +0,0 @@
-import React, {Component} from 'react';
-import {getCssClassName} from '../utils/utils';
-
-export class FilterList extends Component {
-  render = () => (
-      <div className={getCssClassName(this)}/>
-  )
-}

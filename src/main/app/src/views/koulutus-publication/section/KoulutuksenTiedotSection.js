963d94ac8fa34d7ca49af9ca234ae6676c56a92c
Heikki Kupiainen <heikki.kupiainen@metamatic.net>
2018-10-01 16:56:36 +0300

    added default constructor to koulutuksen tiedot section

--- src/main/app/src/views/koulutus-publication/section/KoulutuksenTiedotSection.js
+++ src/main/app/src/views/koulutus-publication/section/KoulutuksenTiedotSection.js
@@ -3,6 +3,11 @@ import {AbstractSection} from '../../../components/AbstractSection';
 
 export class KoulutuksenTiedotSection extends AbstractSection {
 
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   getHeader = () => "3 Koulutuksen tiedot";
 
 }

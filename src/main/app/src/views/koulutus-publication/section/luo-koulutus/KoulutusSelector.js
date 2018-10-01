d57b48b4c410437df2224615686da2736fd0ed04
Heikki Kupiainen <heikki.kupiainen@metamatic.net>
2018-10-01 16:59:43 +0300

    removed references to Metamatic framework from Koulutus Selector

--- src/main/app/src/views/koulutus-publication/section/luo-koulutus/KoulutusSelector.js
+++ src/main/app/src/views/koulutus-publication/section/luo-koulutus/KoulutusSelector.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import {connect, disconnect} from 'metamatic'
-import {STATE_KOULUTUS_SELECTOR_VISIBLE} from '../../../../states/AppState';
 
 export class KoulutusSelector extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {};
-    connect(this, STATE_KOULUTUS_SELECTOR_VISIBLE, (visible) => this.setState({visible}));
   }
-
-  componentWillUnmount = () => disconnect(this);
 
   render = () => this.state.visible ? (
       <h1>KoulutusSelector</h1>

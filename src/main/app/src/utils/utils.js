b02b696393b46684194f76e45a77de74b45995e4
Heikki Kupiainen <heikki.kupiainen@metamatic.net>
2018-10-05 16:03:04 +0300

    added basic utility functions to ease use of JavaScript standard callback feature to store & dispatch container states since MobX does not appear to work with component inheritance & MobX does not appear to detect changes within nested states

--- src/main/app/src/utils/utils.js
+++ src/main/app/src/utils/utils.js
@@ -1,3 +1,47 @@
 const snake = require('to-snake-case');
 
 export const getCssClassName = (component) => snake(component.constructor.name).replace(/_/g, '-');
+
+
+const connectionMap = {};
+const dataStore = {};
+let listenerId = 0;
+
+export const observe = (eventName, item) => dataStore[eventName] = clone(item);
+
+export const updateState = (eventName, item) => {
+  dataStore[eventName] = Object.assign(dataStore[eventName], item);
+  broadcast(eventName, dataStore[eventName]);
+}
+
+// a simple function to couple a listener with matching event
+export const connect = (eventName, listener, targetFunction) => {
+  if (!listener._listenerId) {
+    listenerId += 1;
+    listener._listenerId = listenerId;
+  }
+
+  if (!connectionMap[eventName]) {
+    connectionMap[eventName] = {};
+  }
+  const stringId = listenerId.toString();
+  connectionMap[eventName][stringId] = targetFunction;
+  const dataItem = dataStore[eventName];
+  if (dataItem) {
+    broadcast(eventName, clone(dataItem));
+  }
+};
+
+const clone = (item) => {
+  try {
+    return JSON.parse(JSON.stringify(item));
+  } catch(e) {
+    return item;
+  }
+}
+
+//A simple callback mechanism to achieve what MobX can't do: 1) pass change when value is changed inside a nested object, and work 2) with inheritance
+export const broadcast = (eventName, item) => Object.values(connectionMap[eventName]).forEach((listener) => listener(clone(item)));
+
+
+

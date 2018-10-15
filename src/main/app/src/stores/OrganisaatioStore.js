864c2bb664c10a3effdee1383c223c9aadd6888f
Heikki Kupiainen <heikki.kupiainen@metamatic.net>
2018-10-15 15:45:37 +0300

    made organisaatio list to be loaded only once

--- src/main/app/src/stores/OrganisaatioStore.js
+++ src/main/app/src/stores/OrganisaatioStore.js
@@ -1,16 +1,17 @@
 import axios from 'axios';
 import {urlOrganisaatioList} from '../config/urls';
 import {APP_STATE_KOULUTUS_DETAILS, APP_STATE_ORGANISAATIO} from '../config/states';
-import {connect, updateState} from '../utils/utils';
+import {connect, containsState, updateState} from '../utils/utils';
 import {extractOrganisaatioOptions} from '../model/Organisaatio';
 
-export const OrganisaatioStore = () => {
-  connect(APP_STATE_KOULUTUS_DETAILS, {}, () => loadOrganisaatioList());
-}
+export const OrganisaatioStore = () => connect(APP_STATE_KOULUTUS_DETAILS, {}, () => loadOrganisaatioList());
+
+const excludesOrganisaatioList = () => !containsState(APP_STATE_ORGANISAATIO, 'options');
 
 export const selectOrganisaatio = (activeOrganisaatioId) => updateState(APP_STATE_ORGANISAATIO, {activeOrganisaatioId});
 
-const loadOrganisaatioList = () => axios.get(urlOrganisaatioList()).then((response) => setOrganisaatioOptionsData(response.data));
+const loadOrganisaatioList = () => excludesOrganisaatioList() ?
+    axios.get(urlOrganisaatioList()).then((response) => setOrganisaatioOptionsData(response.data)) : null;
 
 const setOrganisaatioOptionsData = (jsonData) => updateState(APP_STATE_ORGANISAATIO, {
     options: extractOrganisaatioOptions(jsonData)

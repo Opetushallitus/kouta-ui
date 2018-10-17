2bc60f1c4e032fd82eea02d7f2803e607feedcea
Heikki Kupiainen <heikki.kupiainen@metamatic.net>
2018-10-17 17:09:34 +0300

    moved footer action buttons to right class

--- src/main/app/src/views/koulutus-publication/FooterActionButtons.js
+++ src/main/app/src/views/koulutus-publication/FooterActionButtons.js
@@ -1,5 +1,56 @@
 import React, {Component} from 'react';
+import {ATTR_SAVE, ATTR_SAVE_AND_PUBLISH, saveAndPublishKoulutus, saveKoulutus} from '../../stores/KoulutusPersistencyStore';
+import {APP_STATE_KOULUTUS_PERSISTENCY} from '../../config/states';
+import {REQUEST_STATUS} from '../../config/constants';
+import {connectToOne} from '../../utils/stateUtils';
+
+const classNames = require('classnames');
 
 export class FooterActionButtons extends Component {
-  render = () => <h1>Footer action Buttons</h1>
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount = () => connectToOne(APP_STATE_KOULUTUS_PERSISTENCY, {}, (state) => this.setState(state));
+
+  saveKoulutus = () => saveKoulutus();
+
+  saveAndPublishKoulutus = () => saveAndPublishKoulutus();
+
+  isSaveButtonEnabled = () => true;
+
+  isPublishButtonEnabled = () => this.state[ATTR_SAVE_AND_PUBLISH] === REQUEST_STATUS.ENABLED;
+
+  getSaveButtonCssClass = () => this.state[ATTR_SAVE];
+
+  getPublishButtonCssClass = () => this.state[ATTR_SAVE_AND_PUBLISH];
+
+  getSaveButtonClickHandler = () => this.isSaveButtonEnabled() ? this.saveKoulutus : null;
+
+  getPublishButtonClickHandler = () => this.isPublishButtonEnabled() ? this.saveAndPublishKoulutus : null;
+
+  getSaveButtonLabel = () => 'Tallenna';
+
+  getPublishButtonLabel = () => 'Tallenna ja julkaise';
+
+  renderSaveButton = () => (
+      <button className={classNames("primary", "big", this.getSaveButtonCssClass())}
+              onClick={this.getSaveButtonClickHandler()}>{this.getSaveButtonLabel()}</button>
+  )
+
+  renderPublishButton = () => (
+      <button className={classNames("primary", "big", this.getPublishButtonCssClass())}
+              onClick={this.getPublishButtonClickHandler()}>{this.getPublishButtonLabel()}</button>
+  );
+
+  render = () => (
+      <div className={"button-container button-container-right"}>
+        <button className={"secondary big"}>Seuraava</button>
+        {this.renderSaveButton()}
+        {this.renderPublishButton()}
+      </div>
+  )
+
 }
--- src/main/app/src/views/koulutus-publication/KoulutusPublicationView.js
+++ src/main/app/src/views/koulutus-publication/KoulutusPublicationView.js
@@ -6,51 +6,9 @@ import {KoulutustyyppiSection} from './section/KoulutustyyppiSection';
 import {KoulutuksenTiedotSection} from './section/KoulutuksenTiedotSection';
 import {KoulutuksenKuvausSection} from './section/KoulutuksenKuvausSection';
 import {ValitseOrganisaatioSection} from './section/ValitseOrganisaatioSection';
-import {ATTR_SAVE, ATTR_SAVE_AND_PUBLISH, saveAndPublishKoulutus, saveKoulutus} from '../../stores/KoulutusPersistencyStore';
-import {APP_STATE_KOULUTUS_PERSISTENCY} from '../../config/states';
-import {REQUEST_STATUS} from '../../config/constants';
-import {connectToOne} from '../../utils/stateUtils';
-
-const classNames = require('classnames');
+import {FooterActionButtons} from './FooterActionButtons';
 
 export class KoulutusPublicationView extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  componentDidMount = () => connectToOne(APP_STATE_KOULUTUS_PERSISTENCY, {}, (state) => this.setState(state));
-
-  saveKoulutus = () => saveKoulutus();
-
-  saveAndPublishKoulutus = () => saveAndPublishKoulutus();
-
-  isSaveButtonEnabled = () => true;
-
-  isPublishButtonEnabled = () => this.state[ATTR_SAVE_AND_PUBLISH] === REQUEST_STATUS.ENABLED;
-
-  getSaveButtonCssClass = () => this.state[ATTR_SAVE];
-
-  getPublishButtonCssClass = () => this.state[ATTR_SAVE_AND_PUBLISH];
-
-  getSaveButtonClickHandler = () => this.isSaveButtonEnabled() ? this.saveKoulutus : null;
-
-  getPublishButtonClickHandler = () => this.isPublishButtonEnabled() ? this.saveAndPublishKoulutus : null;
-
-  getSaveButtonLabel = () => 'Tallenna';
-
-  getPublishButtonLabel = () => 'Tallenna ja julkaise';
-
-  renderSaveButton = () => (
-      <button className={classNames("primary", "big", this.getSaveButtonCssClass())}
-              onClick={this.getSaveButtonClickHandler()}>{this.getSaveButtonLabel()}</button>
-  )
-
-  renderPublishButton = () => (
-      <button className={classNames("primary", "big", this.getPublishButtonCssClass())}
-              onClick={this.getPublishButtonClickHandler()}>{this.getPublishButtonLabel()}</button>
-  );
 
   render = () => (
       <div className={"koulutus-publication-view"}>
@@ -65,13 +23,8 @@ export class KoulutusPublicationView extends Component {
           <KoulutuksenTiedotSection/>
           <KoulutuksenKuvausSection/>
           <ValitseOrganisaatioSection/>
-          <div className={"button-container button-container-right"}>
-            <button className={"secondary big"}>Seuraava</button>
-            {this.renderSaveButton()}
-            {this.renderPublishButton()}
-          </div>
+          <FooterActionButtons/>
         </div>
       </div>
   )
-
 }

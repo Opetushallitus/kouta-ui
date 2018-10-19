da116e27268fe89838a019ef9770023d6a0947e0
Heikki Kupiainen <heikki.kupiainen@metamatic.net>
2018-10-19 17:13:54 +0300

    updeitattu modelit vastaamaan muuttunutta datan hakua

--- src/main/app/src/model/Organisaatio.js
+++ src/main/app/src/model/Organisaatio.js
@@ -1,17 +1,5 @@
 import {LANGUAGE} from '../config/constants';
 
-/*
-const jsonDataExample = {
-  "organisaatiot": [
-    {
-      "oid": "1.2.246.562.10.16873649764",
-      "nimi": {
-        "fi": "Kuulammen koulu"
-      }
-    }]
-};
-*/
-
 export const extractOrganisaatioOptions = (jsonData) => jsonData.organisaatiot.map((entry) => ({
   label: entry.nimi[LANGUAGE.toLowerCase()],
   value: entry.oid

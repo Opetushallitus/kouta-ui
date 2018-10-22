7cf8065906c4a6828d0fa70c6f8230f053dbc77f
Heikki Kupiainen <heikki.kupiainen@metamatic.net>
2018-10-22 14:24:28 +0300

    yksinkertaistin kieliversion valintakomponentin toteutusta hyödyntämällä abstraktio checkbox-sektio-ylitemplaattia

--- src/main/app/src/views/koulutus-publication/section/KieliversioSection.js
+++ src/main/app/src/views/koulutus-publication/section/KieliversioSection.js
@@ -1,59 +1,26 @@
 import React from 'react';
-import {AbstractSection} from '../../../components/AbstractSection';
 import {
   APP_EVENT_KIELIVERSIO_SELECTION_CHANGE,
   APP_EVENT_KIELIVERSIO_SELECTION_CLEAR,
   APP_STATE_KIELIVERSIO_OPTIONS,
   APP_STATE_KIELIVERSIO_SELECTIONS
 } from '../../../config/states';
-import {broadcast, connectToOne} from '../../../utils/stateUtils';
+import {AbstractCheckboxSection} from '../../../components/AbstractCheckboxSection';
 
-export class KieliversioSection extends AbstractSection {
+export class KieliversioSection extends AbstractCheckboxSection {
+
+  getOptionsStateName = () => APP_STATE_KIELIVERSIO_OPTIONS;
+
+  getSelectionsStateName = () => APP_STATE_KIELIVERSIO_SELECTIONS;
+
+  getSelectionChangeEventName = () => APP_EVENT_KIELIVERSIO_SELECTION_CHANGE;
+
+  getSelectionClearEventName = () => APP_EVENT_KIELIVERSIO_SELECTION_CLEAR;
 
   getClassName = () => 'KieliversioSection';
 
   getHeader = () => 'Kieliversiot';
 
   getInstruction = () => 'Valitse ne kielet, joilla tiedot julkaistaan Opintopolussa';
-
-  componentDidMount = () => {
-    this.connectToSectionStateMap();
-    connectToOne(APP_STATE_KIELIVERSIO_OPTIONS, this, (state) => {
-      this.setState({...this.state, options: state});
-    });
-    connectToOne(APP_STATE_KIELIVERSIO_SELECTIONS, this, (state) => {
-      this.setState({...this.state, selections: state});
-    });
-  }
-
-  isOptionChecked = (option) => this.state.selections[option.value] === true;
-
-  handleCheckboxChange = (event) => {
-    const value = event.target.value;
-    const selected = event.target.checked;
-    broadcast(APP_EVENT_KIELIVERSIO_SELECTION_CHANGE, {value, selected});
-  }
-
-  onClearButtonClick = () => broadcast(APP_EVENT_KIELIVERSIO_SELECTION_CLEAR);
-
-  renderOption = (option, index) => (
-      <li key={index}>
-        <input type="checkbox" name={this.getClassName()} value={option.value} checked={this.isOptionChecked(option)}
-               onChange={this.handleCheckboxChange}/>{option.label}
-      </li>
-  );
-
-  renderOptionList = () => this.state.options ? (
-      <ul className={this.getClassName() + "-list"}>
-        {this.state.options.map(this.renderOption)}
-      </ul>
-  ) : null;
-
-  renderContent = () => (
-      <div className={"content"}>
-        {this.getInstruction()}
-        {this.renderOptionList()}
-      </div>
-  );
 
 }

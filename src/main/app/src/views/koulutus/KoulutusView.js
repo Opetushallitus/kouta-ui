1a345df4eb3e92f3fddcab78c371f82f5be90a77
Heikki Kupiainen <heikki.kupiainen@metamatic.net>
2018-10-25 18:34:45 +0300

    renamed sections

--- src/main/app/src/views/koulutus/KoulutusView.js
+++ src/main/app/src/views/koulutus/KoulutusView.js
@@ -1,9 +1,9 @@
 import React, {Component} from 'react';
-import {KoulutuksenLuontiSection} from './sections/KoulutuksenLuontiSection';
+import {KoulutuksenPohjaSection} from './sections/KoulutuksenPohjaSection';
 import {KoulutuksenTyyppiSection} from './sections/KoulutuksenTyyppiSection';
 import {KoulutuksenTiedotSection} from './sections/KoulutuksenTiedotSection';
 import {KoulutuksenKuvausSection} from './sections/KoulutuksenKuvausSection';
-import {OrganisaatioSection} from './sections/OrganisaatioSection';
+import {KoulutuksenOrganisaatioSection} from './sections/KoulutuksenOrganisaatioSection';
 import {KoulutuksenKieliversioSection} from './sections/KoulutuksenKieliversioSection';
 
 export class KoulutusView extends Component {
@@ -14,11 +14,11 @@ export class KoulutusView extends Component {
             <button className={"secondary small"}>Esikatsele</button>
           </div>
           <KoulutuksenTyyppiSection orderNumber={"1"}/>
-          <KoulutuksenLuontiSection orderNumber={"2"}/>
+          <KoulutuksenPohjaSection orderNumber={"2"}/>
           <KoulutuksenKieliversioSection orderNumber={"3"}/>
           <KoulutuksenTiedotSection orderNumber={"4"}/>
           <KoulutuksenKuvausSection orderNumber={"5"}/>
-          <OrganisaatioSection orderNumber={"6"}/>
+          <KoulutuksenOrganisaatioSection orderNumber={"6"}/>
       </div>
   )
 }

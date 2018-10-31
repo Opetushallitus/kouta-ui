8e79a2ea9eb030aff456a1871890e2ff7cd29d8f
Heikki Kupiainen <heikki.kupiainen@metamatic.net>
2018-10-31 15:48:12 +0200

    implemented: koulutuksen pohjan valinta

--- src/main/app/src/stores/koulutus/KoulutuksenPohjaStore.js
+++ src/main/app/src/stores/koulutus/KoulutuksenPohjaStore.js
@@ -1,5 +1,22 @@
 import {getUrlKoutaBackendKoulutusList} from '../generic/UrlStore';
 import {PohjanValintaStore} from '../generic/PohjanValintaStore';
 import {SCOPE_KOULUTUKSEN_POHJA} from '../../config/scopes/PohjanValinta';
+import {ENTITY_MODIFICATION_MODE} from '../../config/constants';
 
-export const KoulutuksenPohjaStore = () => PohjanValintaStore(SCOPE_KOULUTUKSEN_POHJA, getUrlKoutaBackendKoulutusList());
+export const KoulutuksenPohjaStore = () =>
+  PohjanValintaStore(SCOPE_KOULUTUKSEN_POHJA, getUrlKoutaBackendKoulutusList(), getModeOptions());
+
+const getModeOptions = () => [
+  {
+    label: 'Luo uusi koulutus',
+    value: ENTITY_MODIFICATION_MODE.CREATE_ENTITY
+  },
+  {
+    label: 'Kopioi pohjaksi aiemmin luotu koulutus',
+    value: ENTITY_MODIFICATION_MODE.INHERIT_ENTITY
+  },
+  {
+    label: 'Käytä olemassa olevan toteutuksen tietoja',
+    value: ENTITY_MODIFICATION_MODE.USE_ENTITY
+  }
+];
--- src/main/app/src/views/koulutus/sections/KoulutuksenPohjanValintaSection.js
+++ src/main/app/src/views/koulutus/sections/KoulutuksenPohjanValintaSection.js
@@ -1,14 +1,9 @@
-import {
-  APP_EVENT_KOULUTUKSEN_POHJA_ENTRY,
-  APP_EVENT_KOULUTUKSEN_POHJA_MODE,
-  APP_STATE_KOULUTUKSEN_POHJA_ENTRY,
-  APP_STATE_KOULUTUKSEN_POHJA_ENTRY_OPTIONS,
-  APP_STATE_KOULUTUKSEN_POHJA_MODE,
-  APP_STATE_KOULUTUKSEN_POHJA_MODE_OPTIONS
-} from '../../../config/states';
 import {AbstractPohjanValintaSection} from '../../../components/AbstractPohjanValintaSection';
+import {SCOPE_KOULUTUKSEN_POHJA} from '../../../config/scopes/PohjanValinta';
 
 export class KoulutuksenPohjanValintaSection extends AbstractPohjanValintaSection {
+
+  getScope = () => SCOPE_KOULUTUKSEN_POHJA;
 
   getCssClassName = () => 'pohjan-valinta-section koulutuksen-pohjan-valinta-section';
 
@@ -17,17 +12,5 @@ export class KoulutuksenPohjanValintaSection extends AbstractPohjanValintaSectio
   getHeader = () => 'Luo koulutus';
 
   getCreateEntityInfoText = () => 'Luodaan uusi koulutus.';
-
-  getStateNameForEntryOptions = () => APP_STATE_KOULUTUKSEN_POHJA_ENTRY_OPTIONS;
-
-  getStateNameForMode = () => APP_STATE_KOULUTUKSEN_POHJA_MODE;
-
-  getStateNameForModeOptions = () => APP_STATE_KOULUTUKSEN_POHJA_MODE_OPTIONS;
-
-  getStateNameForEntry = () => APP_STATE_KOULUTUKSEN_POHJA_ENTRY;
-
-  getEventNameForEntry = () => APP_EVENT_KOULUTUKSEN_POHJA_ENTRY;
-
-  getEventNameForMode = () => APP_EVENT_KOULUTUKSEN_POHJA_MODE;
 
 }

1cf345eca553037f31d6c5baf01d4117ad52b307
Heikki Kupiainen <heikki.kupiainen@metamatic.net>
2018-11-09 19:05:03 +0200

    checkbox filter selector: implemented feature that tags can be removed by clicking on their delete icon

--- src/main/app/src/components/CheckboxFilterSelector.js
+++ src/main/app/src/components/CheckboxFilterSelector.js
@@ -7,16 +7,18 @@ export class CheckboxFilterSelector extends Component {
     super(props);
     this.state = {
       filter: '',
-      visibleList: false
+      expanded: false
     };
   }
+
+  getControlIcon = () => this.state.expanded ? 'expand_more' : 'expand_less';
 
   getFilter = () => this.state.filter;
 
   setFilter = (event) => this.setState({
     ...this.state,
     filter: event.target.value,
-    visibleList: true
+    expanded: true
   });
 
   supportsTags = () => this.props.maxTags && this.props.maxTags > 0;
@@ -34,14 +36,19 @@ export class CheckboxFilterSelector extends Component {
     })
   );
 
+  toggleExpanded = () => this.setState({
+    ...this.state,
+    expanded: !this.state.expanded
+  });
+
   renderOption = (option, index) => (
     <li key={index}>
-      <input type='checkbox' value={option.key} checked={option.active}
+      <input type='checkbox' value={option.key} checked={option.active === true}
              onChange={this.handleCheckboxChange}/>{option.label}
     </li>
   );
 
-  renderOptionList = () => this.state.visibleList && (
+  renderOptionList = () => this.state.expanded && (
     <div className={'options-container'}>
       <ul className={'options-list'}>
         {this.getFilteredOptions().map(this.renderOption)}
@@ -49,8 +56,22 @@ export class CheckboxFilterSelector extends Component {
     </div>
   );
 
-  renderTag = (option) => (
-    <div className={'tag'}>{option.label}</div>
+  removeTag = (key) => {
+    this.props.onSelect(
+      updateMultiSelectionOptionActivation(this.props.options, {
+        key,
+        active: false
+      })
+    );
+  }
+
+  renderTag = (option, index) => (
+    <div key={index} className={'tag'}>
+      <span>{option.label}</span>
+      <div className={'remove-button'} onClick={() => this.removeTag(option.key)}>
+        <i className={'material-icons'}>clear</i>
+      </div>
+    </div>
   )
 
   renderTags = () => this.supportsTags() && (
@@ -64,7 +85,12 @@ export class CheckboxFilterSelector extends Component {
       <span className={'label'}>
         {this.getLabel()}
       </span>
-      <input type={'text'} value={this.getFilter()} onChange={this.setFilter}/>
+      <div className={'controls'}>
+        <input type={'text'} value={this.getFilter()} onChange={this.setFilter}/>
+        <div className={'expand-button'}>
+          <i className="material-icons" onClick={this.toggleExpanded}>{this.getControlIcon()}</i>
+        </div>
+      </div>
       {this.renderOptionList()}
       {this.renderTags()}
     </div>

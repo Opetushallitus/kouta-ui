5c8aee18b0c7c397b5ffc99c97fdbbfe15b28e05
Heikki Kupiainen <heikki.kupiainen@metamatic.net>
2018-11-09 16:17:04 +0200

    component: implemented checkbox filter selector

---
+++ src/main/app/src/components/CheckboxFilterSelector.js
@@ -0,0 +1,57 @@
+import React, {Component} from 'react';
+import {updateMultiSelectionOptionActivation} from '../utils/optionListUtils';
+
+export class CheckboxFilterSelector extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      filter: '',
+      visibleList: false
+    };
+  }
+
+  getFilter = () => this.state.filter;
+
+  setFilter = (event) => this.setState({
+    ...this.state,
+    filter: event.target.value,
+    visibleList: true
+  });
+
+  getOptions = () => (this.props.options || []).filter(option => option.label.includes(this.state.filter));
+
+  getLabel = () => this.props.label;
+
+  handleCheckboxChange = (event) => this.props.onSelect(
+    updateMultiSelectionOptionActivation(this.props.options, {
+      key: event.target.value,
+      active: event.target.checked
+    })
+  );
+
+  renderOption = (option, index) => (
+    <li key={index}>
+      <input type='checkbox' value={option.key} checked={option.active}
+             onChange={this.handleCheckboxChange}/>{option.label}
+    </li>
+  );
+
+  renderOptionList = () => this.state.visibleList && (
+    <div className={'options-container'}>
+      <ul className={'options-list'}>
+        {this.getOptions().map(this.renderOption)}
+      </ul>
+    </div>
+  );
+
+  render = () => (
+    <div className={'checkbox-filter-selector'}>
+      <span className={'label'}>
+        {this.getLabel()}
+      </span>
+      <input type={'text'} value={this.getFilter()} onChange={this.setFilter}/>
+      {this.renderOptionList()}
+    </div>
+  );
+}

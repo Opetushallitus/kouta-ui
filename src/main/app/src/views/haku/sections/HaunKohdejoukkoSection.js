8db7bca2a6707bc3b4ab6825bf4a2f64651c7dc4
Heikki Kupiainen <heikki.kupiainen@metamatic.net>
2018-11-13 13:50:31 +0200

    haun kohdejoukko: implmented displaying haun kohdejoukko options

--- src/main/app/src/views/haku/sections/HaunKohdejoukkoSection.js
+++ src/main/app/src/views/haku/sections/HaunKohdejoukkoSection.js
@@ -1,9 +1,27 @@
-import {AbstractSection} from '../../../components/AbstractSection';
+import {APP_EVENT_ORGANISAATIO_SELECTION_CLEAR} from '../../../config/states';
+import {
+  APP_EVENT_HAUN_KOHDEJOUKKO_SELECTION_CHANGE, APP_EVENT_HAUN_KOHDEJOUKKO_SELECTION_CLEAR,
+  APP_STATE_HAUN_KOHDEJOUKKO_OPTIONS,
+  APP_STATE_HAUN_KOHDEJOUKKO_SELECTIONS
+} from '../../../stores/haku/HaunKohdejoukkoStore';
+import {AbstractCheckboxSection} from '../../../components/AbstractCheckboxSection';
 
-export class HaunKohdejoukkoSection extends AbstractSection {
+export class HaunKohdejoukkoSection extends AbstractCheckboxSection {
 
   getClassName = () => 'HaunKohdejoukkoSection';
 
+  getCssClassName = () => 'checkbox-section haun-kohdejoukko-section';
+
   getHeader = () => 'Haun kohdejoukko';
+
+  getOptionsStateName = () => APP_STATE_HAUN_KOHDEJOUKKO_OPTIONS;
+
+  getSelectionsStateName = () => APP_STATE_HAUN_KOHDEJOUKKO_SELECTIONS;
+
+  getSelectionChangeEventName = () => APP_EVENT_HAUN_KOHDEJOUKKO_SELECTION_CHANGE;
+
+  getSelectionClearEventName = () => APP_EVENT_HAUN_KOHDEJOUKKO_SELECTION_CLEAR;
+
+  getInstruction = () => 'Valitse haun kohdejoukko.';
 
 }

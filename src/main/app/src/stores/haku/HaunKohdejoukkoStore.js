80cc34bc7e21f7a33d9a3151d818b43223838b18
Heikki Kupiainen <heikki.kupiainen@metamatic.net>
2018-11-13 14:01:43 +0200

    haun kohdejoukko: enabled store to handlew selecting kohdejoukko and clearing selections

--- src/main/app/src/stores/haku/HaunKohdejoukkoStore.js
+++ src/main/app/src/stores/haku/HaunKohdejoukkoStore.js
@@ -1,14 +1,23 @@
 import axios from 'axios';
-import {handleEvent, setState} from '../../utils/stateUtils';
+import {clearState, handleEvents, initState, setState, updateState} from '../../utils/stateUtils';
 import {APP_STATE_WORKFLOW} from '../generic/WorkflowStore';
 import {WORKFLOW} from '../../config/constants';
 import {urlHaunKohdejoukko} from '../../config/urls';
 import {matchLanguage} from '../generic/LanguageStore';
 
 export const APP_STATE_HAUN_KOHDEJOUKKO_OPTIONS = 'APP_STATE_HAUN_KOHDEJOUKKO_OPTIONS';
+export const APP_STATE_HAUN_KOHDEJOUKKO_SELECTIONS = 'APP_STATE_HAUN_KOHDEJOUKKO_SELECTIONS';
+export const APP_EVENT_HAUN_KOHDEJOUKKO_SELECTION_CHANGE = 'APP_EVENT_HAUN_KOHDEJOUKKO_SELECTION_CHANGE';
+export const APP_EVENT_HAUN_KOHDEJOUKKO_SELECTION_CLEAR = 'APP_EVENT_ORGANISAATIO_SELECTION_CLEAR';
 
-export const HaunKohdejoukkoStore = () => handleEvent(APP_STATE_WORKFLOW,
-  (workflow) => workflow === WORKFLOW.HAKU && loadHaunKohdejoukkoList(workflow));
+export const HaunKohdejoukkoStore = () => {
+  handleEvents({
+    [APP_STATE_WORKFLOW]: (workflow) => workflow === WORKFLOW.HAKU && loadHaunKohdejoukkoList(workflow),
+    [APP_EVENT_HAUN_KOHDEJOUKKO_SELECTION_CHANGE]: (selection) => selectHaunKohdejoukko(selection.value, selection.selected),
+    [APP_EVENT_HAUN_KOHDEJOUKKO_SELECTION_CLEAR]: () => clearHaunKohdejoukkoSelections()
+  });
+  initState(APP_STATE_HAUN_KOHDEJOUKKO_SELECTIONS, {});
+};
 
 const loadHaunKohdejoukkoList = (workflow) =>
   axios.get(urlHaunKohdejoukko()).then(response => configureHaunKohdejoukkoOptions(response.data));
@@ -17,3 +26,9 @@ const configureHaunKohdejoukkoOptions = (data) => setState(APP_STATE_HAUN_KOHDEJ
   key: entry.koodiUri,
   label: entry.metadata.find(entry => matchLanguage(entry.kieli)).nimi
 })));
+
+const clearHaunKohdejoukkoSelections = () => clearState(APP_STATE_HAUN_KOHDEJOUKKO_SELECTIONS);
+
+export const selectHaunKohdejoukko = (haunKohdejoukkoId, selected) => setState(APP_STATE_HAUN_KOHDEJOUKKO_SELECTIONS, {
+  [haunKohdejoukkoId]: selected
+});

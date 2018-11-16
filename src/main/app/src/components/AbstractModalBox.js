91a710c17b4f9a60eb6cef70e57eb048420833b1
Heikki Kupiainen <heikki.kupiainen@metamatic.net>
2018-11-16 14:36:09 +0200

    modalbox: toteutettu modal box komponentti

---
+++ src/main/app/src/components/AbstractModalBox.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+
+export class AbstractModalBox extends Component {
+
+  getOptions = () => this.props.options || [];
+
+  getTitle = () => {
+    throw new Error('AbstractModalBox:getTitle: implement in subclass');
+  };
+
+  getIcon = () => {
+    throw new Error('AbstractModalBox:getIcon: implement in subclass');
+  };
+
+  renderContent = () => {
+    throw new Error('AbstractModalBox:renderContent: implement in subclass');
+  };
+
+  renderHeader = () => (
+    <div className={'modal-box-header'}>
+      <i className={'material-icons modal-box-title-icon'}>{this.getIcon()}</i>
+      <div className={'modal-box-title'}>{this.getTitle()}</div>
+      <i className={'material-icons modal-box-close-icon'} onClick={this.props.onClose}>close</i>
+    </div>
+  );
+
+  renderFooter = () => (
+    <div className={'modal-box-footer'}>
+    </div>
+  );
+
+  render = () => (
+    <div className={'modal-box-overlay'}>
+      <div className={'modal-box'}>
+        {this.renderHeader()}
+        {this.renderContent()}
+        {this.renderFooter()}</div>
+    </div>
+  );
+}

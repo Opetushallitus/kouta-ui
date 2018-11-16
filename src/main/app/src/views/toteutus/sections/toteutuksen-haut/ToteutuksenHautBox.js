d33c3dcc1b5792b714886525f9b719c7b4c173c1
Heikki Kupiainen <heikki.kupiainen@metamatic.net>
2018-11-16 17:30:34 +0200

    toteutuksen haut: implemented layer button and dropdown

--- src/main/app/src/views/toteutus/sections/toteutuksen-haut/ToteutuksenHautBox.js
+++ src/main/app/src/views/toteutus/sections/toteutuksen-haut/ToteutuksenHautBox.js
@@ -1,16 +1,94 @@
 import React from 'react';
 import {AbstractModalBox} from '../../../../components/AbstractModalBox';
+import {connectComponent} from '../../../../utils/stateUtils';
+import {
+  APP_STATE_HAUN_POHJA_ENTRY_OPTIONS,
+  APP_STATE_HAUN_POHJA_MODE_OPTIONS
+} from '../../../../stores/haku/HaunPohjaStore';
+import {SelectorButton} from '../../../../components/SelectorButton';
+import {ENTITY_MODIFICATION_MODE} from '../../../../config/constants';
 
-export class ToteutuksenHautBox extends AbstractModalBox {
+import {withRouter} from 'react-router-dom';
+import {workflowUrlHaku, workflowUrlHakukohde} from '../../../../config/urls';
+import {InfoDropdown} from '../../../../components/InfoDropdown';
+import {selectWorkflow} from '../../../../stores/generic/WorkflowStore';
+
+class ToteutuksenHautBox extends AbstractModalBox {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dropdownVisible: false
+    };
+  }
+
+  componentDidMount = () => connectComponent(this, {
+    [APP_STATE_HAUN_POHJA_MODE_OPTIONS]: (options) => this.setState({
+      ...this.state,
+      options
+    }),
+    [APP_STATE_HAUN_POHJA_ENTRY_OPTIONS]: (entryOptions) => this.setState({
+      ...this.state,
+      entryOptions
+    })
+  });
 
   getIcon = () => 'grid_on';
 
   getTitle = () => 'Haun liittäminen toteutukseen';
 
+  redirectToHaku = () => {
+    selectWorkflow(workflowUrlHaku());
+    this.props.history.push(workflowUrlHaku());
+  };
+
+  redirectToHakukohde = () => {
+    selectWorkflow(workflowUrlHakukohde());
+    this.props.history.push(workflowUrlHakukohde());
+  };
+
+  getEntry = () => this.state.entry;
+
+  showDropdown = () => this.setState({
+    ...this.state,
+    dropdownVisible: true
+  });
+
+  hideDropdown = (callback) => this.setState({
+    ...this.state,
+    dropdownVisible: true
+  }, callback);
+
+  //TODO: replace this with state update in store
+  handleDropdownChange = () => this.redirectToHaku();
+
+  renderInfoDropdown = () => this.state.dropdownVisible &&
+    <InfoDropdown label={'Valitse listasta'} selection={this.getEntry()}
+                  onChange={this.handleDropdownChange}
+                  options={this.getEntryOptions()}/>;
+
+  selectAction = (event) => this.hideDropdown(({
+    [ENTITY_MODIFICATION_MODE.CREATE_ENTITY]: () => this.redirectToHaku(),
+    [ENTITY_MODIFICATION_MODE.INHERIT_ENTITY]: () => this.showDropdown(),
+    [ENTITY_MODIFICATION_MODE.USE_ENTITY]: () => this.redirectToHakukohde()
+  }[event]()));
+
+  getButtonOptions = () => this.state.options || [];
+
+  getEntryOptions = () => this.state.entryOptions || [];
+
   renderContent = () => (
-    <div className={'modal-box'}>
-      Toteutuksen haut Modal Box
+    <div className={'modal-box-content'}>
+      <span className={'instruction-title'}>Valitse haku, jonka haluat liittää toteutukseen.</span>
+      <span className={'instruction-body'}>Voit luoda uuden haun tai liittää aiemman haun.</span>
+      <div className={'row'}>
+        <SelectorButton layerAlign={'left'} label={'Valitse pohja'} onSelect={this.selectAction}
+                        options={this.getButtonOptions()}/>
+        {this.renderInfoDropdown()}
+      </div>
     </div>
   );
 
 }
+
+export default withRouter(ToteutuksenHautBox);

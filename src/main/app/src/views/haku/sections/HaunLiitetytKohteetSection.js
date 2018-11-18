4fa6068eebfcafaa6bae53c870e788a1b9c8ba97
Heikki Kupiainen <heikki.kupiainen@metamatic.net>
2018-11-18 13:13:01 +0200

    haun liitetyt kohteet: implemented section that shows store-based data in a grid component

--- src/main/app/src/views/haku/sections/HaunLiitetytKohteetSection.js
+++ src/main/app/src/views/haku/sections/HaunLiitetytKohteetSection.js
@@ -1,4 +1,11 @@
+import React from 'react';
 import {AbstractSection} from '../../../components/AbstractSection';
+import {connectComponent} from '../../../utils/stateUtils';
+import {
+  APP_STATE_HAUN_LIITETYT_HAKUKOHTEET_ENTRIES,
+  APP_STATE_HAUN_LIITETYT_HAKUKOHTEET_GRID_CONFIG
+} from '../../../stores/haku/HaunLiitetytHakukohteetStore';
+import {KoutaGrid} from '../../../components/KoutaGrid';
 
 export class HaunLiitetytKohteetSection extends AbstractSection {
 
@@ -6,4 +13,26 @@ export class HaunLiitetytKohteetSection extends AbstractSection {
 
   getHeader = () => 'Liitetyt hakukohteet';
 
+  onMount = () => connectComponent(this, {
+    [APP_STATE_HAUN_LIITETYT_HAKUKOHTEET_ENTRIES]: (entries) => this.setState({
+      ...this.state,
+      entries
+    }),
+    [APP_STATE_HAUN_LIITETYT_HAKUKOHTEET_GRID_CONFIG]: (gridConfig) => this.setState({
+      ...this.state,
+      gridColumns: gridConfig.columns
+    })
+  });
+
+  getGridColumns = () => {
+    return this.state.gridColumns || [];
+  };
+
+  getEntries = () => this.state.entries || [];
+
+  renderContent = () => (
+    <div className={'column'}>
+      <KoutaGrid columns={this.getGridColumns()} data={this.getEntries()} pageIndex={0} pageSize={100}/>
+    </div>
+  );
 }

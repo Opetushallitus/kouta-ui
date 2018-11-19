d1a6cd99dca2ca8f247b79870db4aa8fe3b9c8a3
Heikki Kupiainen <heikki.kupiainen@metamatic.net>
2018-11-19 13:01:38 +0200

    siirretty tekstikentät storeen

--- src/main/app/src/stores/toteutus/ToteutuksenYhteystiedotStore.js
+++ src/main/app/src/stores/toteutus/ToteutuksenYhteystiedotStore.js
@@ -2,13 +2,19 @@ import {initState, setState} from '../../utils/stateUtils';
 
 export const APP_STATE_TOTEUTUKSEN_YHTEYSTIEDOT = 'APP_STATE_TOTEUTUKSEN_YHTEYSTIEDOT';
 
-export const ToteutuksenYhteystiedotStore = () => initState(APP_STATE_TOTEUTUKSEN_YHTEYSTIEDOT, getInitialState());
+export const ToteutuksenYhteystiedotStore = () => setState(APP_STATE_TOTEUTUKSEN_YHTEYSTIEDOT, {
+  valueMap: getInitialValueMap(),
+  fields: getFields()
+});
 
-export const clearValues = () => setState(APP_STATE_TOTEUTUKSEN_YHTEYSTIEDOT, getInitialState());
+export const clearValues = () => updateState(APP_STATE_TOTEUTUKSEN_YHTEYSTIEDOT, {
+  valueMap: getInitialValueMap(),
+  fields: getFields()
+});
 
-export const storeValues = (values) => setState(APP_STATE_TOTEUTUKSEN_YHTEYSTIEDOT, values);
+export const storeValues = (valueMap) => updateState(APP_STATE_TOTEUTUKSEN_YHTEYSTIEDOT, {valueMap});
 
-const getInitialState = () => ({
+const getInitialValueMap = () => ({
   nimi: {
     fi: '',
     sv: '',
@@ -30,3 +36,22 @@ const getInitialState = () => ({
     en: ''
   }
 });
+
+const getFields = () => [
+  {
+    id: 'nimi',
+    label: 'Nimi'
+  },
+  {
+    id: 'titteli',
+    label: 'Titteli?'
+  },
+  {
+    id: 'sahkoposti',
+    label: 'Sähköposti'
+  },
+  {
+    id: 'puhelinnumero',
+    label: 'Puhelinnumero'
+  }
+];

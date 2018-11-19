f2755383bb3fe3bd65eacbc14a13f9af3acb92c3
Heikki Kupiainen <heikki.kupiainen@metamatic.net>
2018-11-19 13:02:51 +0200

    abstrahoitu toteutuksen yhteystiedot yleiskäytettävyyden saavuttamiseksi

--- src/main/app/src/views/toteutus/sections/ToteutuksenYhteystiedotSection.js
+++ src/main/app/src/views/toteutus/sections/ToteutuksenYhteystiedotSection.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import {
-  APP_STATE_TOTEUTUKSEN_YHTEYSTIEDOT,
-  clearValues,
-  storeValues
-} from '../../../stores/toteutus/ToteutuksenYhteystiedotStore';
-import {APP_STATE_TOTEUTUKSEN_KIELIVERSIO_SUPPORTED_LANGUAGES} from "../../../stores/toteutus/ToteutuksenKieliversioStore";
-
+  APP_STATE_TOTEUTUKSEN_KIELIVERSIO_SUPPORTED_LANGUAGES,
+  APP_STATE_TOTEUTUKSEN_YHTEYSTIEDOT
+} from '../../../config/states';
+import {clearValues, storeValues} from '../../../stores/toteutus/ToteutuksenYhteystiedotStore';
 import {AbstractYhteystiedotSection} from '../../../components/AbstractYhteystiedotSection';
 
 export class ToteutuksenYhteystiedotSection extends AbstractYhteystiedotSection {

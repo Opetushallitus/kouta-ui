ee6d162c48b615f4888b0c7cebc36c3fee14f803
Heikki Kupiainen <heikki.kupiainen@metamatic.net>
2018-11-19 13:27:50 +0200

    cleaned imports

--- src/main/app/src/views/toteutus/sections/ToteutuksenYhteystiedotSection.js
+++ src/main/app/src/views/toteutus/sections/ToteutuksenYhteystiedotSection.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import {connectListener} from '../../../utils/stateUtils';
-import {AbstractSection} from '../../../components/AbstractSection';
-import {InputField} from '../../../components/InputField';
-import {APP_STATE_TOTEUTUKSEN_YHTEYSTIEDOT, clearValues, storeValues} from '../../../stores/toteutus/ToteutuksenYhteystiedotStore';
-import {APP_STATE_TOTEUTUKSEN_KIELIVERSIO_SUPPORTED_LANGUAGES} from '../../../stores/toteutus/ToteutuksenKieliversioStore';
+import {
+  APP_STATE_TOTEUTUKSEN_YHTEYSTIEDOT, clearValues,
+  storeValues
+} from '../../../stores/toteutus/ToteutuksenYhteystiedotStore';
+import {AbstractYhteystiedotSection} from '../../../components/AbstractYhteystiedotSection';
+import {APP_STATE_TOTEUTUKSEN_KIELIVERSIO_SUPPORTED_LANGUAGES} from "../../../stores/toteutus/ToteutuksenKieliversioStore";
 
 export class ToteutuksenYhteystiedotSection extends AbstractYhteystiedotSection {
 

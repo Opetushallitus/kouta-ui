b4dc67ba05ee41d007b775d0fcb8fd38a53ebd8b
Heikki Kupiainen <heikki.kupiainen@metamatic.net>
2018-11-25 17:08:02 +0200

    hakukohde sections update

--- src/main/app/src/views/hakukohde/sections/HakukohteenAloituspaikatSection.js
+++ src/main/app/src/views/hakukohde/sections/HakukohteenAloituspaikatSection.js
@@ -1,6 +1,15 @@
 import {AbstractSection} from '../../../components/AbstractSection';
+import {connectComponent} from '../../../utils/stateUtils';
+import {APP_STATE_HAKUKOHTEEN_ALOITUSPAIKAT} from '../../../stores/hakukohde/HakukohteenAloituspaikatStore';
 
 export class HakukohteenAloituspaikatSection extends AbstractSection {
+
+  onMount = () => connectComponent(this, {
+    [APP_STATE_HAKUKOHTEEN_ALOITUSPAIKAT]: (incomingState) => this.setState({
+      ...this.state,
+      ...incomingState
+    })
+  });
 
   getClassName = () => 'HakukohteenAloituspaikatSection';
 

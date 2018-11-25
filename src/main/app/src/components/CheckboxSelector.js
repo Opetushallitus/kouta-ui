75fe8b3ca082490e60721beee179c40465ecc28f
Heikki Kupiainen <heikki.kupiainen@metamatic.net>
2018-11-25 16:18:29 +0200

    checkbox selector: enabled to render optional input field

--- src/main/app/src/components/CheckboxSelector.js
+++ src/main/app/src/components/CheckboxSelector.js
@@ -9,10 +9,19 @@ export class CheckboxSelector extends Component {
     active: event.target.checked
   });
 
+  optionallyRenderInputField = (option) => option.input && option.active ? (
+    <input type={'text'} data-key={option.key} value={option.value} placeholder={option.input.placeholder} onChange={this.handleInputFieldChange}/>
+  ) : null;
+
+  handleInputFieldChange = (event) => this.props.onInput({
+    key: event.target.getAttribute('data-key'),
+    value:  event.target.value
+  });
+
   renderOption = (option, index) => (
     <li key={index}>
       <input type="checkbox" value={option.key} checked={option.active === true}
-             onChange={this.handleCheckboxChange}/>{option.label}
+             onChange={this.handleCheckboxChange}/>{option.label} {this.optionallyRenderInputField(option)}
     </li>
   );
 

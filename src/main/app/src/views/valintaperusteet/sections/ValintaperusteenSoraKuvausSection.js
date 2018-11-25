a53d88b0c9f6013b8aab54c9626a93210f04f61a
Heikki Kupiainen <heikki.kupiainen@metamatic.net>
2018-11-25 17:06:18 +0200

    valintaperusteet: implemented checkbox & radiogroup sections

--- src/main/app/src/views/valintaperusteet/sections/ValintaperusteenSoraKuvausSection.js
+++ src/main/app/src/views/valintaperusteet/sections/ValintaperusteenSoraKuvausSection.js
@@ -1,6 +1,15 @@
 import {AbstractSection} from '../../../components/AbstractSection';
+import {connectComponent} from '../../../utils/stateUtils';
+import {APP_STATE_VALINTAPERUSTEEN_SORA_KUVAUS} from '../../../stores/valintaperusteet/ValintaperusteenSoraKuvausStore';
 
 export class ValintaperusteenSoraKuvausSection extends AbstractSection {
+
+  onMount = () => connectComponent(this, {
+    [APP_STATE_VALINTAPERUSTEEN_SORA_KUVAUS]: (incomingState) => this.setState({
+      ...this.state,
+      ...incomingState
+    })
+  });
 
   getClassName = () => 'ValintaperusteenSoraKuvausSection';
 
--- src/main/app/src/views/valintaperusteet/sections/ValintaperusteenVahimmaisehdotSection.js
+++ src/main/app/src/views/valintaperusteet/sections/ValintaperusteenVahimmaisehdotSection.js
@@ -1,9 +1,31 @@
+import React from 'react';
 import {AbstractSection} from '../../../components/AbstractSection';
+import {connectComponent} from '../../../utils/stateUtils';
+import {
+  APP_STATE_VALINTAPERUSTEEN_VAHIMMAISEHDOT,
+  selectOption
+} from '../../../stores/valintaperusteet/ValintaperusteenVahimmaisehdotStore';
+import {CheckboxSelector} from '../../../components/CheckboxSelector';
 
 export class ValintaperusteenVahimmaisehdotSection extends AbstractSection {
+
+  onMount = () => connectComponent(this, {
+    [APP_STATE_VALINTAPERUSTEEN_VAHIMMAISEHDOT]: (incomingState) => this.setState({
+      ...this.state,
+      ...incomingState
+    })
+  });
 
   getClassName = () => 'ValintaperusteenVahimmaisehdotSection';
 
   getHeader = () => 'Valituksi tulemisen vähimmäisehdot';
 
+  getOptions = () => this.state.options || [];
+
+  renderContent = () => (
+    <div className={'content'}>
+      <span className={'instruction-span'}>Hakija on hakukelpoinen jos hän</span>
+      <CheckboxSelector options={this.getOptions()} onChange={selectOption}/>
+    </div>
+  );
 }

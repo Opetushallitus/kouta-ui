5d3216215d04f45466d7af98dc910589887c01ac
Heikki Kupiainen <heikki.kupiainen@metamatic.net>
2018-11-27 12:50:00 +0200

    siirretty haun ja toteuksen nimiin liittyvät tilamääritykset vastaaviin storeihin

--- src/main/app/src/views/haku/sections/HaunNimiSection.js
+++ src/main/app/src/views/haku/sections/HaunNimiSection.js
@@ -1,9 +1,8 @@
 import {
-  APP_EVENT_HAUN_NIMI_TRANSLATION_CLEAR,
   APP_STATE_HAUN_KIELIVERSIO_SUPPORTED_LANGUAGES,
-  APP_STATE_HAUN_NIMI_TRANSLATION_MAP
 } from '../../../config/states';
 import {AbstractNimiSection} from '../../../components/AbstractNimiSection';
+import {APP_EVENT_HAUN_NIMI_TRANSLATION_CLEAR, APP_STATE_HAUN_NIMI_TRANSLATION_MAP} from '../../../stores/haku/HaunNimiStore';
 
 export class HaunNimiSection extends AbstractNimiSection {
 

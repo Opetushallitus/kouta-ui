2bfb27e3de2eafe6c28e25cb08bb6edec7340bcc
Heikki Kupiainen <heikki.kupiainen@metamatic.net>
2018-11-28 22:31:13 +0200

    hakukohteen liitteet: lisätty demodata

--- src/main/app/src/stores/hakukohde/HakukohteenTarvittavatLiitteetStore.js
+++ src/main/app/src/stores/hakukohde/HakukohteenTarvittavatLiitteetStore.js
@@ -6,4 +6,57 @@ export const HakukohteenTarvittavatLiitteetStore = () => initState(APP_STATE_HAK
 
 export const clearSelections = () => setState(APP_STATE_HAKUKOHTEEN_TARVITTAVAT_LIITTEET, getInitialState());
 
-const getInitialState = () => ({});
+const getInitialState = () => ({
+  liitteenTyyppiOptions: [
+    {
+      key: 'tutkintotodistus',
+      label: 'Tutkintotodistus'
+    },
+    {
+      key: 'muu-liitteen-tyyppi-1',
+      label: 'Muu liitteen tyyppi 1'
+    },
+    {
+      key: 'muu-liitteen-tyyppi-2',
+      label: 'Muu liitteen tyyppi 2'
+    },
+    {
+      key: 'muu-liitteen-tyyppi-3',
+      label: 'Muu liitteen tyyppi 3'
+    }
+  ],
+  liitteenNimiOptions: [
+    {
+      key: 'ylioppilastodistus-ennen-vuotta-1990',
+      label: 'Ylioppilastodistus ennen vuotta 1990'
+    },
+    {
+      key: 'muu-liitteen-nimi-1',
+      label: 'Muu liitteen nimi 1'
+    },
+    {
+      key: 'muu-liitteen-nimi-2',
+      label: 'Muu liitteen nimi 2'
+    },
+    {
+      key: 'muu-liitteen-nimi-3',
+      label: 'Muu liitteen nimi 3'
+    }
+  ],
+  liitteenKuvaus: '',
+  liitteenTakaraja: {
+    date: '',
+    time: ''
+  },
+  samaOsoiteKaikissaLiitteissa: false,
+  toimitusosoiteOptions: [
+    {
+      key: 'hakijapalvelun-osoite',
+      label: 'Koulutuksen järjestäjän hakijapalvelun osoite'
+    },
+    {
+      key: 'tonen-toimitusosoite',
+      label: 'Toinen toimitusosoite'
+    }
+  ]
+});

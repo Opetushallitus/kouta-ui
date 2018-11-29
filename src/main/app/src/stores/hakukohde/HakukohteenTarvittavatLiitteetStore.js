19bc8103c7e393118a0a199288a6a0ebd1ddaba6
Heikki Kupiainen <heikki.kupiainen@metamatic.net>
2018-11-29 17:39:51 +0200

    toteutettu Hakukohteen liitteet sektio Hakukohde-näkymään

--- src/main/app/src/stores/hakukohde/HakukohteenTarvittavatLiitteetStore.js
+++ src/main/app/src/stores/hakukohde/HakukohteenTarvittavatLiitteetStore.js
@@ -1,62 +1,81 @@
-import {initState, setState} from '../../utils/stateUtils';
+import {getState, setState, updateState} from '../../utils/stateUtils';
+import {replaceItemsWithMatchingProperty} from "../../utils/arrayUtils";
 
 export const APP_STATE_HAKUKOHTEEN_TARVITTAVAT_LIITTEET = 'APP_STATE_HAKUKOHTEEN_TARVITTAVAT_LIITTEET';
 
-export const HakukohteenTarvittavatLiitteetStore = () => initState(APP_STATE_HAKUKOHTEEN_TARVITTAVAT_LIITTEET, getInitialState());
+export const HakukohteenTarvittavatLiitteetStore = () => setState(APP_STATE_HAKUKOHTEEN_TARVITTAVAT_LIITTEET, getInitialState());
 
 export const clearSelections = () => setState(APP_STATE_HAKUKOHTEEN_TARVITTAVAT_LIITTEET, getInitialState());
 
+//TODO: korvataan tämä APIsta tulevilla datoilla
 const getInitialState = () => ({
-  liitteenTyyppiOptions: [
-    {
-      key: 'tutkintotodistus',
-      label: 'Tutkintotodistus'
-    },
-    {
-      key: 'muu-liitteen-tyyppi-1',
-      label: 'Muu liitteen tyyppi 1'
-    },
-    {
-      key: 'muu-liitteen-tyyppi-2',
-      label: 'Muu liitteen tyyppi 2'
-    },
-    {
-      key: 'muu-liitteen-tyyppi-3',
-      label: 'Muu liitteen tyyppi 3'
-    }
-  ],
-  liitteenNimiOptions: [
-    {
-      key: 'ylioppilastodistus-ennen-vuotta-1990',
-      label: 'Ylioppilastodistus ennen vuotta 1990'
-    },
-    {
-      key: 'muu-liitteen-nimi-1',
-      label: 'Muu liitteen nimi 1'
-    },
-    {
-      key: 'muu-liitteen-nimi-2',
-      label: 'Muu liitteen nimi 2'
-    },
-    {
-      key: 'muu-liitteen-nimi-3',
-      label: 'Muu liitteen nimi 3'
-    }
-  ],
-  liitteenKuvaus: '',
-  liitteenTakaraja: {
-    date: '',
-    time: ''
-  },
-  samaOsoiteKaikissaLiitteissa: false,
-  toimitusosoiteOptions: [
-    {
-      key: 'hakijapalvelun-osoite',
-      label: 'Koulutuksen järjestäjän hakijapalvelun osoite'
-    },
-    {
-      key: 'tonen-toimitusosoite',
-      label: 'Toinen toimitusosoite'
-    }
-  ]
+    hakukohteenLiiteEntries: [
+        createHakukohteenLiiteEntry(1)
+    ]
 });
+
+const createHakukohteenLiiteEntry = (id) => ({
+    id: id.toString(),
+    liitteenTyyppiOptions: [
+        {
+            key: 'tutkintotodistus',
+            label: 'Tutkintotodistus'
+        },
+        {
+            key: 'todistus',
+            label: 'Todistus'
+        },
+        {
+            key: 'portfolio',
+            label: 'Portfolio'
+        }
+    ],
+    liitteenNimi: '',
+    liitteenKuvaus: '',
+    liitteenTakaraja: {
+        date: '',
+        time: ''
+    },
+    samaOsoiteKaikissaLiitteissa: false,
+    toimitusosoiteOptions: [
+        {
+            key: 'hakijapalvelun-osoite',
+            label: 'Koulutuksen järjestäjän hakijapalvelun osoite',
+            content: {
+                jarjestajanNimi: 'Koulutuskeuskus Salpaus-kuntayhtymä',
+                katuosoite: 'Paasikivenkatu 7',
+                postinumero: '15110',
+                postitoimipaikka: 'Lahti',
+                sahkoposti: 'salpaus@salapaus.fi'
+            }
+        },
+        {
+            key: 'vaihtoehtoinen-toimitusosoite',
+            label: 'Toinen toimitusosoite'
+        }
+    ],
+    vaihtoehtoinenToimitusosoite: {
+        jarjestajanNimi: '',
+        katuosoite: '',
+        postinumero: '',
+        postitoimipaikka: '',
+        sahkoposti: ''
+    }
+});
+
+export const getHakukohteenLiiteEntries = () => getState(APP_STATE_HAKUKOHTEEN_TARVITTAVAT_LIITTEET, 'hakukohteenLiiteEntries');
+
+const setHakukohteenLiiteEntries = (hakukohteenLiiteEntries) => updateState(APP_STATE_HAKUKOHTEEN_TARVITTAVAT_LIITTEET, {
+    hakukohteenLiiteEntries
+});
+
+export const updateHakukohteenLiite = (changedHakukohteenLiite) => setHakukohteenLiiteEntries(
+    replaceItemsWithMatchingProperty(getHakukohteenLiiteEntries(), changedHakukohteenLiite, 'id')
+);
+
+export const addNewHakukohteenLiite = () => {
+    const hakukohteenLiiteEntries = getHakukohteenLiiteEntries();
+    const newId = hakukohteenLiiteEntries.length + 1;
+    const newEntries = hakukohteenLiiteEntries.concat([createHakukohteenLiiteEntry(newId)]);
+    setHakukohteenLiiteEntries(newEntries);
+};

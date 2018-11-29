97ddec4f8e5d62b47907e591ee3b3c2f1278eae4
Heikki Kupiainen <heikki.kupiainen@metamatic.net>
2018-11-29 17:27:21 +0200

    input field: yhdenmukaistettu TextAreaField-komponentin toiminnan kanssa

--- src/main/app/src/components/InputField.js
+++ src/main/app/src/components/InputField.js
@@ -2,19 +2,22 @@ import React, {Component} from 'react';
 
 export class InputField extends Component {
 
-  onChange = (event) => this.props.onChange({...this.props.field, value: event.target.value});
+  onChange = (event) => this.props.onChange({
+      key: this.getId(),
+      value: event.target.value
+  });
 
   getKey = () => this.getId();
 
-  getId = () => this.props.field.id;
+  getId = () => this.props.id;
 
-  getLabel = () => this.props.field.label;
+  getLabel = () => this.props.label;
 
-  getValue = () => this.props.field.value;
+  getValue = () => this.props.value;
 
   render = () => (
     <div id={this.getId()} key={this.getKey()} className={'input-field'}>
-      <span className={'label'}>{this.getLabel()}</span>
+      <span className={'label-span'}>{this.getLabel()}</span>
       <input type={'text'} value={this.getValue()} placeholder={this.getLabel()} onChange={this.onChange}/>
     </div>
   );

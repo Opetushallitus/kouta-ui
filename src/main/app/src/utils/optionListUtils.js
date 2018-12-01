4f33813fff6006a14730f559cb6d1092c0ba0230
Heikki Kupiainen <heikki.kupiainen@metamatic.net>
2018-12-01 12:40:50 +0200

    toteutettu apufunktio optionien yhdistämiseksi käyttäjän valintoihin

--- src/main/app/src/utils/optionListUtils.js
+++ src/main/app/src/utils/optionListUtils.js
@@ -27,3 +27,8 @@ export const updateOptionValue = (options, change) => options.map(entry => ({
 }));
 
 export const getActiveKey = (options) => (options.find(option => option.active) || {}).key;
+
+export const getStatefulOptions = (options, selections) => options.map(option => ({
+    ...option,
+    active: selections[option.key]
+}));

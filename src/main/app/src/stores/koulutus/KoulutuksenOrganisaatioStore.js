aab9eeb83605b7e09387578151f517461555585d
Heikki Kupiainen <heikki.kupiainen@metamatic.net>
2018-12-02 19:06:22 +0200

    asetetaan organisaatio automaattisesti valituksia mikäli valintoja on vain yksi

--- src/main/app/src/stores/koulutus/KoulutuksenOrganisaatioStore.js
+++ src/main/app/src/stores/koulutus/KoulutuksenOrganisaatioStore.js
@@ -1,6 +1,14 @@
 import axios from 'axios';
 import {urls} from 'oph-urls-js';
-import {clearState, containsValue, getState, handleEvents, initStates, setState, updateState} from '../../utils/stateUtils';
+import {
+    clearState,
+    containsValue,
+    getState,
+    handleEvents,
+    initStates,
+    setState, setStates,
+    updateState
+} from '../../utils/stateUtils';
 import {getLanguage} from '../generic/LanguageStore';
 import {APP_STATE_KOULUTUKSEN_TIEDOT} from "./KoulutuksenTiedotStore";
 
@@ -49,8 +57,19 @@ const loadOrganisaatioList = (parentOid) => excludesOrganisaatioList() ?
   axios.get(urls.url('organisaatio-service.children', parentOid))
     .then((response) => setOrganisaatioOptionsData(response.data)) : null;
 
-const setOrganisaatioOptionsData = (jsonData) =>
-  setState(APP_STATE_ORGANISAATIO_OPTIONS, extractOrganisaatioOptions(jsonData));
+const setOrganisaatioOptionsData = (jsonData) => {
+    const options = extractOrganisaatioOptions(jsonData);
+    const selections = {}
+    if (options.length ===1) {
+      const activeOid = options[0].key;
+      selections[activeOid] = true;
+    }
+    setStates({
+        [APP_STATE_ORGANISAATIO_OPTIONS] : options,
+        [APP_STATE_ORGANISAATIO_SELECTIONS]: selections
+    });
+};
+
 
 const extractOrganisaatioOptions = (jsonData) =>
   jsonData.filter(entry => entry.tyypit.includes('organisaatiotyyppi_02') && entry.status !== 'PASSIIVINEN')

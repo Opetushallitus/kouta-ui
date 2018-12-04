e20030920fdb8dc1a5ac4f827afa141bc7f1d0e8
Jessika Penttinen <jessika.penttinen@siili.com>
2018-12-04 10:14:06 +0200

    Vaihdettu sessionStorage localStoragen tilalle

--- src/main/app/src/utils/storageUtils.js
+++ src/main/app/src/utils/storageUtils.js
@@ -1,9 +1,9 @@
 import {parseJson} from './objectUtils';
 
-export const getItem = (eventName) => parseJson(localStorage.getItem(eventName));
+export const getItem = (eventName) => parseJson(sessionStorage.getItem(eventName));
 
 export const enforceItem = (eventName) => getItem(eventName) || null;
 
-export const setItem = (eventName, item) => localStorage.setItem(eventName, JSON.stringify(item));
+export const setItem = (eventName, item) => sessionStorage.setItem(eventName, JSON.stringify(item));
 
-export const existsItem = (eventName) => localStorage.getItem(eventName) !== null;
+export const existsItem = (eventName) => sessionStorage.getItem(eventName) !== null;

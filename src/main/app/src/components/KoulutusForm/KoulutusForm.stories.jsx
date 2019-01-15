375a5812d3f1b9518a622fd1e0d9afeaac9450b3
Kalle Ilves <kalleilv@gmail.com>
2019-01-15 14:47:03 +0200

    Api decorator for storybook

---
+++ src/main/app/src/components/KoulutusForm/KoulutusForm.stories.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { reduxForm } from 'redux-form';
+
+import KoulutusForm from './index';
+import { makeStoreDecorator, makeApiDecorator } from '../../storybookUtils';
+
+const Form = reduxForm({
+  form: 'koulutusForm',
+})(KoulutusForm);
+
+storiesOf('KoulutusForm', module)
+  .addDecorator(makeStoreDecorator())
+  .addDecorator(makeApiDecorator())
+  .add('Basic', () => <Form />);
--- src/main/app/src/storybookUtils.js
+++ src/main/app/src/storybookUtils.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { action } from '@storybook/addon-actions';
+import Async from 'react-async';
+import { urls as ophUrls } from 'oph-urls-js';
+import axios from 'axios';
 
 import createStore from './state/store';
+import configureUrls from './apiUrls';
+import HttpContext from './components/HttpContext';
+import UrlContext from './components/UrlContext';
+
+const defaultHttpClient = axios.create({});
+const configureOphUrls = () => configureUrls(ophUrls);
+
+export const makeApiDecorator = ({
+  httpClient = defaultHttpClient,
+} = {}) => storyFn => {
+  return (
+    <HttpContext.Provider value={httpClient}>
+      <Async promiseFn={configureOphUrls}>
+        {({ data }) =>
+          data ? (
+            <UrlContext.Provider value={data}>{storyFn()}</UrlContext.Provider>
+          ) : null
+        }
+      </Async>
+    </HttpContext.Provider>
+  );
+};
+
 export const makeStoreDecorator = ({ logging = false } = {}) => storyFn => {
   const store = createStore({});
   const storeAction = action('change');

f6b58ad0a4c5694fbed290ef59423fca8b118976
Mikko Tuppurainen <mikkotuppurainen@Mikkos-MBP.core.pop2.reaktor.fi>
2019-02-06 15:49:59 +0200

    fixed syntax and removed comment

--- src/main/app/src/components/CreateHakuPage/CreateHakuForm.jsx
+++ src/main/app/src/components/CreateHakuPage/CreateHakuForm.jsx
@@ -6,7 +6,6 @@ import HakuForm, { validate } from '../HakuForm';
 export default compose(
   withProps(({ organisaatio }) => ({
     steps: true,
-   // initialValues: getInitialValues({ organisaatio }),
   })),
   reduxForm({
     form: 'createHakuForm',

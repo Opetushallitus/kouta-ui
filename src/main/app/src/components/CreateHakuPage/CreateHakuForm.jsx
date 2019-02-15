48b78106b6623b1d295d9a0905b30954ecdc452a
Mikko Tuppurainen <mikkotuppurainen@HEL-CND4349D4B.global.ad>
2019-02-15 13:40:13 +0200

    fixed merge conflict

--- src/main/app/src/components/CreateHakuPage/CreateHakuForm.jsx
+++ src/main/app/src/components/CreateHakuPage/CreateHakuForm.jsx
@@ -6,12 +6,6 @@ import HakuForm, { validate } from '../HakuForm';
 export default compose(
   withProps(({ organisaatio }) => ({
     steps: true,
-<<<<<<< HEAD
-   // initialValues: getInitialValues({ organisaatio }),
-
-
-=======
->>>>>>> 61be369a29dcada655e9b3f1c1b3405c6b3c84f5
   })),
   reduxForm({
     form: 'createHakuForm',

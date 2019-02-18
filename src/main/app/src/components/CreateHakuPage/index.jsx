8c1c168d452e3be6add2f41f7987b6564c8af949
Mikko Tuppurainen <mikkotuppurainen@Mikkos-MBP.core.pop2.reaktor.fi>
2019-02-18 13:37:12 +0200

    Korjattu haun GET-kutsu

--- src/main/app/src/components/CreateHakuPage/index.jsx
+++ src/main/app/src/components/CreateHakuPage/index.jsx
@@ -1,32 +1,12 @@
 import React, { useCallback } from 'react';
 import queryString from 'query-string';
 
-import ApiAsync from '../ApiAsync';
 import FormPage, { OrganisaatioInfo } from '../FormPage';
-import { getKoutaHakuByOid } from '../../apiUtils';
 
 import CreateHakuHeader from './CreateHakuHeader';
 import CreateHakuSteps from './CreateHakuSteps';
 import CreateHakuForm from './CreateHakuForm';
 import CreateHakuFooter from './CreateHakuFooter';
-
-const getHaku = args => getKoutaHakuByOid(args);
-
-const CreateHakuFormAsync = ({ hakuOid, organisaatioOid, ...props }) => (
-  <>
-    <OrganisaatioInfo organisaatioOid={organisaatioOid} />
-    <ApiAsync promiseFn={getHaku} oid={hakuOid} watch={hakuOid}>
-      {({ data }) =>
-        data ? (
-          <CreateHakuForm
-            organisaatioOid={organisaatioOid}
-            {...props}
-          />
-        ) : null
-      }
-    </ApiAsync>
-  </>
-);
 
 const CreateHakuPage = props => {
   const {
@@ -49,7 +29,8 @@ const CreateHakuPage = props => {
       steps={<CreateHakuSteps />}
       footer={<CreateHakuFooter />}
     >
-      <CreateHakuFormAsync
+    <OrganisaatioInfo organisaatioOid={organisaatioOid} />
+      <CreateHakuForm
         organisaatioOid={organisaatioOid}
         kopiohakuOid={kopiohakuOid}
         onCreateNew={onCreateNew}

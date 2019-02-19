3e7790526357d270a7016b3ff264c501a78e7394
Mikko Tuppurainen <mikkotuppurainen@Mikkos-MBP.core.pop2.reaktor.fi>
2019-02-19 09:12:23 +0200

    poistettu turhat kommentit

--- src/main/app/src/components/CreateHakuPage/CreateHakuForm.jsx
+++ src/main/app/src/components/CreateHakuPage/CreateHakuForm.jsx
@@ -1,18 +1,3 @@
-/*import { reduxForm } from 'redux-form';
-import { compose, withProps } from 'recompose';
-
-import HakuForm, { validate } from '../HakuForm';
-
-export default compose(
-  withProps(({ organisaatio }) => ({
-    steps: true,
-  })),
-  reduxForm({
-    form: 'createHakuForm',
-    validate,
-  }),
-)(HakuForm);
-*/
 import { reduxForm } from 'redux-form';
 import { compose } from 'recompose';
 import { connect } from 'react-redux';

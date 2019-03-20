518174ec10804f1b055c7cfb1a4cdf1234cfd701
Mikko Tuppurainen <mikkotuppurainen@Mikkos-MBP.core.pop2.reaktor.fi>
2019-03-20 15:39:37 +0200

    korjattu typo haku-hakukohteet urlissa

--- src/main/java/fi/oph/kouta/UrlConfiguration.java
+++ src/main/java/fi/oph/kouta/UrlConfiguration.java
@@ -28,7 +28,7 @@ public class UrlConfiguration extends OphProperties {
         this.frontProperties.setProperty("kouta-backend.haku", this.require("kouta-backend.haku"));
         this.frontProperties.setProperty("kouta-backend.haku-by-oid", this.require("kouta-backend.haku-by-oid"));
         this.frontProperties.setProperty("kouta-backend.haku-list", this.require("kouta-backend.haku-list"));
-        this.frontProperties.setProperty("kouta-backend.haku-hakukohteet"), this.require("kouta-backend.haku-hakukohteet"));
+        this.frontProperties.setProperty("kouta-backend.haku-hakukohteet", this.require("kouta-backend.haku-hakukohteet"));
 
         this.frontProperties.setProperty("kouta-backend.hakukohde", this.require("kouta-backend.hakukohde"));
         this.frontProperties.setProperty("kouta-backend.hakukohde-by-oid", this.require("kouta-backend.hakukohde-by-oid"));

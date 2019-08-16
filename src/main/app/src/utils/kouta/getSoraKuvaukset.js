8f2940c1dfb36f6fc95f206882e41fc6c20b14b5
Kalle Ilves <kalleilv@gmail.com>
2019-08-16 09:00:49 +0300

    organisaatioOid query-parametriksi

--- src/main/app/src/utils/kouta/getSoraKuvaukset.js
+++ src/main/app/src/utils/kouta/getSoraKuvaukset.js
@@ -1,6 +1,7 @@
-const getSoraKuvaukset = async ({ httpClient, apiUrls }) => {
+const getSoraKuvaukset = async ({ httpClient, apiUrls, organisaatioOid }) => {
   const { data } = await httpClient.get(
     apiUrls.url('kouta-backend.sora-kuvaus-list'),
+    { params: { organisaatioOid } },
   );
 
   return data;

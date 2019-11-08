6d58a36b17b4a222952fdfda0f1472cd5c2e8229
teo.mertanen <teo.mertanen@gofore.com>
2019-11-08 09:01:13 +0200

    KTO-344 add toteutuksen kuvaus field

--- src/main/app/cypress/integration/toteutusForm/createToteutusForm.js
+++ src/main/app/cypress/integration/toteutusForm/createToteutusForm.js
@@ -163,6 +163,10 @@ const fillTiedotSection = () => {
       .find('input')
       .type('toteutuksen nimi', { force: true });
 
+    cy.getByTestId('kuvaus')
+      .find('input')
+      .type('toteutuksen kuvaus', { force: true });
+
     jatka();
   });
 };

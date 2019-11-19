c17cdfcc57379641754dec433bcee718c0937633
oph-petar <petar@phz.fi>
2019-11-19 13:48:36 +0200

    Update cypress snapshots to reflect bugfix in postinumero

--- src/main/app/cypress/integration/oppilaitosForm/snapshots.js
+++ src/main/app/cypress/integration/oppilaitosForm/snapshots.js
@@ -1,103 +1,99 @@
 module.exports = {
-  "createOppilaitosForm": {
-    "should be able to create oppilaitos": {
-      "1": {
-        "organisaatioOid": "1.1.1.1.1.1",
-        "oid": "1.1.1.1.1.1",
-        "tila": "julkaistu",
-        "muokkaaja": "1.2.246.562.24.62301161440",
-        "kielivalinta": [
-          "fi"
-        ],
-        "metadata": {
-          "yhteystiedot": {
-            "osoite": {
-              "osoite": {
-                "fi": "Osoite"
+  createOppilaitosForm: {
+    'should be able to create oppilaitos': {
+      '1': {
+        organisaatioOid: '1.1.1.1.1.1',
+        oid: '1.1.1.1.1.1',
+        tila: 'julkaistu',
+        muokkaaja: '1.2.246.562.24.62301161440',
+        kielivalinta: ['fi'],
+        metadata: {
+          yhteystiedot: {
+            osoite: {
+              osoite: {
+                fi: 'Osoite',
               },
-              "postinumeroKoodiUri": "posti_0#fi"
+              postinumeroKoodiUri: 'posti_0#2',
             },
-            "sahkoposti": {
-              "fi": "sahkoposti@sahkoposti.fi"
+            sahkoposti: {
+              fi: 'sahkoposti@sahkoposti.fi',
             },
-            "puhelinnumero": {
-              "fi": "12345"
+            puhelinnumero: {
+              fi: '12345',
             },
-            "wwwSivu": {
-              "fi": "www.verkkosivu.fi"
-            }
+            wwwSivu: {
+              fi: 'www.verkkosivu.fi',
+            },
           },
-          "esittely": {
-            "fi": "<p>Esittely</p>"
+          esittely: {
+            fi: '<p>Esittely</p>',
           },
-          "tietoaOpiskelusta": [
+          tietoaOpiskelusta: [
             {
-              "otsikkoKoodiUri": "koulutuksenlisatiedot_0#1",
-              "teksti": {
-                "fi": "Tietoa"
-              }
-            }
-          ],
-          "opiskelijoita": 1,
-          "korkeakouluja": 2,
-          "tiedekuntia": 3,
-          "kampuksia": 4,
-          "yksikoita": 5,
-          "toimipisteita": 6,
-          "akatemioita": 7
-        }
-      }
-    }
-  },
-  "__version": "3.4.1",
-  "editOppilaitosForm": {
-    "should be able to edit oppilaitos": {
-      "1": {
-        "organisaatioOid": "1.1.1.1.1.1",
-        "oid": "1.1.1.1.1.1",
-        "kielivalinta": [
-          "fi"
-        ],
-        "tila": "tallennettu",
-        "metadata": {
-          "yhteystiedot": {
-            "osoite": {
-              "osoite": {
-                "fi": "Fi osoite"
+              otsikkoKoodiUri: 'koulutuksenlisatiedot_0#1',
+              teksti: {
+                fi: 'Tietoa',
               },
-              "postinumeroKoodiUri": "posti_0#2"
             },
-            "sahkoposti": {
-              "fi": "fi@sahkoposti.fi"
-            },
-            "puhelinnumero": {
-              "fi": "1234"
-            },
-            "wwwSivu": {
-              "fi": "www.verkkosivu.fi"
-            }
-          },
-          "esittely": {
-            "fi": "<p><em>Fi esittely</em></p>"
-          },
-          "tietoaOpiskelusta": [
-            {
-              "otsikkoKoodiUri": "koulutuksenlisatiedot_0#1",
-              "teksti": {
-                "fi": "Fi tiedot"
-              }
-            }
           ],
-          "opiskelijoita": 100,
-          "korkeakouluja": 5,
-          "tiedekuntia": 4,
-          "kampuksia": 3,
-          "yksikoita": 2,
-          "toimipisteita": 9,
-          "akatemioita": 1
+          opiskelijoita: 1,
+          korkeakouluja: 2,
+          tiedekuntia: 3,
+          kampuksia: 4,
+          yksikoita: 5,
+          toimipisteita: 6,
+          akatemioita: 7,
         },
-        "muokkaaja": "1.2.246.562.24.62301161440"
-      }
-    }
-  }
-}
+      },
+    },
+  },
+  __version: '3.4.1',
+  editOppilaitosForm: {
+    'should be able to edit oppilaitos': {
+      '1': {
+        organisaatioOid: '1.1.1.1.1.1',
+        oid: '1.1.1.1.1.1',
+        kielivalinta: ['fi'],
+        tila: 'tallennettu',
+        metadata: {
+          yhteystiedot: {
+            osoite: {
+              osoite: {
+                fi: 'Fi osoite',
+              },
+              postinumeroKoodiUri: 'posti_0#2',
+            },
+            sahkoposti: {
+              fi: 'fi@sahkoposti.fi',
+            },
+            puhelinnumero: {
+              fi: '1234',
+            },
+            wwwSivu: {
+              fi: 'www.verkkosivu.fi',
+            },
+          },
+          esittely: {
+            fi: '<p><em>Fi esittely</em></p>',
+          },
+          tietoaOpiskelusta: [
+            {
+              otsikkoKoodiUri: 'koulutuksenlisatiedot_0#1',
+              teksti: {
+                fi: 'Fi tiedot',
+              },
+            },
+          ],
+          opiskelijoita: 100,
+          korkeakouluja: 5,
+          tiedekuntia: 4,
+          kampuksia: 3,
+          yksikoita: 2,
+          toimipisteita: 9,
+          akatemioita: 1,
+        },
+        muokkaaja: '1.2.246.562.24.62301161440',
+      },
+    },
+  },
+};

37e71c9d528a554feed7969e477f3a41215cd544
teo.mertanen <teo.mertanen@gofore.com>
2019-12-02 13:51:13 +0200

    KTO-425 valintaperuste käyttämään validaatiota konfiguraatiosta

--- src/main/app/src/utils/validateValintaperusteForm.js
+++ src/main/app/src/utils/validateValintaperusteForm.js
@@ -1,48 +1,8 @@
-import get from 'lodash/get';
+import getValintaperusteFormConfig from "./getValintaperusteFormConfig";
+import getErrorBuilderByFormConfig from './getErrorBuilderByFormConfig';
 
-import { JULKAISUTILA } from '../constants';
-import createErrorBuilder from './createErrorBuilder';
-
-const getKieliversiot = values => get(values, 'kieliversiot') || [];
-
-const validateEssentials = ({ errorBuilder, values }) => {
-  const kieliversiot = getKieliversiot(values);
-
-  return errorBuilder
-    .validateArrayMinLength('kieliversiot', 1)
-    .validateTranslations('kuvaus.nimi', kieliversiot);
+const validateValintaperusteForm = values => {
+  return getErrorBuilderByFormConfig(getValintaperusteFormConfig(), values).getErrors();
 };
 
-const validateCommon = ({ errorBuilder, values }) => {
-  const kieliversiot = getKieliversiot(values);
-
-  return errorBuilder
-    .validateExistence('hakutapa')
-    .validateExistence('kohdejoukko')
-    .validateArrayMinLength('valintatavat', 1, {
-      isFieldArray: true,
-    })
-    .validateArray('valintatavat', eb => {
-      return eb
-        .validateExistence('tapa')
-        .validateTranslations('nimi', kieliversiot);
-    });
-};
-
-const validate = values => {
-  const { tila } = values;
-
-  let errorBuilder = createErrorBuilder(values);
-
-  errorBuilder = validateEssentials({ errorBuilder, values });
-
-  if (tila === JULKAISUTILA.TALLENNETTU) {
-    return errorBuilder.getErrors();
-  }
-
-  errorBuilder = validateCommon({ errorBuilder, values });
-
-  return errorBuilder.getErrors();
-};
-
-export default validate;
+export default validateValintaperusteForm;

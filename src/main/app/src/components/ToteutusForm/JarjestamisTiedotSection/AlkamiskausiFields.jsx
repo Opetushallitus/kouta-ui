1c42bd21ffd0651e9b3779c2f621ed3db0a65d47
Jussi Jartamo <jussi.jartamo@gofore.com>
2020-01-09 13:14:12 +0200

    KTO-287 koulutuksen alkamiskausi added

--- src/main/app/src/components/ToteutusForm/JarjestamisTiedotSection/AlkamiskausiFields.jsx
+++ src/main/app/src/components/ToteutusForm/JarjestamisTiedotSection/AlkamiskausiFields.jsx
@@ -3,14 +3,28 @@ import { Field } from 'redux-form';
 
 import { getTestIdProps } from '../../../utils';
 import useTranslation from '../../useTranslation';
-import { FormFieldDatePickerInput } from '../../formFields';
+import { FormFieldDatePickerInput, FormFieldRadioGroup } from '../../formFields';
 import Box from '../../Box';
+import useKoodistoOptions from '../../useKoodistoOptions';
 
 const AlkamiskausiFields = ({ name }) => {
   const { t } = useTranslation();
+  const { options } = useKoodistoOptions({ koodisto: 'kausi' });
 
   return (
     <Box display="flex" m={-1}>
+      <Box
+      flexGrow="1"
+      p={1}
+      {...getTestIdProps('koulutuksenAlkamiskausi')}
+      >
+        <Field
+            name={`${name}.kausi`}
+            component={FormFieldRadioGroup}
+            label={t('yleiset.kausi')}
+            options={options}
+        />
+      </Box>
       <Box
         flexGrow="1"
         p={1}

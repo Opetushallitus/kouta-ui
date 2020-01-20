e004fb4ff6df963ce7338f8336aa052e96b28d24
Jussi Jartamo <jussi.jartamo@gofore.com>
2020-01-20 11:41:15 +0200

    code cleanup

--- upgrade_image.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-git clone --single-branch --branch dynamo-switch https://github.com/Opetushallitus/ci-tools.git
-source ci-tools/common/setup-tools.sh
-export ARTIFACT_NAME="kouta-ui"
-export BASE_IMAGE="baseimage-fatjar:master"
-
-./ci-tools/build/upload-image.sh $ARTIFACT_NAME --dynamo-write

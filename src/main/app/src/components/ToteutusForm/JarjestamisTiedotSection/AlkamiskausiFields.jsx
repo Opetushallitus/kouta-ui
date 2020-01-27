ccf1337b6b90f4f14f95eac27a94d92c760d4c2b
Jussi Jartamo <jussi.jartamo@gofore.com>
2020-01-27 15:50:11 +0200

    code cleanup

--- src/main/app/src/components/ToteutusForm/JarjestamisTiedotSection/AlkamiskausiFields.jsx
+++ src/main/app/src/components/ToteutusForm/JarjestamisTiedotSection/AlkamiskausiFields.jsx
@@ -14,9 +14,67 @@ import useKoodistoOptions from '../../useKoodistoOptions';
 import Spacing from "../../Spacing";
 import useFieldValue from "../../useFieldValue";
 
+const TarkkaAlkamiskausiFields = ({name}) => {
+  const {t} = useTranslation();
+  return <Spacing>
+    <Box
+      flexGrow="1"
+      p={1}
+      {...getTestIdProps('koulutuksenAlkamispaivamaara')}
+    >
+      <Field
+        name={`${name}.koulutuksenAlkamispaivamaara`}
+        component={FormFieldDatePickerInput}
+        label={t('yleiset.alkaa')}
+      />
+    </Box>
+    <Box
+      flexGrow="1"
+      p={1}
+      {...getTestIdProps('koulutuksenPaattymispaivamaara')}
+    >
+      <Field
+        name={`${name}.koulutuksenPaattymispaivamaara`}
+        component={FormFieldDatePickerInput}
+        label={t('yleiset.paattyy')}
+      />
+    </Box>
+  </Spacing>
+};
+
+const KausiJaVuosiFields = ({name}) => {
+  const {options} = useKoodistoOptions({koodisto: 'kausi'});
+  const {t} = useTranslation();
+  return <Spacing>
+    <Box
+      flexGrow="1"
+      p={1}
+      {...getTestIdProps('koulutuksenAlkamiskausi')}
+    >
+      <Field
+        name={`${name}.koulutuksenAlkamiskausi`}
+        component={FormFieldRadioGroup}
+        label={t('toteutuslomake.koulutuksenAlkamiskausi')}
+        options={options}
+      />
+    </Box>
+    <Box
+      flexGrow="1"
+      p={1}
+      {...getTestIdProps('koulutuksenAlkamisvuosi')}
+    >
+      <Field
+        name={`${name}.koulutuksenAlkamisvuosi`}
+        component={FormFieldYearSelect}
+        isClearable
+        placeholder={t('toteutuslomake.koulutuksenAlkamisvuosi')}
+      />
+    </Box>
+  </Spacing>;
+};
+
 const AlkamiskausiFields = ({name}) => {
   const {t} = useTranslation();
-  const {options} = useKoodistoOptions({koodisto: 'kausi'});
   const koulutuksenTarkkaAlkamisaika = useFieldValue(`${name}.koulutuksenTarkkaAlkamisaika`);
   return (
     <Box display="flex" m={-1}>
@@ -33,55 +91,7 @@ const AlkamiskausiFields = ({name}) => {
           />
         </Box>
       </Spacing>
-      {koulutuksenTarkkaAlkamisaika ? <Spacing>
-          <Box
-            flexGrow="1"
-            p={1}
-            {...getTestIdProps('koulutuksenAlkamispaivamaara')}
-          >
-            <Field
-              name={`${name}.koulutuksenAlkamispaivamaara`}
-              component={FormFieldDatePickerInput}
-              label={t('yleiset.alkaa')}
-            />
-          </Box>
-          <Box
-            flexGrow="1"
-            p={1}
-            {...getTestIdProps('koulutuksenPaattymispaivamaara')}
-          >
-            <Field
-              name={`${name}.koulutuksenPaattymispaivamaara`}
-              component={FormFieldDatePickerInput}
-              label={t('yleiset.paattyy')}
-            />
-          </Box></Spacing> :
-        <Spacing>
-          <Box
-            flexGrow="1"
-            p={1}
-            {...getTestIdProps('koulutuksenAlkamiskausi')}
-          >
-            <Field
-              name={`${name}.koulutuksenAlkamiskausi`}
-              component={FormFieldRadioGroup}
-              label={t('toteutuslomake.koulutuksenAlkamiskausi')}
-              options={options}
-            />
-          </Box>
-          <Box
-            flexGrow="1"
-            p={1}
-            {...getTestIdProps('koulutuksenAlkamisvuosi')}
-          >
-            <Field
-              name={`${name}.koulutuksenAlkamisvuosi`}
-              component={FormFieldYearSelect}
-              isClearable
-              placeholder={t('toteutuslomake.koulutuksenAlkamisvuosi')}
-            />
-          </Box>
-        </Spacing>}
+      {koulutuksenTarkkaAlkamisaika ? <TarkkaAlkamiskausiFields name={name}/> : <KausiJaVuosiFields name={name}/>}
     </Box>
   );
 };

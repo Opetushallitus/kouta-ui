97e7017b7463ec82974e4833a49116abeb63908d
Petteri Tolonen <petteri.tolonen@cybercom.com>
2020-02-11 15:37:42 +0200

    Refactor hakuajat - Use shared HakuajatFields-component in ScheduleSection and HakuajatSection

--- src/main/app/src/components/HakuForm/ScheduleSection.jsx
+++ src/main/app/src/components/HakuForm/ScheduleSection.jsx
@@ -1,65 +1,17 @@
 import React from 'react';
 import { Field, FieldArray } from 'redux-form';
-
-import Button from '../Button';
 import useKoodistoOptions from '../useKoodistoOptions';
 import useTranslation from '../useTranslation';
-import Flex, { FlexItem } from '../Flex';
 import { getTestIdProps } from '../../utils';
 import DividerHeading from '../DividerHeading';
 import Box from '../Box';
+import HakuajatFields from '../HakuajatFields';
 
 import {
   FormFieldDateTimeInput,
   FormFieldRadioGroup,
   FormFieldYearSelect,
 } from '../formFields';
-
-const renderHakuajatFields = ({ fields, t }) => {
-  return (
-    <>
-      {fields.map((hakuaika, index) => (
-        <Flex key={index} mb={2} alignCenter>
-          <FlexItem grow={1} paddingRight={2} {...getTestIdProps('alkaa')}>
-            <Field
-              name={`${hakuaika}.alkaa`}
-              component={FormFieldDateTimeInput}
-              label={t('yleiset.alkaa')}
-              helperText={t('yleiset.paivamaaraJaKellonaika')}
-            />
-          </FlexItem>
-          <FlexItem grow={1} paddingLeft={2} {...getTestIdProps('paattyy')}>
-            <Field
-              name={`${hakuaika}.paattyy`}
-              component={FormFieldDateTimeInput}
-              label={t('yleiset.paattyy')}
-              helperText={t('yleiset.paivamaaraJaKellonaika')}
-            />
-          </FlexItem>
-          <FlexItem grow={0} paddingLeft={2}>
-            <Button
-              onClick={() => fields.remove(index)}
-              variant="outlined"
-              color="secondary"
-            >
-              {t('yleiset.poista')}
-            </Button>
-          </FlexItem>
-        </Flex>
-      ))}
-      <Button
-        type="button"
-        variant="outlined"
-        onClick={() => {
-          fields.push({});
-        }}
-        {...getTestIdProps('lisaaButton')}
-      >
-        {t('yleiset.lisaaHakuaika')}
-      </Button>
-    </>
-  );
-};
 
 const ScheduleSection = ({ isOphVirkailija, isYhteishaku, name }) => {
   const { t } = useTranslation();
@@ -71,7 +23,7 @@ const ScheduleSection = ({ isOphVirkailija, isYhteishaku, name }) => {
         <DividerHeading>{t('hakulomake.hakuaika')}</DividerHeading>
         <FieldArray
           name={`${name}.hakuaika`}
-          component={renderHakuajatFields}
+          component={HakuajatFields}
           t={t}
         />
       </Box>
@@ -83,7 +35,7 @@ const ScheduleSection = ({ isOphVirkailija, isYhteishaku, name }) => {
           </DividerHeading>
           <FieldArray
             name={`${name}.aikataulu`}
-            component={renderHakuajatFields}
+            component={HakuajatFields}
             t={t}
           />
         </Box>
--- src/main/app/src/components/HakukohdeForm/HakuajatSection.jsx
+++ src/main/app/src/components/HakukohdeForm/HakuajatSection.jsx
@@ -3,12 +3,11 @@ import { Field, FieldArray } from 'redux-form';
 
 import Typography from '../Typography';
 import Spacing from '../Spacing';
-import Button from '../Button';
-import { isArray, formatKoutaDateString, getTestIdProps } from '../../utils';
+import { isArray, formatKoutaDateString } from '../../utils';
 import useTranslation from '../useTranslation';
-import Flex, { FlexItem } from '../Flex';
-import { FormFieldDateTimeInput, FormFieldCheckbox } from '../formFields';
+import { FormFieldCheckbox } from '../formFields';
 import useFieldValue from '../useFieldValue';
+import HakuajatFields from '../HakuajatFields';
 
 const HakuaikaInterval = ({ haku }) => {
   const dateFormat = 'DD.MM.YYYY HH:mm';
@@ -16,12 +15,13 @@ const HakuaikaInterval = ({ haku }) => {
 
   const hakuajat = useMemo(
     () =>
-      (haku && isArray(haku.hakuajat) ? haku.hakuajat : []).map(
-        ({ alkaa, paattyy }) => [
-          formatKoutaDateString(alkaa, dateFormat),
-          formatKoutaDateString(paattyy, dateFormat),
-        ],
-      ),
+      (haku && isArray(haku.hakuajat)
+        ? haku.hakuajat
+        : []
+      ).map(({ alkaa, paattyy }) => [
+        formatKoutaDateString(alkaa, dateFormat),
+        formatKoutaDateString(paattyy, dateFormat),
+      ]),
     [haku],
   );
 
@@ -40,60 +40,12 @@ const HakuaikaInterval = ({ haku }) => {
   );
 };
 
-const renderHakuajatFields = ({ fields, t }) => (
-  <>
-    {fields.map((hakuaika, index) => (
-      <Flex key={index} marginBottom={2} alignCenter>
-        <FlexItem grow={1} paddingRight={2} {...getTestIdProps('alkaa')}>
-          <Field
-            name={`${hakuaika}.alkaa`}
-            component={FormFieldDateTimeInput}
-            label={t('yleiset.alkaa')}
-            helperText={t('yleiset.paivamaaraJaKellonaika')}
-          />
-        </FlexItem>
-        <FlexItem grow={1} paddingLeft={2} {...getTestIdProps('paattyy')}>
-          <Field
-            name={`${hakuaika}.paattyy`}
-            component={FormFieldDateTimeInput}
-            label={t('yleiset.paattyy')}
-            helperText={t('yleiset.paivamaaraJaKellonaika')}
-          />
-        </FlexItem>
-        <FlexItem grow={0} paddingLeft={2}>
-          <Button
-            onClick={() => fields.remove(index)}
-            variant="outlined"
-            color="secondary"
-          >
-            {t('yleiset.poista')}
-          </Button>
-        </FlexItem>
-      </Flex>
-    ))}
-    <Button
-      type="button"
-      variant="outlined"
-      onClick={() => {
-        fields.push({});
-      }}
-      {...getTestIdProps('lisaaButton')}
-    >
-      {t('yleiset.lisaaHakuaika')}
-    </Button>
-  </>
-);
-
 const CustomHakuaika = ({ name }) => {
   const { t } = useTranslation();
 
   return (
     <Spacing marginTop={2}>
-      <FieldArray
-        name={`${name}.hakuajat`}
-        component={renderHakuajatFields}
-        t={t}
-      />
+      <FieldArray name={`${name}.hakuajat`} component={HakuajatFields} t={t} />
     </Spacing>
   );
 };

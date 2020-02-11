b2263990ae83da0b9142587e01c0816ae18c5b2a
Petteri Tolonen <petteri.tolonen@cybercom.com>
2020-02-11 11:33:33 +0200

    Clean up formFields/utils - No need to add '__formFieldError__' class anymore - Use isArray from lodash - Remove unneeded variables

--- src/main/app/src/components/formFields/utils.jsx
+++ src/main/app/src/components/formFields/utils.jsx
@@ -1,34 +1,31 @@
 import React, { createElement } from 'react';
+import { isArray, isNil } from 'lodash';
 import FormControl from '../FormControl';
 import useTranslation from '../useTranslation';
-import { isArray } from '../../utils';
 
 export const createComponent = (Component, mapProps) => {
   const InputComponent = props => {
-    const { t } = useTranslation();
+    const {
+      disabled,
+      label,
+      helperText,
+      meta: { error },
+    } = props;
 
-    const { disabled, label, helperText, meta } = props;
-    const hasError = !!meta.error;
-    const showError = hasError;
+    const { t } = useTranslation();
     const children = createElement(Component, mapProps(props));
 
     return (
-      <div className={showError ? '__formFieldError__' : ''}>
-        <FormControl
-          error={showError}
-          helperText={
-            showError
-              ? isArray(meta.error)
-                ? t(...meta.error)
-                : t(meta.error)
-              : helperText
-          }
-          label={label}
-          disabled={disabled}
-        >
-          {children}
-        </FormControl>
-      </div>
+      <FormControl
+        error={!isNil(error)}
+        helperText={
+          error ? (isArray(error) ? t(...error) : t(error)) : helperText
+        }
+        label={label}
+        disabled={disabled}
+      >
+        {children}
+      </FormControl>
     );
   };
 

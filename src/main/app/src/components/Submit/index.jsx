79c36cd73eaef0a8cb276d7e5cd2d35077843c08
Jussi Jartamo <jussi.jartamo@gofore.com>
2020-02-14 13:34:00 +0200

    format submit

--- src/main/app/src/components/Submit/index.jsx
+++ src/main/app/src/components/Submit/index.jsx
@@ -2,21 +2,25 @@ import React, { useState } from 'react';
 
 import Button from '../Button';
 
-export const Submit = ({ disabled = false,
-                         title,
-                         children,
-                         onClick,
-                       ...props}) => {
+export const Submit = ({
+  disabled = false,
+  title,
+  children,
+  onClick,
+  ...props
+}) => {
   const [wait, setWait] = useState(null);
   const release = () => setWait(null);
-  return <Button
-    disabled={disabled || wait}
-    onClick={() => setWait(true) || onClick().then(release,release)}
-    title={title}
-    {...props}
-  >
-    {children}
-  </Button>;
+  return (
+    <Button
+      disabled={disabled || wait}
+      onClick={() => setWait(true) || onClick().then(release, release)}
+      title={title}
+      {...props}
+    >
+      {children}
+    </Button>
+  );
 };
 
 export default Submit;

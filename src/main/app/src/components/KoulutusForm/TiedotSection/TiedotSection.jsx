860e755ff4411174dba44e9b7ce0474a24c5d86b
Petteri Tolonen <petteri.tolonen@cybercom.com>
2020-02-19 10:28:26 +0200

    KTO-588: Copy KOMO 'koulutus' field value to 'nimi on change - When 'koulutus' changed, copy it's value to all 'nimi' field language versions - Show right language versions for 'koulutus' in language tabs

--- src/main/app/src/components/KoulutusForm/TiedotSection/TiedotSection.jsx
+++ src/main/app/src/components/KoulutusForm/TiedotSection/TiedotSection.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
-import { Field } from 'redux-form';
+import React, { useEffect, useContext, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { Field, change } from 'redux-form';
+import { get, each, find, toLower } from 'lodash';
 
 import FormConfigField from '../../FormConfigField';
 import KoulutuksenTiedotSection from './KoulutuksenTiedotSection';
@@ -11,9 +13,61 @@ import KoulutusalatField from './KoulutusalatField';
 import useTranslation from '../../useTranslation';
 import { FormFieldInput } from '../../formFields';
 import { getTestIdProps } from '../../../utils';
+import FormNameContext from '../../FormNameContext';
+import useKoodi from '../../useKoodi';
+
+const useFormName = () => useContext(FormNameContext);
+
+const useLocalizedKoulutus = ({ fieldName, language, koulutusValue }) => {
+  const dispatch = useDispatch();
+  const formName = useFormName();
+  const [changedKoulutus, setChangedKoulutus] = useState(null);
+  const koulutusKoodi = useKoodi(get(koulutusValue, 'value'));
+  const koodi = get(koulutusKoodi, 'koodi');
+
+  // When language changes, change the selected 'koulutus' label accordingly
+  useEffect(() => {
+    if (koodi) {
+      const { metadata } = koodi;
+      const localizedNimi = get(
+        find(metadata, ({ kieli }) => toLower(kieli) === language),
+        'nimi',
+      );
+      if (localizedNimi) {
+        dispatch(
+          change(formName, `${fieldName}.koulutus.label`, localizedNimi),
+        );
+      }
+    }
+  }, [language, koodi, dispatch, formName, fieldName]);
+
+  useEffect(() => {
+    if (koulutusValue) {
+      setChangedKoulutus(koulutusValue.value);
+    }
+  }, [koulutusValue, language]);
+
+  // When koulutus field has changed to a defined value and got its 'koodi'
+  // change the language versioned 'nimi' fields accordingly.
+  useEffect(() => {
+    if (changedKoulutus && koodi) {
+      each(get(koodi, 'metadata'), ({ kieli, nimi }) => {
+        const lang = toLower(kieli);
+        dispatch(change(formName, `${fieldName}.nimi.${lang}`, nimi));
+      });
+      setChangedKoulutus(null);
+    }
+  }, [changedKoulutus, dispatch, fieldName, formName, koodi, language]);
+};
 
 const TiedotSection = ({ language, koulutustyyppi, koulutuskoodi, name }) => {
   const { t } = useTranslation();
+
+  useLocalizedKoulutus({
+    fieldName: name,
+    koulutusValue: koulutuskoodi,
+    language,
+  });
 
   return (
     <Box mb={-2}>
@@ -46,6 +100,7 @@ const TiedotSection = ({ language, koulutustyyppi, koulutuskoodi, name }) => {
           <KoulutusField
             name={`${name}.koulutus`}
             koulutustyyppi={koulutustyyppi}
+            language={language}
           />
         </Box>
       </FormConfigField>

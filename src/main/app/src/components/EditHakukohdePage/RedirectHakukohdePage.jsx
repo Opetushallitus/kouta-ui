b6e4825a5731a18b5605a108cafaeb32e278ab4a
Jussi Jartamo <jussi.jartamo@gofore.com>
2020-02-25 14:41:34 +0200

    Redirect refactored

--- src/main/app/src/components/EditHakukohdePage/RedirectHakukohdePage.jsx
+++ src/main/app/src/components/EditHakukohdePage/RedirectHakukohdePage.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
-import useApiAsync from "../useApiAsync";
-import getHakukohdeByOid from "../../utils/kouta/getHakukohdeByOid";
+import useApiAsync from '../useApiAsync';
+import getHakukohdeByOid from '../../utils/kouta/getHakukohdeByOid';
+import useOrganisaatioHierarkia from '../useOrganisaatioHierarkia';
+import useAuthorizedUser from '../useAuthorizedUser';
+import getUserRoles from '../../utils/getUserRoles';
+import getUserOrganisaatiotWithRoles from '../../utils/getUserOrganisaatiotWithRoles';
+import uniqBy from 'lodash/uniqBy';
+import first from 'lodash/first';
+import Spin from '@opetushallitus/virkailija-ui-components/Spin';
+import Flex, { FlexItem } from '../Flex';
+
+const Loader = () => (
+  <Flex alignCenter column>
+    <FlexItem marginBottom={1}>
+      <Spin />
+    </FlexItem>
+  </Flex>
+);
+
+const isParent = parentOid => org => org.parentOidPath.includes(parentOid);
+const isChild = childOid => org =>
+  org.oid === childOid || first(org.children.filter(isChild(childOid)));
 
 const getData = async ({ httpClient, apiUrls, oid: hakukohdeOid }) => {
   const hakukohde = await getHakukohdeByOid({
@@ -9,26 +29,59 @@ const getData = async ({ httpClient, apiUrls, oid: hakukohdeOid }) => {
     apiUrls,
     oid: hakukohdeOid,
   });
-  return {hakukohde}
+  return { hakukohde };
+};
+
+const Hierarkia = ({ hakukohde, hierarkia }) => {
+  const user = useAuthorizedUser();
+  const roles = getUserRoles(user);
+  const orgs = uniqBy(getUserOrganisaatiotWithRoles(user, roles));
+  const firstChildOrganisation = first(
+    orgs.filter(org => hierarkia.filter(isChild(org))),
+  );
+  const firstParentOrganisation = first(
+    orgs.filter(org => hierarkia.filter(isParent(org))),
+  );
+
+  const preferredOrganisation =
+    firstChildOrganisation ||
+    firstParentOrganisation ||
+    hakukohde.organisaatioOid;
+
+  return (
+    <Redirect
+      to={`/organisaatio/${preferredOrganisation}/hakukohde/${hakukohde.oid}/muokkaus`}
+    />
+  );
+};
+
+const Hakukohde = ({ hakukohde }) => {
+  const { hierarkia } = useOrganisaatioHierarkia(hakukohde.organisaatioOid, {
+    skipParents: false,
+  });
+  return hierarkia ? (
+    <Hierarkia hakukohde={hakukohde} hierarkia={hierarkia} />
+  ) : (
+    <Loader />
+  );
 };
 
 const RedirectHakukohdeFooter = props => {
   const {
     match: {
-      params: {oid},
-    }
+      params: { oid },
+    },
   } = props;
 
   const watch = JSON.stringify([oid]);
 
-  const {
-    data: {hakukohde} = {},
-  } = useApiAsync({
+  const { data: { hakukohde } = {} } = useApiAsync({
     promiseFn: getData,
     oid,
     watch,
   });
-  return hakukohde ? <Redirect to={`/organisaatio/${hakukohde.organisaatioOid}/hakukohde/${oid}/muokkaus`} /> : null;
+
+  return hakukohde ? <Hakukohde hakukohde={hakukohde} /> : <Loader />;
 };
 
 export default RedirectHakukohdeFooter;

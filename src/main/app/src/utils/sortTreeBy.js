0138295e17841dc117eab1c2467a9fc740d6aa06
Petteri Tolonen <petteri.tolonen@cybercom.com>
2020-03-27 09:28:42 +0200

    Sort items in OrganisaatioHierarkiaTreeSelect

---
+++ src/main/app/src/utils/sortTreeBy.js
@@ -0,0 +1,12 @@
+import { sortBy } from 'lodash';
+
+const sortTreeBy = (tree = [], sortOpt) => {
+  return sortBy(
+    tree.map(item => ({
+      ...item,
+      children: sortTreeBy(item.children, sortOpt),
+    })),
+    sortOpt,
+  );
+};
+export default sortTreeBy;

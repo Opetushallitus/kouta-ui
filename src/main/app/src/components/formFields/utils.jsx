f5a8445a71842c1ca7a9bc25dae5b93b3f90feae
Petteri Tolonen <petteri.tolonen@cybercom.com>
2020-04-14 11:23:49 +0300

    KTO-404: Show "*" after label in form fields with 'required' config attribute.

--- src/main/app/src/components/formFields/utils.jsx
+++ src/main/app/src/components/formFields/utils.jsx
@@ -1,27 +1,50 @@
-import React, { createElement } from 'react';
-import { isArray, isNil } from 'lodash';
+import React, { createElement, useContext } from 'react';
+import _ from 'lodash';
 import { useTranslation } from 'react-i18next';
-import FormControl from '../FormControl';
+import FormConfigSectionContext from '#/src/components/FormConfigSectionContext';
+import FormControl from '#/src/components/FormControl';
+import useFormConfig from '#/src/components/useFormConfig';
+
+const withoutLang = name => {
+  const parts = _.split(name, '.');
+  if (['fi', 'sv', 'en'].includes(_.last(parts))) {
+    return _.dropRight(parts, 1).join('.');
+  }
+  return name;
+};
 
 export const createComponent = (Component, mapProps) => {
   const InputComponent = props => {
     const {
       disabled,
-      label,
+      label = '',
       helperText,
-      meta: { error },
+      meta,
+      input: { name },
     } = props;
 
+    const { error } = meta;
     const { t } = useTranslation();
     const children = createElement(Component, mapProps(props));
 
+    const formConfig = useFormConfig();
+    const section = useContext(FormConfigSectionContext);
+
+    const sectionFields = _.get(formConfig, `sections[${section}].fields`);
+
+    const fieldConfig = _.find(
+      sectionFields,
+      f => f.formFieldName === withoutLang(name),
+    );
+    const required = _.get(fieldConfig, 'required');
+
     return (
       <FormControl
-        error={!isNil(error)}
+        error={!_.isNil(error)}
         helperText={
-          error ? (isArray(error) ? t(...error) : t(error)) : helperText
+          error ? (_.isArray(error) ? t(...error) : t(error)) : helperText
         }
-        label={label}
+        label={`${label}${required ? ' *' : ''}`}
         disabled={disabled}
       >
         {children}

d6298e5432d07cb668d27a658c0054951002a8d4
Petteri Tolonen <petteri.tolonen@cybercom.com>
2020-04-17 15:06:10 +0300

    Add useful cypress commands - overwrite cy.type() to use default params - Add cy.paste() for faster text input

--- src/main/app/cypress/support/commands.js
+++ src/main/app/cypress/support/commands.js
@@ -4,6 +4,25 @@ Cypress.Commands.add('getByTestId', (testId, options = {}) => {
   return cy.get(`[data-testid="${testId}"]`, options);
 });
 
+Cypress.Commands.add(
+  'paste',
+  {
+    prevSubject: true,
+  },
+  (subject, text, options = { force: true }) =>
+    cy
+      .get(subject)
+      .click()
+      .invoke('val', text)
+      .trigger('change', options),
+);
+
+Cypress.Commands.overwrite(
+  'type',
+  (originalFn, subject, text = '', options = {}) =>
+    originalFn(subject, text, { force: true, delay: 0, ...options }),
+);
+
 // Workaround for broken cypress snapshot updates caused by this change:
 // https://github.com/cypress-io/cypress/issues/2466
 // TODO: Remove after @cypress/snapshot gets this fixed

18c4a74f5a429791d04d7ab97637924f55acdf9b
Petteri Tolonen <petteri.tolonen@cybercom.com>
2020-05-08 10:22:44 +0300

    KTO-948: Improve local dev proxy conf to ease proxying to local kouta-backend.

--- src/main/app/src/setupProxy.js
+++ src/main/app/src/setupProxy.js
@@ -1,31 +1,51 @@
 // This file is used by react-scripts to customize webpack-dev-server proxy
 // https://create-react-app.dev/docs/proxying-api-requests-in-development
 const { createProxyMiddleware } = require('http-proxy-middleware');
-
 const {
   DEV_VIRKAILIJA_URL,
   REACT_APP_DEV_SERVER_URL,
   DISABLE_LOCAL_PROXY,
+  KOUTA_BACKEND_URL,
 } = process.env;
 
-const devProxyMiddleware = createProxyMiddleware({
-  autoRewrite: true,
-  headers: {
-    'Access-Control-Allow-Origin': DEV_VIRKAILIJA_URL,
-  },
-  changeOrigin: true,
-  cookieDomainRewrite: REACT_APP_DEV_SERVER_URL,
-  secure: false,
-  target: DEV_VIRKAILIJA_URL,
-});
+const devServerURL = new URL(REACT_APP_DEV_SERVER_URL);
+
+const createKoutaProxyMiddleware = targetUrl =>
+  createProxyMiddleware({
+    autoRewrite: true,
+    headers: {
+      'Access-Control-Allow-Origin': targetUrl,
+    },
+    changeOrigin: true,
+    cookieDomainRewrite: 'localhost',
+    secure: false,
+    target: targetUrl,
+    debug: true,
+  });
+
+const devProxyMiddleware = createKoutaProxyMiddleware(DEV_VIRKAILIJA_URL);
+
+const koutaBackendProxyMiddleware =
+  KOUTA_BACKEND_URL && createKoutaProxyMiddleware(KOUTA_BACKEND_URL);
 
 module.exports = function (app) {
   app.use('*', function (req, res, next) {
-    return ['/', '/kouta'].includes(req.originalUrl) ||
+    if (
+      ['/', '/kouta'].includes(req.originalUrl) ||
       req.originalUrl.startsWith('/kouta/')
-      ? next()
-      : DISABLE_LOCAL_PROXY
-      ? res.sendStatus(404)
-      : devProxyMiddleware(req, res, next);
+    ) {
+      next();
+    } else {
+      if (DISABLE_LOCAL_PROXY) {
+        res.sendStatus(404);
+      } else if (
+        KOUTA_BACKEND_URL &&
+        req.originalUrl.startsWith('/kouta-backend')
+      ) {
+        koutaBackendProxyMiddleware(req, res, next);
+      } else {
+        devProxyMiddleware(req, res, next);
+      }
+    }
   });
 };

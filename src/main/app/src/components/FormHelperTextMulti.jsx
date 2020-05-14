d227f1251b1dc76ec327f7ffcdda62dfe2f6d1cf
Petteri Tolonen <petteri.tolonen@cybercom.com>
2020-05-14 06:44:49 +0300

    Refactor FormHelperText wrapper to a separate file

---
+++ src/main/app/src/components/FormHelperTextMulti.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import _ from 'lodash';
+import { useTranslation } from 'react-i18next';
+import FormHelperText from '@opetushallitus/virkailija-ui-components/FormHelperText';
+import { otherwise } from '#/src/utils';
+
+/**
+ * Wrapper for FormHelperText able to display one helpertext and multiple error messages
+ */
+export const FormHelperTextMulti = ({ errorMessage = [], helperText = '' }) => {
+  const { t } = useTranslation();
+  const errors = errorMessage ? _.castArray(errorMessage) : [];
+  return (
+    <>
+      {helperText && <FormHelperText>{helperText}</FormHelperText>}
+      {errors &&
+        errors.map((e, index) => (
+          <FormHelperText key={index} error>
+            {_.cond([
+              [_.isFunction, f => f(t)],
+              [otherwise, t],
+            ])(e)}
+          </FormHelperText>
+        ))}
+    </>
+  );
+};
+
+export default FormHelperTextMulti;
--- src/main/app/src/components/formFields/utils.jsx
+++ src/main/app/src/components/formFields/utils.jsx
@@ -1,26 +1,22 @@
 import React, { createElement } from 'react';
 import _ from 'lodash';
-import { useTranslation } from 'react-i18next';
 import FormControl from '#/src/components/FormControl';
-import FormHelperText from '#/src/components/FormHelperText';
 import {
   useFieldConfig,
   useFieldIsRequired,
 } from '#/src/hooks/fieldConfigHooks';
-import { otherwise } from '#/src/utils';
+import FormHelperTextMulti from '#/src/components/FormHelperTextMulti';
 
 export const createComponent = (Component, mapProps) => {
   const InputComponent = props => {
     const {
       disabled,
       label = '',
-      helperText: helperTextProp,
+      helperText,
       meta,
       input: { name },
       configurable = true,
     } = props;
-
-    const { t } = useTranslation();
 
     const { error } = meta;
     const isError = !_.isNil(error);
@@ -29,27 +25,12 @@ export const createComponent = (Component, mapProps) => {
     const fieldConfig = useFieldConfig(name);
     const required = useFieldIsRequired(fieldConfig);
 
-    const errors = _.isArray(error) ? error : [error];
-
-    const HelperText = () => (
-      <>
-        {helperTextProp && <FormHelperText>{helperTextProp}</FormHelperText>}
-        {isError &&
-          errors.map(e => (
-            <FormHelperText key={_.uniqueId()} error>
-              {_.cond([
-                [_.isFunction, f => f(t)],
-                [otherwise, t],
-              ])(e)}
-            </FormHelperText>
-          ))}
-      </>
-    );
-
     return fieldConfig || !configurable ? (
       <FormControl
         error={isError}
-        helperText={<HelperText />}
+        helperText={
+          <FormHelperTextMulti errorMessage={error} helperText={helperText} />
+        }
         label={`${label}${required ? ' *' : ''}`}
         disabled={disabled}
       >

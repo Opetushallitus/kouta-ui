3a7da91c21e8bc4cbf1c2d7d5110b5416ee85b4d
Petteri Tolonen <petteri.tolonen@cybercom.com>
2020-06-02 13:57:34 +0300

    Simplify ReduxForm wrapper component - ReduxForm is not passed children as a function anywhere anymore

--- src/main/app/src/components/ReduxForm/index.jsx
+++ src/main/app/src/components/ReduxForm/index.jsx
@@ -1,18 +1,11 @@
 import React from 'react';
 import { reduxForm } from 'redux-form';
-
 import FormNameContext from '../FormNameContext';
 
-const ReduxForm = reduxForm({ initialValues: {} })(
-  ({ children, form, ...props }) => {
-    return (
-      <FormNameContext.Provider value={form}>
-        {React.isValidElement(children) || React.Children.count(children) > 1
-          ? children
-          : children({ form, ...props })}
-      </FormNameContext.Provider>
-    );
-  }
-);
+const ReduxForm = reduxForm({ initialValues: {} })(({ children, form }) => {
+  return (
+    <FormNameContext.Provider value={form}>{children}</FormNameContext.Provider>
+  );
+});
 
 export default ReduxForm;

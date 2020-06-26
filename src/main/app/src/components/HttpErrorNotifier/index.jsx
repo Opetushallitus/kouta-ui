2b6d290834938b18875df162bcdbf5b975ea1b91
Petteri Tolonen <petteri.tolonen@cybercom.com>
2020-06-26 13:04:36 +0300

    KTO-761: Show HTTP request error response body in toast as an accordion

--- src/main/app/src/components/HttpErrorNotifier/index.jsx
+++ src/main/app/src/components/HttpErrorNotifier/index.jsx
@@ -1,22 +1,25 @@
-import { useEffect, useContext, useMemo } from 'react';
-import { get, throttle } from 'lodash';
-
-import HttpContext from '../HttpContext';
+import { useEffect, useContext } from 'react';
 import { useTranslation } from 'react-i18next';
-import useToaster from '../useToaster';
+import _ from 'lodash/fp';
+
+import HttpContext from '#/src/components/HttpContext';
+import useToaster from '#/src/components/useToaster';
+import { otherwise } from '#/src/utils';
 
 const getToastOptions = (error, t) => {
-  const status = get(error, 'response.status');
+  const { response } = error;
+  const status = response?.status;
+  const data = response?.data;
 
-  let label = t('ilmoitukset.tuntematonVirhe');
-
-  if (status === 403) {
-    label = t('ilmoitukset.kayttooikeusVirhe');
-  }
+  const label = _.cond([
+    [_.equals(403), () => t('ilmoitukset.kayttooikeusVirhe')],
+    [otherwise, () => t('ilmoitukset.tuntematonVirhe')],
+  ])(status);
 
   return {
     status: 'danger',
     label,
+    error: status >= 400 && data,
   };
 };
 
@@ -42,7 +45,7 @@ export const HttpErrorNotifier = () => {
     return () => {
       httpClient.interceptors.response.eject(interceptor);
     };
-  }, [openToastThrottle, httpClient, t]);
+  }, [openToast, httpClient, t]);
 
   return null;
 };
--- src/main/app/src/components/Toaster/index.jsx
+++ src/main/app/src/components/Toaster/index.jsx
@@ -8,6 +8,8 @@ import Icon from '#/src/components/Icon';
 import Typography from '#/src/components/Typography';
 import Box from '#/src/components/Box';
 import useToaster from '#/src/components/useToaster';
+import AbstractCollapse from '#/src/components/AbstractCollapse';
+import { useTranslation } from 'react-i18next';
 
 const ToasterContainer = styled.div`
   display: flex;
@@ -16,11 +18,11 @@ const ToasterContainer = styled.div`
 
 const ToastContainer = styled.div`
   display: inline-flex;
-  align-items: center;
+  align-items: flex-start;
   box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.15);
   border-radius: ${getThemeProp('shape.borderRadius')};
   overflow: hidden;
-  max-width: 450px;
+  max-width: 800px;
   background-color: white;
   color: ${({ theme }) => theme.palette.text.primary};
   padding: 16px;
@@ -47,9 +49,20 @@ const ToastContainer = styled.div`
     `}
 `;
 
-const CloseIcon = styled(Icon).attrs({ type: 'close' })`
+const ToastWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+
+  ${({ last }) =>
+    !last &&
+    css`
+      margin-bottom: ${({ theme }) => theme.spacing.unit * 2}px;
+    `};
+`;
+
+const hoverHighlightStyle = css`
   color: inherit;
-  opacity: 0.6;
+  opacity: 0.7;
   font-size: 1.25rem;
   transition: opacity 0.25s;
   cursor: pointer;
@@ -58,6 +71,25 @@ const CloseIcon = styled(Icon).attrs({ type: 'close' })`
     opacity: 1;
   }
 `;
+
+const CloseIcon = styled(Icon).attrs({ type: 'close' })(hoverHighlightStyle);
+
+const ErrorContainer = styled.div`
+  display: flex;
+  ${hoverHighlightStyle}
+`;
+
+const ErrorToggle = ({ open, onToggle, ...props }) => {
+  const { t } = useTranslation();
+  return (
+    <ErrorContainer onClick={onToggle} {...props}>
+      <Icon type={open ? 'arrow_drop_down' : 'arrow_right'} />
+      <Typography color="inherit">
+        {open ? t('ilmoitukset.piilotaVirhe') : t('ilmoitukset.naytaVirhe')}
+      </Typography>
+    </ErrorContainer>
+  );
+};
 
 const iconByStatus = {
   success: 'check_circle_outline',
@@ -73,9 +105,7 @@ export const Toast = ({
 }) => (
   <ToastContainer status={status} {...props}>
     <Icon type={iconByStatus[status] || ''} mr={2} />
-    <Box>
-      <Typography color="inherit">{children}</Typography>
-    </Box>
+    <Typography color="inherit">{children}</Typography>
     {_.isFunction(onClose) ? (
       <Box ml={2}>
         <CloseIcon onClick={onClose} />
@@ -84,15 +114,10 @@ export const Toast = ({
   </ToastContainer>
 );
 
-const ToastWrapper = styled.div`
-  display: flex;
-  justify-content: flex-end;
-
-  ${({ last }) =>
-    !last &&
-    css`
-      margin-bottom: ${({ theme }) => theme.spacing.unit * 2}px;
-    `};
+const FormattedError = styled.pre`
+  font-size: 13px;
+  white-space: pre-wrap;
+  word-break: keep-all;
 `;
 
 export const Toaster = ({ ...props }) => {
@@ -126,7 +151,20 @@ export const Toaster = ({ ...props }) => {
               onMouseEnter={() => toastMouseEnter(item.key)}
               onMouseLeave={() => toastMouseLeave(item.key)}
             >
-              {item.label}
+              <Box mb={1}>{item.label}</Box>
+              {item.error && (
+                <AbstractCollapse
+                  content={
+                    <FormattedError>
+                      {JSON.stringify(item.error, null, 2)}
+                    </FormattedError>
+                  }
+                >
+                  {({ open, onToggle }) => (
+                    <ErrorToggle open={open} onToggle={onToggle} />
+                  )}
+                </AbstractCollapse>
+              )}
             </Toast>
           </ToastWrapper>
         )}

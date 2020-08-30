aaebd2ae28844e80439323d0a552c365d4fd5c8f
Petteri Tolonen <petteri.tolonen@cybercom.com>
2020-08-30 16:04:20 +0300

    Make Heading-component 100% wide by default

--- src/main/app/src/components/DividerHeading/index.tsx
+++ src/main/app/src/components/DividerHeading/index.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import Heading from '#/src/components/Heading';
 
-export const DividerHeading = ({
-  children = undefined,
-  variant = 'h6',
-  ...spacing
-}) => {
+export const DividerHeading = ({ children, variant = 'h6', ...spacing }) => {
   return (
     <Heading variant={variant} hasDivider={true} {...spacing}>
       {children}

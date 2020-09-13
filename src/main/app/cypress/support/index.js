d665f776bec80980c7a40ebff1744171618c4ef6
Petteri Tolonen <petteri.tolonen@cybercom.com>
2020-09-13 13:20:03 +0300

    Upgrade cypress to v5 and remove cypress-plugin-retries

--- src/main/app/cypress/support/index.js
+++ src/main/app/cypress/support/index.js
@@ -1,5 +1,4 @@
 import 'cypress-pipe';
-import 'cypress-plugin-retries';
 import 'cypress-plugin-snapshots/commands';
 import '@testing-library/cypress/add-commands';
 import { configure } from '@testing-library/cypress';

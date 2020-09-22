49479739fdadbcfa3caa983dadc4795df9f5b458
Petteri Tolonen <petteri.tolonen@cybercom.com>
2020-09-22 14:59:54 +0300

    Small refactoring of TekstiKuvausSection
    - Call getEPerusteKuvaus() from the component instead of getEPerusteById function

--- src/main/app/src/utils/ePeruste/getEPerusteById.ts
+++ src/main/app/src/utils/ePeruste/getEPerusteById.ts
@@ -1,12 +1,9 @@
-import getEPerusteKuvaus from '#/src/utils/ePeruste/getEPerusteKuvaus';
-
 export const getEPerusteById = async ({ httpClient, apiUrls, ePerusteId }) => {
   if (ePerusteId) {
     const { data } = await httpClient.get(
       apiUrls.url('eperusteet-service.peruste-by-id', ePerusteId)
     );
 
-    const kuvaus = getEPerusteKuvaus(data);
-    return data && { ...data, kuvaus };
+    return data;
   }
 };

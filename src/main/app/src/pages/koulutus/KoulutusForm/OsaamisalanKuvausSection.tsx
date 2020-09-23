bdac1aaded1f3080befef7bd23b2d2e3b1edc058
Petteri Tolonen <petteri.tolonen@cybercom.com>
2020-09-23 09:45:11 +0300

    KTO-826: Add OsaamisalanKuvausSection

---
+++ src/main/app/src/pages/koulutus/KoulutusForm/OsaamisalanKuvausSection.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { Box, Typography } from '#/src/components/virkailija';
+import { useFieldValue } from '#/src/hooks/form';
+import { useEPerusteOsaamisalaKuvaukset } from '#/src/utils/ePeruste/getOsaamisalakuvauksetByEPerusteId';
+import { getLanguageValue } from '#/src/utils/languageUtils';
+import StyledSectionHTML from '#/src/components/StyledSectionHTML';
+
+export const OsaamisalanKuvausSection = ({ disabled, language, name }) => {
+  const { t } = useTranslation();
+  const selectedEPeruste = useFieldValue('osaamisala.eperuste')?.value;
+  const selectedOsaamisala = useFieldValue('osaamisala.osaamisala')?.value;
+
+  const { data: osaamisalaKuvaukset } = useEPerusteOsaamisalaKuvaukset({
+    ePerusteId: selectedEPeruste,
+  });
+
+  const osaamisalaKuvaus =
+    osaamisalaKuvaukset?.[`osaamisala_${selectedOsaamisala}`]?.[0];
+
+  const nimi = getLanguageValue(osaamisalaKuvaus?.nimi, language);
+  const kuvaus = getLanguageValue(osaamisalaKuvaus?.teksti, language);
+
+  return (
+    <Box>
+      <Typography variant="h6">{nimi}</Typography>
+      <StyledSectionHTML html={kuvaus} />
+      <Typography variant="secondary" as="div" marginTop={1}>
+        ({t('yleiset.lahde')}: {t('yleiset.ePerusteet')})
+      </Typography>
+    </Box>
+  );
+};
+
+export default OsaamisalanKuvausSection;

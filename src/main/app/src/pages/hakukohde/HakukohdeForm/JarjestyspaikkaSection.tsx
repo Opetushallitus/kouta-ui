f567901b487e24fc14bc284c54c97ee13a4a2b4f
Petteri Tolonen <petteri.tolonen@cybercom.com>
2020-10-22 09:40:59 +0300

    KTO-908: Hakukohteen järjestyspaikan valinnan parempi esitysmuoto
    - Näytä kaikki toteutuksen luoja-organisaation toimipisteet hakukohteella, mutta disabloi ne, joita ei voi valita (ei valittu tarjoajiksi toteutuksella tai käyttäjällä ei ole hakukohteen päivitysoikeuksia organisaatioon)

--- src/main/app/src/pages/hakukohde/HakukohdeForm/JarjestyspaikkaSection.tsx
+++ src/main/app/src/pages/hakukohde/HakukohdeForm/JarjestyspaikkaSection.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Field } from 'redux-form';
 import _ from 'lodash/fp';
 import { useTranslation } from 'react-i18next';
+import styled, { css } from 'styled-components';
 
 import useOrganisaatioHierarkia from '#/src/hooks/useOrganisaatioHierarkia';
-import { FormFieldRadioGroup } from '#/src/components/formFields';
-import { ORGANISAATIOTYYPPI } from '#/src/constants';
+import {
+  createFormFieldComponent,
+  simpleMapProps,
+} from '#/src/components/formFields';
+import { CRUD_ROLES, ENTITY, ORGANISAATIOTYYPPI } from '#/src/constants';
 import organisaatioMatchesTyyppi from '#/src/utils/organisaatio/organisaatioMatchesTyyppi';
 import { getTestIdProps } from '#/src/utils';
 import { getFirstLanguageValue } from '#/src/utils/languageUtils';
 import useLanguage from '#/src/hooks/useLanguage';
-import { flatFilterHierarkia } from '#/src/utils/organisaatio/hierarkiaHelpers';
+import { Radio } from '#/src/components/virkailija';
+import { useGetCurrentUserHasRole } from '#/src/hooks/useCurrentUserHasRole';
+import { flattenHierarkia } from '#/src/utils/organisaatio/hierarkiaHelpers';
 
 const JARJESTYSPAIKATTOMAT_OPETUSTAVAT = [
   'opetuspaikkakk_2#1', // Verkko
@@ -18,32 +24,117 @@ const JARJESTYSPAIKATTOMAT_OPETUSTAVAT = [
   'opetuspaikkakk_5#1', // Itsenainen
 ];
 
+const Container = styled.div<{ isLast: boolean }>`
+  ${({ isLast }) =>
+    !isLast &&
+    css`
+      margin-bottom: 4px;
+    `}
+`;
+
+type RadioGroupChild = React.ReactElement<{
+  checked?: boolean;
+  value: string;
+  onChange?: (arg: any) => void;
+  disabled?: boolean;
+  error?: boolean;
+}>;
+
+// TODO: Use RadioGroup from virkailija-ui-components when it supports disabled-props for individual Radio-elements
+export const RadioGroup = ({
+  value,
+  onChange,
+  disabled = false,
+  error = false,
+  children: childrenProp,
+}) => {
+  const validChildren = React.Children.toArray(childrenProp).filter(c =>
+    React.isValidElement(c)
+  ) as RadioGroupChild[];
+
+  const childrenCount = React.Children.count(validChildren);
+
+  return (
+    <>
+      {validChildren.map((child, index) => {
+        const checked = value !== undefined && child?.props?.value === value;
+        const element = React.cloneElement(child, {
+          checked,
+          onChange,
+          disabled: disabled || child.props?.disabled,
+          error,
+        });
+
+        return (
+          <Container
+            key={_.uniqueId('RadioContainer_')}
+            isLast={index === childrenCount - 1}
+          >
+            {element}
+          </Container>
+        );
+      })}
+    </>
+  );
+};
+
+const JarjestyspaikkaRadioGroup = createFormFieldComponent(
+  ({ disabled, options, value, error, onChange }) => {
+    return (
+      <RadioGroup
+        value={value}
+        disabled={disabled}
+        error={error}
+        onChange={onChange}
+      >
+        {options.map(({ value, disabled, label }) => (
+          <Radio key={value} value={value} disabled={disabled}>
+            {label}
+          </Radio>
+        ))}
+      </RadioGroup>
+    );
+  },
+  simpleMapProps
+);
+
 const JarjestyspaikkaSection = ({
   tarjoajat,
   opetustapaKoodiUrit,
-  organisaatioOid,
+  toteutusOrganisaatioOid,
 }: {
   tarjoajat: Array<string>;
   opetustapaKoodiUrit: Array<string>;
-  organisaatioOid: string;
+  toteutusOrganisaatioOid: string;
 }) => {
   const { t } = useTranslation();
 
-  const { hierarkia = [] } = useOrganisaatioHierarkia(organisaatioOid);
+  const { hierarkia = [] } = useOrganisaatioHierarkia(toteutusOrganisaatioOid);
+
+  const getCanUpdate = useGetCurrentUserHasRole(
+    ENTITY.HAKUKOHDE,
+    CRUD_ROLES.UPDATE
+  );
 
   const language = useLanguage();
-  const jarjestyspaikkaOptions = flatFilterHierarkia(
-    hierarkia,
-    _.overEvery([
-      organisaatioMatchesTyyppi(ORGANISAATIOTYYPPI.TOIMIPISTE),
-      org =>
-        // Select intersection of organisaatio branches from selected organisaatio and tarjoajat
-        _.some(tarjoaja => _.includes(tarjoaja, org?.parentOidPath))(tarjoajat),
-    ])
-  ).map(({ oid, nimi }) => ({
-    value: oid,
-    label: getFirstLanguageValue(nimi, language),
-  }));
+  const jarjestyspaikkaOptions = useMemo(
+    () =>
+      _.pipe(
+        flattenHierarkia,
+        _.filter(organisaatioMatchesTyyppi(ORGANISAATIOTYYPPI.TOIMIPISTE)),
+        _.map(org => ({
+          value: org?.oid,
+          label: getFirstLanguageValue(org?.nimi, language),
+          disabled:
+            // Select intersection of organisaatio branches from selected organisaatio and tarjoajat
+            !_.some(tarjoaja => _.includes(tarjoaja, org?.parentOidPath))(
+              tarjoajat
+            ) || !getCanUpdate(org),
+        })),
+        _.sortBy('label')
+      )(hierarkia),
+    [getCanUpdate, hierarkia, language, tarjoajat]
+  );
 
   const jarjestyspaikkaOidRequired = _.difference(opetustapaKoodiUrit)(
     JARJESTYSPAIKATTOMAT_OPETUSTAVAT
@@ -53,7 +144,7 @@ const JarjestyspaikkaSection = ({
     <div {...getTestIdProps('jarjestyspaikkaOidSelection')}>
       <Field
         label={t('hakukohdelomake.valitseJarjestyspaikka')}
-        component={FormFieldRadioGroup}
+        component={JarjestyspaikkaRadioGroup}
         options={jarjestyspaikkaOptions}
         name={`jarjestyspaikkaOid`}
       />

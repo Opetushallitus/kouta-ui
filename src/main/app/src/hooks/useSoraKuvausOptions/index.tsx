373cd217de21e2cd118cb1df4504820485b847b4
Vesa Ruuskanen <vesa.ruuskanen@gofore.com>
2020-11-10 15:37:47 +0200

    KTO-955: Vaihdettu käyttämään useEntityOptionsHookia

--- src/main/app/src/hooks/useSoraKuvausOptions/index.tsx
+++ src/main/app/src/hooks/useSoraKuvausOptions/index.tsx
@@ -1,26 +1,9 @@
-import { useMemo } from 'react';
-import { isArray } from 'lodash';
 import useSoraKuvaukset from '#/src/hooks/useSoraKuvaukset';
-import useLanguage from '#/src/hooks/useLanguage';
-import { getFirstLanguageValue } from '#/src/utils/languageUtils';
-import { useTranslation } from 'react-i18next';
-import { getJulkaisutilaTranslationKey } from '#/src/constants';
+import useEntityOptions from '#/src/hooks/useEntityOptionsHook';
 
 export const useSoraKuvausOptions = args => {
   const { soraKuvaukset, ...rest } = useSoraKuvaukset(args);
-  const language = useLanguage();
-  const { t } = useTranslation();
-
-  const options = useMemo(() => {
-    return isArray(soraKuvaukset)
-      ? soraKuvaukset.map(({ id, nimi, tila }) => ({
-          value: id,
-          label:
-            getFirstLanguageValue(nimi, language) +
-            ` (${t(getJulkaisutilaTranslationKey(tila))})`,
-        }))
-      : [];
-  }, [soraKuvaukset, language, t]);
+  const options = useEntityOptions(soraKuvaukset);
 
   return { options, ...rest };
 };

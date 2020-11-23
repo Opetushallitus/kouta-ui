3ff66c41307768c1a37266ce5bbd4adba3f8694c
Petteri Tolonen <petteri.tolonen@cybercom.com>
2020-11-23 15:36:58 +0200

    Refactor useKoodistoOptions and add useKoodistoDataOptions
    - useKoodistoDataOptions can be used to get select options when you already got the koodisto data by other means

---
+++ src/main/app/src/hooks/useKoodistoOptions/useKoodistoOptions.test.ts
@@ -0,0 +1,62 @@
+import { renderHook } from '@testing-library/react-hooks';
+import { useKoodistoDataOptions } from './index';
+
+test('useKoodistoDataOptions should return array of select options sorted by label for simple koodisto', () => {
+  const { result } = renderHook(() =>
+    useKoodistoDataOptions({
+      koodistoData: [
+        {
+          koodiUri: 'jokukoodisto_0',
+          versio: 1,
+          metadata: [
+            {
+              kieli: 'FI',
+              nimi: 'x fi',
+            },
+            {
+              kieli: 'SV',
+              nimi: 'x sv',
+            },
+          ],
+        },
+        {
+          koodiUri: 'jokukoodisto_1',
+          versio: 1,
+          metadata: [
+            {
+              kieli: 'FI',
+              nimi: 'a fi',
+            },
+            {
+              kieli: 'SV',
+              nimi: 'a sv',
+            },
+          ],
+        },
+      ],
+      language: 'fi',
+    })
+  );
+
+  expect(result.current).toEqual([
+    {
+      value: 'jokukoodisto_1#1',
+      label: 'a fi',
+    },
+    {
+      value: 'jokukoodisto_0#1',
+      label: 'x fi',
+    },
+  ]);
+});
+
+test('useKoodistoDataOptions should return empty array when koodistoData is empty array', () => {
+  const { result } = renderHook(() =>
+    useKoodistoDataOptions({
+      koodistoData: [],
+      language: 'fi',
+    })
+  );
+
+  expect(result.current).toEqual([]);
+});
--- src/main/app/src/types/common.d.ts
+++ src/main/app/src/types/common.d.ts
@@ -5,3 +5,11 @@ type FormDate = string;
 type FormDateRange = { alkaa: FormDate; paattyy: FormDate };
 
 type TranslatedField<T> = Partial<Record<LanguageCode, T>>;
+
+type SelectOptions = Array<{ value: string; label: string }>;
+
+type Koodi = {
+  koodiUri: string;
+  versio: number;
+  metadata: any;
+};

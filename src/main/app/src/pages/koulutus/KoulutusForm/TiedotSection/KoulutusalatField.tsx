01127cb5e3d7332ba7ef38a99203d7a6a310d761
Petteri Tolonen <petteri.tolonen@cybercom.com>
2020-11-23 14:00:08 +0200

    KTO-875: Use FormFieldKoulutusalaSelect in KoulutusalatField

--- src/main/app/src/pages/koulutus/KoulutusForm/TiedotSection/KoulutusalatField.tsx
+++ src/main/app/src/pages/koulutus/KoulutusForm/TiedotSection/KoulutusalatField.tsx
@@ -2,17 +2,8 @@ import React from 'react';
 import { Field } from 'redux-form';
 import { useTranslation } from 'react-i18next';
 
-import {
-  createFormFieldComponent,
-  selectMapProps,
-} from '#/src/components/formFields';
+import { FormFieldKoulutusalaSelect } from '#/src/components/formFields';
 import { getTestIdProps } from '#/src/utils';
-import KoulutusalaSelect from '#/src/components/KoulutusalaSelect';
-
-const KoulutusalaFieldComponent = createFormFieldComponent(
-  KoulutusalaSelect,
-  selectMapProps
-);
 
 export const KoulutusalatField = ({ disabled, name }) => {
   const { t } = useTranslation();
@@ -22,7 +13,7 @@ export const KoulutusalatField = ({ disabled, name }) => {
       <Field
         disabled={disabled}
         name={`${name}.koulutusalat`}
-        component={KoulutusalaFieldComponent}
+        component={FormFieldKoulutusalaSelect}
         label={t('koulutuslomake.valitseKoulutusalat')}
         isMulti={true}
       />

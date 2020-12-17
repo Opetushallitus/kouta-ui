6776bff7bb8a3993fb6aa25e50d294996badc73c
Petteri Tolonen <petteri.tolonen@cybercom.com>
2020-12-17 12:05:56 +0200

    KTO-1057: Retry getMe on redirect

--- src/main/app/src/utils/api/getMe.ts
+++ src/main/app/src/utils/api/getMe.ts
@@ -1,5 +1,22 @@
-export const getMe = async ({ httpClient, apiUrls }) => {
-  const { data } = await httpClient.get(apiUrls.url('kayttooikeus-service.me'));
+// Need to retry getMe a few times, because often session is not created when first called
+// 'kayttooikeus-service.me' is redirected (302) to /cas/login which can be detected checking
+// if the response URL has changed.
 
-  return data;
+export const getMe = async ({ httpClient, apiUrls }) => {
+  const fn = () =>
+    httpClient.get(apiUrls.url('kayttooikeus-service.me'), {
+      cache: {
+        ignoreCache: true,
+      },
+    });
+  let res = await fn();
+  let count = 3;
+  while (
+    res?.request.responseURL !== apiUrls.url('kayttooikeus-service.me') &&
+    count !== 0
+  ) {
+    res = await fn();
+    count -= 1;
+  }
+  return res?.data;
 };

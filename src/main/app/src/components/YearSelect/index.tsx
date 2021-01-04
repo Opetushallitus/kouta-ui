e6df36f7c9ccda4ef306209a540f0b8cc9be9d2f
Petteri Tolonen <petteri.tolonen@cybercom.com>
2021-01-04 14:59:49 +0200

    Small improvements to YearSelect-component

--- src/main/app/src/components/YearSelect/index.tsx
+++ src/main/app/src/components/YearSelect/index.tsx
@@ -1,20 +1,25 @@
 import React, { useMemo } from 'react';
 import getYear from 'date-fns/getYear';
+import _ from 'lodash';
 
 import Select from '#/src/components/Select';
 import { isNumeric } from '#/src/utils';
 
+const SELECTABLE_YEARS_COUNT = 20;
+
 const currentYear = getYear(new Date());
 
 const getYearOptions = ({ min, max }) =>
-  [...new Array(max - min)].map((value, index) => ({
+  _.times(max - min, index => ({
     value: `${min + index}`,
     label: `${min + index}`,
   }));
 
 export const YearSelect = ({ min: minProp, max: maxProp, ...props }) => {
-  const min = isNumeric(minProp) ? parseInt(minProp) : currentYear;
-  const max = isNumeric(maxProp) ? parseInt(maxProp) : min + 20;
+  const min = isNumeric(minProp) ? _.parseInt(minProp) : currentYear;
+  const max = isNumeric(maxProp)
+    ? _.parseInt(maxProp)
+    : min + SELECTABLE_YEARS_COUNT;
 
   const options = useMemo(() => getYearOptions({ min, max }), [min, max]);
 

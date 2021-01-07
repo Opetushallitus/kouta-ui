b700b18d5c5fe2bb27fae9e3b494532a47ad677a
Petteri Tolonen <petteri.tolonen@cybercom.com>
2021-01-07 16:30:57 +0200

    Use intercept and match mock response body if defined

--- src/main/app/cypress/mockUtils.js
+++ src/main/app/cypress/mockUtils.js
@@ -1,16 +1,16 @@
 const path = require('path');
+const { isEqual } = require('lodash');
 
 const MOCK_PATH = 'cypress/mocks';
 
 export const addMockFileRoutes = fileName => {
   cy.readFile(path.join(MOCK_PATH, fileName)).then(data => {
-    data.forEach(({ url, method = 'GET', response }) => {
-      // TODO: Mock only on matching response body and use cy.intercept()
-      cy.route({
-        method,
-        url,
-        status: response.status,
-        response: response.body,
+    data.forEach(({ url, method = 'GET', body, response }) => {
+      cy.intercept(method, url, req => {
+        // Check that body matches the one from mock if defined
+        if (body === undefined || isEqual(body, req.body)) {
+          req.reply(response.status, response.body);
+        }
       });
     });
   });

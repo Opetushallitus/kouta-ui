589ed4502e0ceba5e1f69ccec5e96265b3d43b56
Petteri Tolonen <petteri.tolonen@cybercom.com>
2021-01-12 12:47:24 +0200

    KTO-438: Changed koodisto 'pohjakoulutuskouta' to 'pohjakoulutusvaatimuskouta'

--- src/main/app/cypress/data/hakukohde.ts
+++ src/main/app/cypress/data/hakukohde.ts
@@ -12,7 +12,7 @@ const hakukohde = () => ({
   aloituspaikat: 150,
   ensikertalaisenAloituspaikat: 49,
   kaytetaanHaunAlkamiskautta: false,
-  pohjakoulutusvaatimusKoodiUrit: ['pohjakoulutusvaatimustoinenaste_yo#1'],
+  pohjakoulutusvaatimusKoodiUrit: ['pohjakoulutusvaatimuskouta_0#1'],
   pohjakoulutusvaatimusTarkenne: {
     fi: '<p>Fi tarkenne</p>',
   },

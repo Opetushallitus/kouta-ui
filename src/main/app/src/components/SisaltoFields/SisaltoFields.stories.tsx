58762c0262e88dced56c1215b20c650afb58bff9
Antti Sarjanoja <antti.sarjanoja@gofore.com>
2021-01-14 15:07:15 +0200

    review fixes

--- src/main/app/src/components/SisaltoFields/SisaltoFields.stories.tsx
+++ src/main/app/src/components/SisaltoFields/SisaltoFields.stories.tsx
@@ -2,13 +2,13 @@ import { storiesOf } from '@storybook/react';
 import React from 'react';
 import { reduxForm } from 'redux-form';
 import { makeStoreDecorator } from '#/src/storybookUtils';
-import { SisaltoFields } from '../SisaltoFields';
+import { SisaltoFields } from '.';
 
 const StoryForm = reduxForm({
   form: 'storyForm',
 })(({ children }) => <form children={children} />);
 
-storiesOf('ValintatapaContentFields', module)
+storiesOf('SisaltoFields', module)
   .addDecorator(makeStoreDecorator({ logging: true }))
   .add('Basic', () => (
     <StoryForm>

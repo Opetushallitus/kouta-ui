1b5cdf4ddd64ff32d6b832ad5513bc47b9a1230f
Petteri Tolonen <petteri.tolonen@cybercom.com>
2021-01-20 11:40:03 +0200

    Update hakukohde test snapshots and test data

--- src/main/app/cypress/data/hakukohde.ts
+++ src/main/app/cypress/data/hakukohde.ts
@@ -12,7 +12,7 @@ const hakukohde = () => ({
   aloituspaikat: 150,
   ensikertalaisenAloituspaikat: 49,
   kaytetaanHaunAlkamiskautta: false,
-  pohjakoulutusvaatimusKoodiUrit: ['pohjakoulutusvaatimuskouta_0#1'],
+  pohjakoulutusvaatimusKoodiUrit: ['pohjakoulutusvaatimuskouta_pk#1'],
   pohjakoulutusvaatimusTarkenne: {
     fi: '<p>Fi tarkenne</p>',
   },

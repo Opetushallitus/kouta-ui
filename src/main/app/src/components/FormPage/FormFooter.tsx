c3a2b8212719650b32f355d43f191eb0a7c01937
Petteri Tolonen <petteri.tolonen@cybercom.com>
2021-02-05 11:09:16 +0200

    Enable save button also when no unsaved changes on form.

--- src/main/app/src/components/FormPage/FormFooter.tsx
+++ src/main/app/src/components/FormPage/FormFooter.tsx
@@ -4,17 +4,16 @@ import { useTranslation } from 'react-i18next';
 
 import Button from '#/src/components/Button';
 import Flex from '#/src/components/Flex';
-import { useIsDirty, useIsSubmitting } from '#/src/hooks/form';
+import { useIsSubmitting } from '#/src/hooks/form';
 
 const FormFooter = ({ entity, save, canUpdate = true, submitProps = {} }) => {
   const { t } = useTranslation();
   const isSubmitting = useIsSubmitting();
-  const isDirty = useIsDirty();
   return (
     <Flex justifyEnd>
       <Button
         onClick={save}
-        disabled={!canUpdate || isSubmitting || !isDirty}
+        disabled={!canUpdate || isSubmitting}
         title={!canUpdate ? t(`${entity}lomake.eiMuokkausOikeutta`) : undefined}
         {...submitProps}
       >

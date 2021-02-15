b4d090f1ad3f754d8316bb9dc3db2741fbc80ba9
Petteri Tolonen <petteri.tolonen@cybercom.com>
2021-02-15 12:55:07 +0200

    KTO-1139: Remove koulutustyyppi from default values
    - Setting initial koulutustyyppi is tricky, because we might set it to value, which shouldn't be allowed

--- src/main/app/src/pages/koulutus/KoulutusForm/index.tsx
+++ src/main/app/src/pages/koulutus/KoulutusForm/index.tsx
@@ -1,14 +1,9 @@
-import {
-  KOULUTUSTYYPPI,
-  POHJAVALINTA,
-  DEFAULT_JULKAISUTILA,
-} from '#/src/constants';
+import { POHJAVALINTA, DEFAULT_JULKAISUTILA } from '#/src/constants';
 
 export { default } from './KoulutusForm';
 
 export const initialValues = {
   tila: DEFAULT_JULKAISUTILA,
-  koulutustyyppi: KOULUTUSTYYPPI.AMMATILLINEN_KOULUTUS,
   pohja: {
     tapa: POHJAVALINTA.UUSI,
   },

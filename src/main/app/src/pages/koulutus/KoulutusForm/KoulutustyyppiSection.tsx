1cd91290a5bd59b57edf7350767eee824ded63fb
Petteri Tolonen <petteri.tolonen@cybercom.com>
2021-02-19 14:04:06 +0200

    KTO-1139: Disable koulutustyyppi select on komo when loading organisaatio hierarkia

--- src/main/app/src/pages/koulutus/KoulutusForm/KoulutustyyppiSection.tsx
+++ src/main/app/src/pages/koulutus/KoulutusForm/KoulutustyyppiSection.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import _ from 'lodash';
+import _fp from 'lodash/fp';
 import { useTranslation } from 'react-i18next';
 import { Field } from 'redux-form';
 
@@ -17,10 +17,11 @@ import {
 import { useCurrentUserHasRole } from '#/src/hooks/useCurrentUserHasRole';
 import { useIsOphVirkailija } from '#/src/hooks/useIsOphVirkailija';
 import { useOrganisaatioHierarkia } from '#/src/hooks/useOrganisaatioHierarkia';
+import { otherwise } from '#/src/utils';
 import iterateTree from '#/src/utils/iterateTree';
 
 const useOppilaitosTyypit = organisaatioOid => {
-  const { hierarkia } = useOrganisaatioHierarkia(organisaatioOid, {
+  const { hierarkia, isLoading } = useOrganisaatioHierarkia(organisaatioOid, {
     skipParents: false,
   });
 
@@ -32,29 +33,32 @@ const useOppilaitosTyypit = organisaatioOid => {
     }
   });
 
-  const isAmmatillinen = !_.isEmpty(
-    _.intersection(AMMATILLISET_OPPILAITOSTYYPIT, oppilaitosTyypit)
+  const isAmmatillinen = !_fp.isEmpty(
+    _fp.intersection(AMMATILLISET_OPPILAITOSTYYPIT, oppilaitosTyypit)
   );
 
-  const isKorkeakoulutus = !_.isEmpty(
-    _.intersection(KORKEAKOULU_OPPILAITOSTYYPIT, oppilaitosTyypit)
+  const isKorkeakoulutus = !_fp.isEmpty(
+    _fp.intersection(KORKEAKOULU_OPPILAITOSTYYPIT, oppilaitosTyypit)
   );
 
-  const isLukio = !_.isEmpty(
-    _.intersection(LUKIO_OPPILAITOSTYYPIT, oppilaitosTyypit)
+  const isLukio = !_fp.isEmpty(
+    _fp.intersection(LUKIO_OPPILAITOSTYYPIT, oppilaitosTyypit)
   );
 
-  return { isAmmatillinen, isKorkeakoulutus, isLukio };
+  return { isAmmatillinen, isKorkeakoulutus, isLukio, isLoading };
 };
 
-const KoulutustyyppiSection = ({ organisaatioOid, johtaaTutkintoon, name }) => {
+const KoulutustyyppiSection = ({ organisaatioOid, name }) => {
   const { t } = useTranslation();
 
   const isOphVirkailija = useIsOphVirkailija();
 
-  const { isAmmatillinen, isKorkeakoulutus, isLukio } = useOppilaitosTyypit(
-    organisaatioOid
-  );
+  const {
+    isAmmatillinen,
+    isKorkeakoulutus,
+    isLukio,
+    isLoading,
+  } = useOppilaitosTyypit(organisaatioOid);
 
   const canCreate = useCurrentUserHasRole(
     ENTITY.KOULUTUS,
@@ -68,30 +72,28 @@ const KoulutustyyppiSection = ({ organisaatioOid, johtaaTutkintoon, name }) => {
       component={FormFieldKoulutustyyppiSelect}
       label={t('yleiset.valitseKoulutustyyppi')}
       disabled={!canCreate}
-      getIsDisabled={value => {
-        if (isOphVirkailija) {
-          return false;
-        } else if (
-          isAmmatillinen &&
-          !isKorkeakoulutus &&
-          ![
-            KOULUTUSTYYPPI.TUTKINNON_OSA,
-            KOULUTUSTYYPPI.OSAAMISALA,
-            KOULUTUSTYYPPI.MUUT_KOULUTUKSET,
-          ].includes(value)
-        ) {
-          return true;
-        } else if (
-          isKorkeakoulutus &&
-          !KORKEAKOULU_KOULUTUSTYYPIT.includes(value)
-        ) {
-          return true;
-        } else if (isLukio && value !== KOULUTUSTYYPPI.LUKIOKOULUTUS) {
-          return true;
-        } else {
-          return false;
-        }
-      }}
+      getIsDisabled={_fp.cond([
+        [() => isLoading, _fp.T],
+        [() => isOphVirkailija, _fp.F],
+        [
+          (value: any) =>
+            isAmmatillinen &&
+            !isKorkeakoulutus &&
+            ![
+              KOULUTUSTYYPPI.TUTKINNON_OSA,
+              KOULUTUSTYYPPI.OSAAMISALA,
+              KOULUTUSTYYPPI.MUUT_KOULUTUKSET,
+            ].includes(value),
+          _fp.T,
+        ],
+        [
+          value =>
+            isKorkeakoulutus && !KORKEAKOULU_KOULUTUSTYYPIT.includes(value),
+          _fp.T,
+        ],
+        [value => isLukio && value !== KOULUTUSTYYPPI.LUKIOKOULUTUS, _fp.T],
+        [otherwise, _fp.F],
+      ])}
     />
   );
 };

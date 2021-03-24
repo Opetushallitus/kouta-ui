e5331647d1597fb081ae4694f85964f1dc4f45c3
Vesa Ruuskanen <vesa.ruuskanen@gofore.com>
2021-03-24 13:26:57 +0200

    KTO-283: Lisätty testi koulutusKoodiUrin päättelyyn ammatilliselle koulutukselle

--- src/main/app/src/utils/koulutus/getFormValuesByKoulutus.test.ts
+++ src/main/app/src/utils/koulutus/getFormValuesByKoulutus.test.ts
@@ -43,3 +43,47 @@ test('getFormValuesByKoulutus returns correct form values given koulutus', () =>
 
   expect(values).toMatchSnapshot();
 });
+
+test('getFormValuesByKoulutus returns correct form values for ammatillinen koulutus', () => {
+  const values = getFormValuesByKoulutus({
+    tila: 'tallennettu',
+    julkinen: true,
+    kielivalinta: ['fi', 'sv'],
+    koulutuksetKoodiUri: ['koulutus_371101#1'],
+    koulutustyyppi: 'amm',
+    metadata: {
+      kuvauksenNimi: {
+        fi: 'Fi nimi',
+        sv: 'Sv nimi',
+      },
+      kuvaus: {
+        fi: 'Fi kuvaus',
+        sv: 'Sv kuvaus',
+      },
+      lisatiedot: [
+        {
+          otsikkoKoodiUri: 'osio_1#1',
+          teksti: {},
+        },
+        {
+          otsikkoKoodiUri: 'osio_2#1',
+          teksti: {
+            fi: 'Fi kuvaus',
+            sv: 'Sv kuvaus',
+          },
+        },
+      ],
+      opintojenLaajuusKoodiUri: 'laajuus_1#1',
+      tutkintonimikeKoodiUrit: ['nimike_1#1', 'nimike_2#1'],
+      tyyppi: 'amm',
+      koulutusalaKoodiUrit: ['koulutusala_1#1', 'koulutusala_2#1'],
+    },
+    nimi: {
+      fi: 'Fi nimi',
+      sv: 'Sv nimi',
+    },
+    tarjoajat: ['123.456.789'],
+  });
+
+  expect(values).toMatchSnapshot();
+});

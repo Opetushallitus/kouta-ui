d42fa8f5587b3ec82e2d5d6dbae30a6ef09b84df
Petteri Tolonen <petteri.tolonen@cybercom.com>
2021-03-25 09:30:42 +0200

    KTO-1175: Hide toteutus korkeakoulutus osaamisala sections

--- src/main/app/src/pages/toteutus/ToteutusForm/KorkeakouluOsaamisalatSection.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import KorkeakouluOsaamisalatFields from './KorkeakoulutuOsaamisalatFields';
-
-export const KorkeakouluOsaamisalatSection = ({ language, name }) => {
-  return <KorkeakouluOsaamisalatFields language={language} name={name} />;
-};
-
-export default KorkeakouluOsaamisalatSection;

813c2fdf07d07c8d5adc76b2f2585a7de6b92ea2
Petteri Tolonen <petteri.tolonen@cybercom.com>
2021-03-25 09:31:49 +0200

    Improve types in useKoodistoOptions

--- src/main/app/src/hooks/useLanguage/index.ts
+++ src/main/app/src/hooks/useLanguage/index.ts
@@ -3,7 +3,7 @@ import { useTranslation } from 'react-i18next';
 export const useLanguage = () => {
   const { i18n } = useTranslation();
 
-  return i18n.language;
+  return i18n.language as LanguageCode;
 };
 
 export default useLanguage;

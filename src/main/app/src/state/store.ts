009a2bbb3f8ac391e240dbfac1af37a87aa3c80b
Petteri Tolonen <petteri.tolonen@cybercom.com>
2021-05-04 14:09:53 +0300

    Remove unused redux-thunk

--- src/main/app/src/state/store.ts
+++ src/main/app/src/state/store.ts
@@ -1,30 +1,18 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, compose } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { reducer as formReducer } from 'redux-form';
 import { persistStore } from 'redux-persist';
-import thunk from 'redux-thunk';
 
 import { isDev } from '#/src/utils';
 
 import { createRootReducer } from './rootReducer';
 
-export const store = ({ apiUrls, httpClient, history, localization }) => {
+export const store = () => {
   const rootReducer = createRootReducer({
     form: formReducer,
   });
 
-  const context = {
-    httpClient,
-    apiUrls,
-    history,
-    localization,
-  };
-
-  const middleware = applyMiddleware(thunk.withExtraArgument(context));
-
-  const enhancer = isDev
-    ? composeWithDevTools(compose(middleware))
-    : compose(middleware);
+  const enhancer = isDev ? composeWithDevTools(compose()) : compose();
 
   const store = createStore(rootReducer, enhancer);
 

221881520e68da05ebd35900361ff978a864aa08
Petteri Tolonen <petteri.tolonen@cybercom.com>
2021-05-04 14:09:55 +0300

    Fix KOMO form 'pohja' field type

--- src/main/app/src/types/koulutusTypes.ts
+++ src/main/app/src/types/koulutusTypes.ts
@@ -24,7 +24,7 @@ export type KoulutusFormValues = {
   kieliversiot: Array<LanguageCode>;
   muokkaaja?: string;
   tila: JULKAISUTILA;
-  pohja: any;
+  pohja?: PohjaValinta;
   information: InformationSectionValues;
   lisatiedot: {
     osiot: SelectOptions;

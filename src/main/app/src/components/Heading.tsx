fe39585bc4ff01817d0ff2373b9c01b93fe5d64b
Petteri Tolonen <petteri.tolonen@cybercom.com>
2021-05-19 16:29:08 +0300

    Heading-komponentin tyypitys

--- src/main/app/src/components/Heading.tsx
+++ src/main/app/src/components/Heading.tsx
@@ -8,12 +8,18 @@ const StyledTypography = styled(Typography)`
   width: 100%;
 `;
 
+type HeadingProps = {
+  children?: React.ReactNode;
+  variant?: string;
+  hasDivider?: boolean;
+};
+
 export const Heading = ({
-  children = null,
+  children,
   variant = 'h6',
   hasDivider = false,
   ...props
-}) => (
+}: HeadingProps) => (
   <StyledTypography variant={variant} marginBottom={2} {...props}>
     {children}
     {hasDivider && <Divider />}

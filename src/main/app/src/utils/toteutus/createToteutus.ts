96c612b1f7c534a64a2958c459b2b85084c38e7b
Petteri Tolonen <petteri.tolonen@cybercom.com>
2021-05-19 16:29:09 +0300

    KTO-1149: Poistettu vanhentunut lukio KOMOTO nimen päättely lukiolinjaKoodiUri-kentän perusteella

--- src/main/app/src/utils/toteutus/createToteutus.ts
+++ src/main/app/src/utils/toteutus/createToteutus.ts
@@ -1,51 +1,7 @@
-import _ from 'lodash';
-
-import getKoodiNimiTranslation from '#/src/utils/getKoodiNimiTranslation';
-import getKoodisto from '#/src/utils/koodi/getKoodisto';
-import parseKoodiUri from '#/src/utils/koodi/parseKoodiUri';
-
-const resolveToteutusNimi = async ({ httpClient, apiUrls, toteutus }) => {
-  if (!_.isEmpty(toteutus.nimi)) {
-    return toteutus.nimi;
-  }
-
-  const { kielivalinta } = toteutus;
-  const lukiolinjaKoodiUri = _.get(toteutus, 'metadata.lukiolinjaKoodiUri');
-
-  if (_.isString(lukiolinjaKoodiUri) && _.isArray(kielivalinta)) {
-    const { koodisto, koodi } = parseKoodiUri(lukiolinjaKoodiUri);
-
-    const koodistoKoodit = await getKoodisto({
-      httpClient,
-      apiUrls,
-      koodistoUri: koodisto,
-    });
-
-    const targetKoodi = _.isArray(koodistoKoodit)
-      ? koodistoKoodit.find(k => k.koodiUri === koodi)
-      : undefined;
-
-    return targetKoodi
-      ? _.zipObject(
-          kielivalinta,
-          kielivalinta.map(k => getKoodiNimiTranslation(targetKoodi, k))
-        )
-      : {};
-  }
-
-  return {};
-};
-
 const createToteutus = async ({ httpClient, apiUrls, toteutus }) => {
-  let toteutusData = { ...toteutus };
-
-  const nimi = await resolveToteutusNimi({ httpClient, apiUrls, toteutus });
-
-  toteutusData = { ...toteutusData, nimi };
-
   const { data } = await httpClient.put(
     apiUrls.url('kouta-backend.toteutus'),
-    toteutusData
+    toteutus
   );
 
   return data;

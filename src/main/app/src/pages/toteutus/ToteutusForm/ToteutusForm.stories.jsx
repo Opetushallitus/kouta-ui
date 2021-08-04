7c11952e27e5a408dc43573c3cafc4b45d777f45
Petteri Tolonen <petteri.tolonen@cybercom.com>
2021-08-04 13:30:59 +0300

    Korjattu ToteutusFormin storybook

--- src/main/app/src/pages/toteutus/ToteutusForm/ToteutusForm.stories.jsx
+++ src/main/app/src/pages/toteutus/ToteutusForm/ToteutusForm.stories.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 import ReduxForm from '#/src/components/ReduxForm';
 import { KOULUTUSTYYPIT } from '#/src/constants';
 
-import ToteutusForm, { initialValues } from './index';
+import { initialValues } from '../initialToteutusValues';
+import ToteutusForm from './index';
+
 export default {
   title: 'ToteutusForm',
   argTypes: {
@@ -20,7 +22,11 @@ const Wrapper = ({ koulutustyyppi = 'amm' }) => {
   return (
     <ReduxForm
       form="toteutus"
-      initialValues={initialValues('koulutuksen nimi', ['fi'])}
+      initialValues={initialValues({
+        koulutustyyppi,
+        koulutusNimi: 'koulutuksen nimi',
+        koulutusKielet: ['fi'],
+      })}
     >
       <ToteutusForm
         organisaatioOid="1.2.246.562.10.594252633210"

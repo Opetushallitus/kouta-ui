2c7a48c90be8b3012ccf37b8793791a6d4020dc2
Petteri Tolonen <petteri.tolonen@cybercom.com>
2021-08-04 09:48:41 +0300

    Korjattu SectionInnerCollapse otsikon tekstin väri
    Mennyt rikki heading-komponentin rakenteen muuttuessa.

--- src/main/app/src/components/SectionInnerCollapse.tsx
+++ src/main/app/src/components/SectionInnerCollapse.tsx
@@ -24,7 +24,7 @@ const SectionInnerCollapseHeader = styled.div`
     css`
       background-color: ${getThemeProp('colors.primary.main')};
       border-radius: 4px 4px 0 0;
-      & > * {
+      & * {
         color: white;
       }
     `}

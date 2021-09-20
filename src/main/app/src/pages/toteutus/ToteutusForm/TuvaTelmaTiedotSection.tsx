1fda2998fc0bc7050bc12b7a157c02450665acac
Heidi Lammi-Mihaljov <heidi.lammi-mihaljov@gofore.com>
2021-09-20 16:12:19 +0300

    KTO-187: Pakotetaan TUVA- ja TELMA-nimet niiden toteutuksille

--- src/main/app/src/pages/toteutus/ToteutusForm/TuvaTelmaTiedotSection.tsx
+++ src/main/app/src/pages/toteutus/ToteutusForm/TuvaTelmaTiedotSection.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { useTranslation } from 'react-i18next';
 import { Field } from 'redux-form';
@@ -6,6 +6,7 @@ import { Field } from 'redux-form';
 import { FormFieldInput, FormFieldSwitch } from '#/src/components/formFields';
 import { Box, FormControl, Input } from '#/src/components/virkailija';
 import { KOULUTUSTYYPPI } from '#/src/constants';
+import { useBoundFormActions } from '#/src/hooks/form';
 import useKoodi from '#/src/hooks/useKoodi';
 import { useUserLanguage } from '#/src/hooks/useUserLanguage';
 import { getTestIdProps } from '#/src/utils';
@@ -21,6 +22,16 @@ export const TuvaTelmaTiedotSection = ({ language, name, koulutus }) => {
   const { koodi: laajuusYksikko } = useKoodi('opintojenlaajuusyksikko_6');
 
   const userLanguage = useUserLanguage();
+  const koulutustyyppi = koulutus.koulutustyyppi;
+  const { change } = useBoundFormActions();
+  useEffect(() => {
+    change(`${name}.nimi`, {
+      fi: t(`koulutustyypit.${koulutustyyppi}`, { lng: 'fi' }),
+      sv: t(`koulutustyypit.${koulutustyyppi}`, { lng: 'sv' }),
+      en: t(`koulutustyypit.${koulutustyyppi}`, { lng: 'en' }),
+    });
+    return () => change(`${name}.nimi`, {});
+  }, [change, koulutustyyppi, name, t]);
 
   let toteutuksenLaajuus = getKoodiNimiTranslation(laajuusKoodi, userLanguage);
 
@@ -38,6 +49,7 @@ export const TuvaTelmaTiedotSection = ({ language, name, koulutus }) => {
           component={FormFieldInput}
           label={t('toteutuslomake.toteutuksenNimi')}
           required
+          disabled
         />
       </Box>
       <Box mb={2} display="flex">

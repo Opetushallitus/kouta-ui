1740cffc10b1b42018c7ba79f32a740f76369940
Tatu Honkanen <tatu.honkanen@cybercom.com>
2021-10-27 14:26:56 +0300

    OY-3145 : Disable yhteishaku if not oph-virkailija

--- src/main/app/src/pages/haku/HakuForm/HakutapaSection.tsx
+++ src/main/app/src/pages/haku/HakuForm/HakutapaSection.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
 import { useTranslation } from 'react-i18next';
 import { Field } from 'redux-form';
@@ -6,9 +6,16 @@ import { Field } from 'redux-form';
 import { FormFieldRadioGroup } from '#/src/components/formFields';
 import useKoodistoOptions from '#/src/hooks/useKoodistoOptions';
 
-export const HakutapaSection = ({ name }) => {
+export const HakutapaSection = ({ name, isOphVirkailija }) => {
   const { options } = useKoodistoOptions({ koodisto: 'hakutapa' });
   const { t } = useTranslation();
+
+  const getIsDisabled = useCallback(
+    value => {
+      return !isOphVirkailija && value?.startsWith('hakutapa_01');
+    },
+    [isOphVirkailija]
+  );
 
   return (
     <Field
@@ -16,6 +23,7 @@ export const HakutapaSection = ({ name }) => {
       component={FormFieldRadioGroup}
       label={t('hakulomake.valitseHakutapa')}
       options={options}
+      getIsDisabled={getIsDisabled}
       required
     />
   );

95b715510aa55da9ff352fee1430041b1cf65cc8
Petteri Tolonen <petteri.tolonen@knowit.fi>
2022-01-14 14:40:19 +0200

    Siistitty useFilterState-hookkia

--- src/main/app/src/pages/HomePage/useFilterState.ts
+++ src/main/app/src/pages/HomePage/useFilterState.ts
@@ -10,49 +10,33 @@ export const useFilterState = (name: string) => {
   );
   const dispatch = useDispatch();
   const setPagination = useCallback(
-    pagination => {
-      return dispatch(setPaginationAction({ name, ...pagination }));
-    },
+    pagination => dispatch(setPaginationAction({ name, ...pagination })),
     [dispatch, name]
   );
-  const setPage = page => setPagination({ page });
-  const setOrderBy = orderBy => setPagination({ orderBy });
 
   // Muut kuin järjestykseen ja paginointiin liittyvät valinnat vaikuttavat hakutulosten määrään -> page 0
-  const setNimi = nimi => setPagination({ page: 0, nimi });
   const setTila = tila => setPagination({ page: 0, tila });
-  const setShowArchived = showArchived =>
-    setPagination({ page: 0, showArchived });
 
-  const onShowArchivedChange = useCallback(
-    e => {
-      setPagination({ page: 0, showArchived: e.target.checked });
-    },
-    [setPagination]
-  );
-  const onNimiChange = useCallback(
-    value => {
-      setPagination({ page: 0, nimi: value });
-    },
-    [setPagination]
-  );
   return {
-    setNimi,
+    setNimi: nimi => setPagination({ page: 0, nimi }),
     nimi,
     page,
-    setPage,
+    setPage: page => setPagination({ page }),
     orderBy,
-    setOrderBy,
+    setOrderBy: orderBy => setPagination({ orderBy }),
     showArchived,
-    setShowArchived,
     tila,
     setTila,
     filtersProps: {
       nimi,
       showArchived,
       tila,
-      onNimiChange,
-      onShowArchivedChange,
+      onNimiChange: useCallback(
+        value => {
+          setPagination({ page: 0, nimi: value });
+        },
+        [setPagination]
+      ),
       onTilaChange: setTila,
     },
   };

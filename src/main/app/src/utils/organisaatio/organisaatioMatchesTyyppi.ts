c60832eafedf5ce5ecf56afb48e4a2df9301c1e5
Petteri Tolonen <petteri.tolonen@knowit.fi>
2022-01-17 10:50:59 +0200

    Muutettu organisaatioMatchesTyyppi toimimaan monella tilalla

--- src/main/app/src/utils/organisaatio/organisaatioMatchesTyyppi.ts
+++ src/main/app/src/utils/organisaatio/organisaatioMatchesTyyppi.ts
@@ -1,11 +1,14 @@
 import _fp from 'lodash/fp';
 
+import { valueToArray } from '#/src/utils';
+
 export const getOrganisaatioTyypit = organisaatio =>
   organisaatio?.organisaatiotyypit ?? organisaatio?.tyypit ?? [];
 
 export const organisaatioMatchesTyyppi = _fp.curry((tyyppi, organisaatio) => {
-  const tyypit = getOrganisaatioTyypit(organisaatio);
-  return tyypit.includes(tyyppi);
+  const tyypit = valueToArray(tyyppi);
+  const orgTyypit = getOrganisaatioTyypit(organisaatio);
+  return orgTyypit.some(t => tyypit.includes(t));
 });
 
 export default organisaatioMatchesTyyppi;

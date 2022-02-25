541b9fd320a5a353eda144a70938c9481fe04645
Petteri Tolonen <petteri.tolonen@knowit.fi>
2022-02-25 09:29:44 +0200

    Lisätty OsaamisalatInput ja sille testi

---
+++ src/main/app/src/pages/toteutus/OsaamisalatInput.tsx
@@ -0,0 +1,168 @@
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
+
+import _ from 'lodash';
+import { useTranslation } from 'react-i18next';
+
+import { FieldGroup } from '#/src/components/FieldGroup';
+import { CheckboxGroup } from '#/src/components/virkailija';
+import getKoodiNimiTranslation from '#/src/utils/getKoodiNimiTranslation';
+import { koodiUriWithoutVersion } from '#/src/utils/koodi/koodiUriWithoutVersion';
+import { getLanguageValue } from '#/src/utils/languageUtils';
+
+type Osaamisala = { nimi: TranslatedField<string>; uri: string };
+
+type UseSplitOptionsProps = {
+  osaamisalat: Array<Osaamisala>;
+  allSeenValues: Array<string>;
+  language: LanguageCode;
+  osaamisalatKoodistoData: Array<Koodi>;
+};
+
+const useSplitOptions = ({
+  osaamisalat,
+  allSeenValues,
+  language,
+  osaamisalatKoodistoData,
+}: UseSplitOptionsProps) => {
+  const validOptions = useMemo(
+    () =>
+      osaamisalat.map(({ nimi, uri }) => ({
+        label: getLanguageValue(nimi, language),
+        value: uri,
+      })),
+    [osaamisalat, language]
+  );
+
+  const invalidOptions = useMemo(
+    () =>
+      _.difference(
+        allSeenValues.map(koodiUriWithoutVersion),
+        osaamisalat.map(({ uri }) => koodiUriWithoutVersion(uri))
+      ).map(koodiUri => {
+        const foundKoodi = _.find(
+          osaamisalatKoodistoData,
+          koodi =>
+            koodiUriWithoutVersion(koodi.koodiUri) ===
+            koodiUriWithoutVersion(koodiUri)
+        );
+
+        return {
+          label: foundKoodi
+            ? getKoodiNimiTranslation(foundKoodi, language)
+            : koodiUri,
+          value: koodiUri,
+        };
+      }),
+    [allSeenValues, osaamisalat, osaamisalatKoodistoData, language]
+  );
+
+  return useMemo(
+    () => ({
+      validOptions: _.sortBy(validOptions, 'label'),
+      invalidOptions: _.sortBy(invalidOptions, 'label'),
+    }),
+    [validOptions, invalidOptions]
+  );
+};
+
+const useSplitValues = ({ osaamisalat, value }) =>
+  useMemo(() => {
+    const validValues: Array<string> = [];
+    const invalidValues: Array<string> = [];
+
+    value?.forEach(v => {
+      if (
+        _.find(
+          osaamisalat,
+          ({ uri }) => koodiUriWithoutVersion(uri) === koodiUriWithoutVersion(v)
+        )
+      ) {
+        validValues.push(v);
+      } else {
+        invalidValues.push(v);
+      }
+    });
+
+    return {
+      validValues,
+      invalidValues,
+    };
+  }, [osaamisalat, value]);
+
+type OsaamisalatInputProps = {
+  value: Array<string>;
+  onChange?: (val: Array<string>) => void;
+  ePeruste: { osaamisalat: Array<Osaamisala>; nimi: string };
+  language: LanguageCode;
+  disabled?: boolean;
+  error?: boolean;
+  osaamisalatKoodistoData: Array<Koodi>;
+};
+
+export const OsaamisalatInput = ({
+  value = [],
+  error = false,
+  disabled = false,
+  language,
+  ePeruste,
+  osaamisalatKoodistoData = [],
+  onChange = _.noop,
+}: OsaamisalatInputProps) => {
+  const { t } = useTranslation();
+
+  const { osaamisalat = [] } = ePeruste;
+
+  const [allSeenValues, setAllSeenValues] = useState(() => value);
+
+  useEffect(() => {
+    setAllSeenValues(allVals => _.uniq([...allVals, ...value]));
+  }, [value]);
+
+  const { validOptions, invalidOptions } = useSplitOptions({
+    osaamisalat,
+    allSeenValues,
+    language,
+    osaamisalatKoodistoData,
+  });
+
+  const { validValues, invalidValues } = useSplitValues({ osaamisalat, value });
+
+  const validChangeHandler = useCallback(
+    newValue => {
+      onChange([...invalidValues, ...(newValue ?? [])].sort());
+    },
+    [onChange, invalidValues]
+  );
+
+  const invalidChangeHandler = useCallback(
+    newValue => {
+      onChange([...validValues, ...(newValue ?? [])].sort());
+    },
+    [onChange, validValues]
+  );
+
+  return (
+    <>
+      <FieldGroup title={t('toteutuslomake.valitseOsaamisalat')}>
+        <CheckboxGroup
+          options={validOptions}
+          value={validValues}
+          onChange={validChangeHandler}
+          disabled={disabled}
+          error={error}
+        />
+      </FieldGroup>
+      {invalidOptions?.length > 0 && (
+        <FieldGroup title={t('toteutuslomake.virheellinenOsaamisalaValinta')}>
+          <CheckboxGroup
+            options={invalidOptions}
+            error={true}
+            value={invalidValues}
+            onChange={invalidChangeHandler}
+            disabled={disabled}
+          />
+        </FieldGroup>
+      )}
+    </>
+  );
+};

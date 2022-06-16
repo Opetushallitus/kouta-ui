e1bca462567430acda451b50857c814f1ab6f5d9
Petteri Tolonen <petteri.tolonen@knowit.fi>
2022-06-16 14:04:21 +0300

    Refaktoroitu koulutus-lomakkeella tutkintoon johtamattomien tiedot-osioita koulutustyypin perusteella erillisiin komponentteihin.

--- src/main/app/src/pages/koulutus/KoulutusForm/TiedotSection/TuvaTelmaAikuistenperusopetusTiedotSection.tsx
+++ src/main/app/src/pages/koulutus/KoulutusForm/TiedotSection/TuvaTelmaAikuistenperusopetusTiedotSection.tsx
@@ -7,25 +7,15 @@ import { Field } from 'redux-form';
 import { FormFieldInput } from '#/src/components/formFields';
 import OpintojenLaajuusFieldExtended from '#/src/components/OpintojenLaajuusFieldExtended';
 import { Box } from '#/src/components/virkailija';
-import { KOULUTUSTYYPPI, KOULUTUS_PERUSOPETUS_KOODIURI } from '#/src/constants';
+import { KOULUTUS_PERUSOPETUS_KOODIURI } from '#/src/constants';
 import { useBoundFormActions, useFieldValue } from '#/src/hooks/form';
 import { useIsOphVirkailija } from '#/src/hooks/useIsOphVirkailija';
-import {
-  isIn,
-  otherwise,
-  getKoulutustyyppiTranslationKey,
-  getTestIdProps,
-} from '#/src/utils';
+import { getKoulutustyyppiTranslationKey, getTestIdProps } from '#/src/utils';
 
 import EnforcedKoulutusSelect from './EnforcedKoulutusSelect';
 import OpintojenlaajuusField from './OpintojenlaajuusField';
 
-export const TuvaTelmaAikuistenperusopetusTiedotSection = ({
-  disabled,
-  language,
-  name,
-  koulutustyyppi,
-}) => {
+const useNimiFromKoulutustyyppi = ({ name, koulutustyyppi }) => {
   const { t } = useTranslation();
   const koulutustyyppiKey = getKoulutustyyppiTranslationKey(koulutustyyppi);
   const { change } = useBoundFormActions();
@@ -39,47 +29,76 @@ export const TuvaTelmaAikuistenperusopetusTiedotSection = ({
       });
     }
   }, [change, currNimi, koulutustyyppiKey, name, t]);
+};
+
+const NimiFieldFromKoulutustyyppi = ({ name, koulutustyyppi, language }) => {
+  const { t } = useTranslation();
 
   const nimiDisabled = !useIsOphVirkailija();
+
+  useNimiFromKoulutustyyppi({ koulutustyyppi, name });
+
   return (
-    <Box mb={-2}>
-      {koulutustyyppi === KOULUTUSTYYPPI.AIKUISTEN_PERUSOPETUS && (
-        <Box mb={2}>
-          <EnforcedKoulutusSelect
-            value={{ value: KOULUTUS_PERUSOPETUS_KOODIURI }}
-          />
-        </Box>
-      )}
+    <Box {...getTestIdProps('nimiInput')}>
+      <Field
+        disabled={nimiDisabled}
+        name={`${name}.nimi.${language}`}
+        component={FormFieldInput}
+        label={t('koulutuslomake.koulutuksenNimi')}
+        helperText={t('koulutuslomake.koulutuksenNimiNakyyOppijalleVaroitus')}
+        required
+      />
+    </Box>
+  );
+};
+
+export const TuvaTiedotSection = ({
+  disabled,
+  language,
+  name,
+  koulutustyyppi,
+}) => {
+  return (
+    <Box>
       <Box mb={2} {...getTestIdProps('opintojenlaajuusSelect')}>
-        {_fp.cond([
-          [
-            isIn([KOULUTUSTYYPPI.AIKUISTEN_PERUSOPETUS]),
-            () => (
-              <OpintojenLaajuusFieldExtended
-                name={name}
-                disabled={disabled}
-                required
-              />
-            ),
-          ],
-          [
-            otherwise,
-            () => (
-              <OpintojenlaajuusField disabled={disabled} name={name} required />
-            ),
-          ],
-        ])(koulutustyyppi)}
+        <OpintojenlaajuusField disabled={disabled} name={name} required />
       </Box>
-      <Box mb={2} {...getTestIdProps('nimiInput')}>
-        <Field
-          disabled={nimiDisabled}
-          name={`${name}.nimi.${language}`}
-          component={FormFieldInput}
-          label={t('koulutuslomake.koulutuksenNimi')}
-          helperText={t('koulutuslomake.koulutuksenNimiNakyyOppijalleVaroitus')}
+      <NimiFieldFromKoulutustyyppi
+        koulutustyyppi={koulutustyyppi}
+        name={name}
+        language={language}
+      />
+    </Box>
+  );
+};
+
+export const TelmaTiedotSection = TuvaTiedotSection;
+
+export const AikuistenPerusopetusTiedotSection = ({
+  disabled,
+  language,
+  name,
+  koulutustyyppi,
+}) => {
+  return (
+    <Box>
+      <Box mb={2}>
+        <EnforcedKoulutusSelect
+          value={{ value: KOULUTUS_PERUSOPETUS_KOODIURI }}
+        />
+      </Box>
+      <Box mb={2} {...getTestIdProps('opintojenlaajuusSelect')}>
+        <OpintojenLaajuusFieldExtended
+          name={name}
+          disabled={disabled}
           required
         />
       </Box>
+      <NimiFieldFromKoulutustyyppi
+        koulutustyyppi={koulutustyyppi}
+        name={name}
+        language={language}
+      />
     </Box>
   );
 };

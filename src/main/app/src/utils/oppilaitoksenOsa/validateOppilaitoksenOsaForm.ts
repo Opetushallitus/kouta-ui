7cca348f0b9d8992ba93a128f5eb748870449f61
Mikko Siukola <mikko.siukola@cybercom.com>
2022-09-19 17:43:47 +0300

    OY-3789 Add a validator for hakijapalveluiden yhteystiedot

--- src/main/app/src/utils/oppilaitoksenOsa/validateOppilaitoksenOsaForm.ts
+++ src/main/app/src/utils/oppilaitoksenOsa/validateOppilaitoksenOsaForm.ts
@@ -1,7 +1,7 @@
 import _fp from 'lodash/fp';
 
 import createErrorBuilder, {
-  validateArrayMinLength,
+  validateArrayMinLength, validateHakijapalveluidenYhteystiedot
 } from '#/src/utils/form/createErrorBuilder';
 import {
   getKielivalinta,
@@ -10,12 +10,15 @@ import {
 
 export const validateOppilaitoksenOsaForm = values => {
   const kieliversiot = getKielivalinta(values);
+
   return _fp
     .flow(
       validateArrayMinLength('kieliversiot', 1),
       validateIfJulkaistu(eb =>
         eb.validateTranslations('perustiedot.wwwSivuUrl', kieliversiot)
-      )
+      ),
+      validateHakijapalveluidenYhteystiedot('hakijapalveluidenYhteystiedot',
+          { message: 'validointivirheet.nimiJollainKielellaPakollinen', languages: kieliversiot})
     )(createErrorBuilder(values))
     .getErrors();
 };

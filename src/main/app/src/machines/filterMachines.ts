2bdfbc88f4bd56c57821cf78fadf2f91af5d6913
Petteri Tolonen <petteri.tolonen@knowit.fi>
2022-11-16 15:48:50 +0200

    Poistettu console.log koulutusMachine:n actionsista

--- src/main/app/src/machines/filterMachines.ts
+++ src/main/app/src/machines/filterMachines.ts
@@ -69,7 +69,6 @@ export const koulutusMachine = createMachine({
   on: {
     SET_VALUES: {
       actions: [
-        console.log('setting values'),
         assign({
           values: (context, event) => ({
             ...context.values,

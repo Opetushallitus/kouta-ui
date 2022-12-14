0bab481d9adaf5a3e93f603bc18e4dc1b5e58395
Petteri Tolonen <petteri.tolonen@knowit.fi>
2022-12-14 11:54:40 +0200

    Korvattu test-serverissä riippuvuus pem -> https-pem

--- src/main/app/test-server.js
+++ src/main/app/test-server.js
@@ -2,26 +2,14 @@ const https = require('https');
 const path = require('path');
 
 const express = require('express');
-const pem = require('pem');
+const pem = require('https-pem');
 
-// Certificate properties
-const certProps = {
-  days: 1, // Validity in days
-  selfSigned: true,
-};
+const app = express();
+app.use('/kouta', express.static(path.join(__dirname, 'build')));
 
-pem.createCertificate(certProps, (error, keys) => {
-  if (error) {
-    throw error;
-  }
-  const credentials = { key: keys.serviceKey, cert: keys.certificate };
-  const app = express();
-  app.use('/kouta', express.static(path.join(__dirname, 'build')));
-
-  app.get(['/kouta', '/kouta/*'], function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-  });
-
-  const httpsServer = https.createServer(credentials, app);
-  httpsServer.listen(3000);
+app.get(['/kouta', '/kouta/*'], function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
+const httpsServer = https.createServer(pem, app);
+httpsServer.listen(3000);

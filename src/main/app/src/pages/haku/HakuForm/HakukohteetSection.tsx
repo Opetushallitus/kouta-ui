a59900e69d5ab93fa6b9a987f6a380948e91554c
Tatu Honkanen <tatu.honkanen@cybercom.com>
2022-12-29 08:56:54 +0200

    OY-3920 : Checkboxit ja dropdown lisätty haun hakukohteille

--- src/main/app/src/pages/haku/HakuForm/HakukohteetSection.tsx
+++ src/main/app/src/pages/haku/HakuForm/HakukohteetSection.tsx
@@ -1,40 +1,61 @@
-import React, { useMemo } from 'react';
+import React, { useCallback, useMemo } from 'react';
 
 import { useInterpret } from '@xstate/react';
 import { useTranslation } from 'react-i18next';
 
-import {
-  makeModifiedColumn,
-  makeNimiColumn,
-  makeTilaColumn,
-  makeOrganisaatioColumn,
-} from '#/src/components/ListTable';
 import { ENTITY } from '#/src/constants';
 import { hakukohdeMachine } from '#/src/machines/filterMachines';
+import { createHakukohdeListColumns } from '#/src/pages/haku/HakuForm/createHakukohdeListColumns';
+import { EntityListActionBar } from '#/src/pages/HomePage/EntityListActionBar';
 import { EntitySearchList } from '#/src/pages/HomePage/EntitySearchList';
+import {
+  SERVICE_BY_ENTITY,
+  useEntitySelection,
+} from '#/src/pages/HomePage/useEntitySelection';
 import { useFilterState } from '#/src/pages/HomePage/useFilterState';
 import { searchFilteredHakukohteet } from '#/src/utils/hakukohde/searchHakukohteet';
-
-const useTableColumns = (t, organisaatioOid) =>
-  useMemo(
-    () => [
-      makeNimiColumn(t, {
-        getLinkUrl: ({ oid }) =>
-          `/organisaatio/${organisaatioOid}/hakukohde/${oid}/muokkaus`,
-      }),
-      makeOrganisaatioColumn(t),
-      makeTilaColumn(t),
-      makeModifiedColumn(t),
-    ],
-    [t, organisaatioOid]
-  );
+import isYhteishakuHakutapa from '#/src/utils/isYhteishakuHakutapa';
 
 const { HAKUKOHDE } = ENTITY;
 
+const HakukohdeActionBar = () => {
+  const { selection, removeSelection } = useEntitySelection(HAKUKOHDE);
+
+  const changeTila = useCallback(
+    tila => {
+      //TODO vaihdetaanko heti vai tallennuksen yhteydessä?
+      console.log('testi tila');
+      console.log(tila);
+      console.log('testi selection');
+      console.log(selection);
+    },
+    [selection]
+  );
+  return (
+    <EntityListActionBar
+      entityType={HAKUKOHDE}
+      selection={selection}
+      removeSelection={removeSelection}
+      changeTila={changeTila}
+      copyEntities={undefined}
+    />
+  );
+};
+
 export const HakukohteetSection = function ({ haku, organisaatioOid }) {
   const { t } = useTranslation();
+  const isYhteishaku = isYhteishakuHakutapa(haku?.hakutapaKoodiUri);
 
-  const columns = useTableColumns(t, organisaatioOid);
+  const columns = useMemo(
+    () =>
+      createHakukohdeListColumns(
+        t,
+        organisaatioOid
+      )(SERVICE_BY_ENTITY[HAKUKOHDE]),
+    [t, organisaatioOid]
+  );
+
+  const actionBar = isYhteishaku ? HakukohdeActionBar : undefined;
 
   let filterParams = { hakuOid: haku?.oid };
 
@@ -44,6 +65,7 @@ export const HakukohteetSection = function ({ haku, organisaatioOid }) {
 
   return (
     <EntitySearchList
+      ActionBar={actionBar}
       searchEntities={searchFilteredHakukohteet(filterParams)}
       organisaatioOid={organisaatioOid}
       entityType={HAKUKOHDE}

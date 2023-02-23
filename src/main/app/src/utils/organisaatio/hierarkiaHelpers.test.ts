8aa1d9fe168b0cb02ad7f832b6fcd63977f9f403
Risto Salama <risto.salama@gofore.com>
2023-02-23 10:04:17 +0200

    Add types to hierarkiahelper

--- src/main/app/src/utils/organisaatio/hierarkiaHelpers.test.ts
+++ src/main/app/src/utils/organisaatio/hierarkiaHelpers.test.ts
@@ -1,26 +1,29 @@
 import { flatFilterHierarkia, flattenHierarkia } from './hierarkiaHelpers';
 
-const childWithChildren = { oid: 4, children: [{ oid: 5 }, { oid: 6 }] };
+const childWithChildren = {
+  oid: '4',
+  children: [{ oid: '5' }, { oid: '6' }],
+} as Organisaatio;
 
 const hierarchy = {
-  oid: 1,
-  children: [{ oid: 2 }, { oid: 3 }, childWithChildren],
-};
+  oid: '1',
+  children: [{ oid: '2' }, { oid: '3' }, childWithChildren],
+} as Organisaatio;
 
 test('flattens organization hierarchy', () => {
   const expected = [
     hierarchy,
-    { oid: 2 },
-    { oid: 3 },
+    { oid: '2' },
+    { oid: '3' },
     childWithChildren,
-    { oid: 5 },
-    { oid: 6 },
+    { oid: '5' },
+    { oid: '6' },
   ];
   expect(flattenHierarkia(hierarchy)).toEqual(expected);
 });
 
 test('flattens organization hierarchy with filter', () => {
-  const filterFn = obj => obj.oid % 2 === 0;
-  const expected = [{ oid: 2 }, childWithChildren, { oid: 6 }];
+  const filterFn = obj => Number.parseInt(obj.oid) % 2 === 0;
+  const expected = [{ oid: '2' }, childWithChildren, { oid: '6' }];
   expect(flatFilterHierarkia([hierarchy], filterFn)).toEqual(expected);
 });
--- src/main/app/src/utils/organisaatio/hierarkiaHelpers.ts
+++ src/main/app/src/utils/organisaatio/hierarkiaHelpers.ts
@@ -1,11 +1,14 @@
-export const flatFilterHierarkia = (hierarkia, filterFn) =>
+export const flatFilterHierarkia = (
+  hierarkia: Array<Organisaatio>,
+  filterFn: (org: Organisaatio) => boolean
+) =>
   hierarkia
     .flatMap(org =>
       [org].concat(flatFilterHierarkia(org.children || [], filterFn))
     )
     .filter(filterFn);
 
-export const flattenHierarkia = org =>
+export const flattenHierarkia = (org: Organisaatio) =>
   org?.children
     ? [org].concat(org?.children.flatMap(child => flattenHierarkia(child)))
     : [org];

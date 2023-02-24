523cc4429b2a8393defbbb3ff392345675db5fb5
Risto Salama <risto.salama@gofore.com>
2023-02-24 09:27:26 +0200

    Refactor flatHierarchyFilter

--- src/main/app/src/utils/organisaatio/hierarkiaHelpers.ts
+++ src/main/app/src/utils/organisaatio/hierarkiaHelpers.ts
@@ -1,14 +1,9 @@
-export const flatFilterHierarkia = (
-  hierarkia: Array<Organisaatio>,
-  filterFn: (org: Organisaatio) => boolean
-) =>
-  hierarkia
-    .flatMap(org =>
-      [org].concat(flatFilterHierarkia(org.children || [], filterFn))
-    )
-    .filter(filterFn);
-
 export const flattenHierarkia = (org: Organisaatio) =>
   org?.children
     ? [org].concat(org?.children.flatMap(child => flattenHierarkia(child)))
     : [org];
+
+export const flatFilterHierarkia = (
+  hierarkia: Array<Organisaatio>,
+  filterFn: (org: Organisaatio) => boolean
+) => hierarkia.flatMap(flattenHierarkia).filter(filterFn);

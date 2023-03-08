d52100eee24d32782abc47fa8872029aace31ab4
Petteri Tolonen <petteri.tolonen@knowit.fi>
2023-03-08 11:14:55 +0200

    OPHJOD-112: Lisätty koulutus-lomakkeelle pelastusalan koulutusten kentät ja siistitty KoulutusForm-koodia

--- src/main/app/src/pages/koulutus/KoulutusForm/AmmatillinenTiedotSection/AmmatillinenTiedotSection.tsx
+++ src/main/app/src/pages/koulutus/KoulutusForm/AmmatillinenTiedotSection/AmmatillinenTiedotSection.tsx
@@ -1,32 +1,63 @@
 import React from 'react';
 
 import _ from 'lodash';
+import { useTranslation } from 'react-i18next';
+import { Field } from 'redux-form';
 
+import { FormFieldInput } from '#/src/components/formFields';
 import { OpintojenLaajuusFieldExtended } from '#/src/components/OpintojenLaajuusFieldExtended';
 import { VerticalBox } from '#/src/components/VerticalBox';
 import { Box } from '#/src/components/virkailija';
 import { PELASTUSALAN_AMMATILLISET_KOULUTUSKOODIURIT } from '#/src/constants';
 import { useLanguageTab } from '#/src/contexts/LanguageTabContext';
 import { useFieldValue } from '#/src/hooks/form';
+import { getTestIdProps } from '#/src/utils';
 import { koodiUriWithoutVersion } from '#/src/utils/koodi/koodiUriWithoutVersion';
 
+import KoulutusalatField from '../TiedotSection/KoulutusalatField';
+import TutkintonimikeField from '../TiedotSection/TutkintonimikeField';
 import { EPerusteTiedot } from './EPerusteTiedot';
 import { ValitseKoulutusBox } from './ValitseKoulutusBox';
 
+const KoulutusNimiField = ({ name, disabled }) => {
+  const language = useLanguageTab();
+  const { t } = useTranslation();
+  return (
+    <Box {...getTestIdProps('nimiInput')}>
+      <Field
+        disabled={disabled}
+        name={`${name}.nimi.${language}`}
+        component={FormFieldInput}
+        label={t('koulutuslomake.muokkaaKoulutuksenNimea')}
+        required
+      />
+    </Box>
+  );
+};
+
 // Pelastusalan koulutuksille halutaan itse syötettävät ePeruste-tyyppiset tiedot, koska
 // niillä ei ole vielä ePerusteita
-const SyntheticEPerusteTiedotSection = ({ disabled, name }) => {
+const SyntheticEPerusteTiedotSection = ({
+  name,
+  disabled,
+}: {
+  name: string;
+  disabled?: boolean;
+}) => {
   return (
     <VerticalBox gap={2}>
-      <OpintojenLaajuusFieldExtended disabled={disabled} name={name} required />
+      <OpintojenLaajuusFieldExtended name={name} disabled={disabled} required />
+      <TutkintonimikeField name={name} disabled={disabled} />
+      <KoulutusalatField name={name} disabled={disabled} />
+      <KoulutusNimiField name={name} disabled={disabled} />
     </VerticalBox>
   );
 };
 
 type AmmatillinenTiedotProps = {
+  name: string;
   disabled?: boolean;
   language: LanguageCode;
-  name?: string;
   koulutusLabel?: string;
 };
 
@@ -55,18 +86,22 @@ export const WithKoulutusSelect = ({
   );
 };
 
-export const AmmatillinenTiedotSection = ({
-  disabled,
-  language,
-  name,
-  koulutusLabel,
-}: AmmatillinenTiedotProps) => {
-  const koulutus = useFieldValue(`${name}.koulutus`)?.value;
+export const useIsPelastusalanKoulutus = () => {
+  const koulutus = useFieldValue('information.koulutus')?.value;
 
-  const isPelastusalanKoulutus = _.includes(
+  return _.includes(
     PELASTUSALAN_AMMATILLISET_KOULUTUSKOODIURIT,
     koodiUriWithoutVersion(koulutus)
   );
+};
+
+export const AmmatillinenTiedotSection = ({
+  name,
+  disabled,
+  language,
+  koulutusLabel,
+}: AmmatillinenTiedotProps) => {
+  const isPelastusalanKoulutus = useIsPelastusalanKoulutus();
 
   return (
     <WithKoulutusSelect name={name} koulutusLabel={koulutusLabel}>

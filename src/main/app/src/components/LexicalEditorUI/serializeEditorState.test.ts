4dff37248fd8138b3a10dd17d4b8959fc0517c67
Petteri Tolonen <petteri.tolonen@knowit.fi>
2023-04-20 17:15:17 +0300

    OY-4152: Toteutettu parseEditorState ja serializeEditorState editor-objektilla WIP
    - Käytetään discrete-optiota update-funktiolle, jotta saadaan toteutettua parsiminen ja serialisointi sykronisesti
    - Korvattu TextNode omalla, jossa ylikirjoitettu exportDOM-funktio, jotta voidaan kustomoida mitä elementtejä palautetaan
    - Lisäilty muutamia yksikkötestejä serializeEditorState:lle

---
+++ src/main/app/src/components/LexicalEditorUI/serializeEditorState.test.ts
@@ -0,0 +1,46 @@
+import { parseEditorState, serializeEditorState } from './utils';
+
+test('serializeEditorState simple paragraph', () => {
+  const es = parseEditorState('<p>test</p>');
+  expect(serializeEditorState(es)).toEqual('<p>test</p>');
+});
+
+test('serializeEditorState paragraph with line break', () => {
+  const es = parseEditorState('<p>test<br></p>');
+  expect(serializeEditorState(es)).toEqual('<p>test<br></p>');
+});
+
+test('serializeEditorState paragraph with em', () => {
+  const es = parseEditorState('<p><em>test</em></p>');
+  expect(serializeEditorState(es)).toEqual('<p><em>test</em></p>');
+});
+
+test('serializeEditorState <strong>', () => {
+  const es = parseEditorState('<strong>test</strong>');
+  expect(serializeEditorState(es)).toEqual('<p><strong>test</strong></p>');
+});
+
+test('serializeEditorState paragraph with <a>', () => {
+  const es = parseEditorState(
+    '<p><a href="http://example.com" target="_blank">example</a></p><p></p>'
+  );
+  expect(serializeEditorState(es)).toEqual(
+    '<p><a href="http://example.com" target="_blank">example</a></p><p><br></p>'
+  );
+});
+
+test('serializeEditorState headings with paragraphs', () => {
+  const es = parseEditorState(
+    '<h1>heading1</h1><p>paragraph1</p><h2>heading2</h2><p>paragraph2</p>'
+  );
+  console.log(JSON.stringify(es, null, 2));
+  expect(serializeEditorState(es)).toEqual(
+    '<h1>heading1</h1><p>paragraph1</p><h2>heading2</h2><p>paragraph2</p>'
+  );
+});
+
+test('serializeEditorState span', () => {
+  const es = parseEditorState('<strong>qwe</strong><span>asd</span>');
+  console.log(JSON.stringify(es, null, 2));
+  expect(serializeEditorState(es)).toEqual('<p><strong>qwe</strong>asd</p>');
+});
--- src/main/app/src/components/LexicalEditorUI/utils.tsx
+++ src/main/app/src/components/LexicalEditorUI/utils.tsx
@@ -1,32 +1,125 @@
-/* The editor handles HTML <-> state conversions internally, this is mainly for making sure
- * the HTML strings are nicely encapsulated so they can be recognized in the data. */
+import { $generateHtmlFromNodes, $generateNodesFromDOM } from '@lexical/html';
+import { AutoLinkNode, LinkNode } from '@lexical/link';
+import { ListItemNode, ListNode } from '@lexical/list';
+import { HeadingNode } from '@lexical/rich-text';
+import {
+  $getRoot,
+  createEditor,
+  CreateEditorArgs,
+  LexicalEditor,
+  TextNode,
+  DOMExportOutput,
+  $insertNodes,
+  EditorState,
+  $createParagraphNode,
+} from 'lexical';
+import _ from 'lodash';
 
-import { LexicalEditorHtml } from './LexicalEditorUI';
+function wrapElementWith(element: HTMLElement, tag: string): HTMLElement {
+  const el = document.createElement(tag);
+  el.appendChild(element);
+  return el;
+}
 
-export const parseEditorState = (value: string): LexicalEditorHtml => {
-  return { htmlStr: value };
-};
-
-export const serializeEditorState = (state: LexicalEditorHtml): string => {
-  return state.htmlStr;
-};
-
-export const isEditorState = (value: any): boolean => {
-  // eslint-disable-next-line lodash/prefer-lodash-typecheck
-  return value && typeof value === 'object' && value.hasOwnProperty('htmlStr');
-};
-
-export const isEmptyEditorState = (state: LexicalEditorHtml) => {
-  if (!isEditorState) return false;
-  if (state.htmlStr.trim().length === 0) return true;
-
-  const parser = new DOMParser();
-  const doc = parser.parseFromString(state.htmlStr, 'text/html');
-  const text = doc.body.textContent;
-
-  if (text?.trim().length === 0) {
-    return true;
+class CustomTextNode extends TextNode {
+  static clone(node: CustomTextNode): CustomTextNode {
+    return new CustomTextNode(node.__text);
   }
 
-  return false;
+  exportDOM(editor: LexicalEditor): DOMExportOutput {
+    let element = document.createElement('span');
+    element.textContent = this.__text;
+
+    // This is the only way to properly add support for most clients,
+    // even if it's semantically incorrect to have to resort to using
+    // <b>, <u>, <s>, <i> elements.
+    if (element !== null) {
+      if (this.hasFormat('bold')) {
+        element = wrapElementWith(element, 'strong');
+      }
+      if (this.hasFormat('italic')) {
+        element = wrapElementWith(element, 'em');
+      }
+      if (this.hasFormat('strikethrough')) {
+        element = wrapElementWith(element, 's');
+      }
+      if (this.hasFormat('underline')) {
+        element = wrapElementWith(element, 'u');
+      }
+    }
+
+    return {
+      element,
+    };
+  }
+}
+
+const NODES = [
+  HeadingNode,
+  ListNode,
+  ListItemNode,
+  AutoLinkNode,
+  LinkNode,
+  CustomTextNode,
+  {
+    replace: TextNode,
+    with: (node: TextNode) => {
+      return new CustomTextNode(node.__text);
+    },
+  },
+];
+
+const editorConfig: CreateEditorArgs = {
+  namespace: 'ImportExportEditor',
+  onError: error => {
+    console.error(error);
+  },
+  nodes: NODES,
+};
+
+export const parseEditorState = (value: string): EditorState => {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(value, 'text/html');
+  const editor = createEditor(editorConfig);
+  editor.update(
+    () => {
+      const p = $createParagraphNode();
+      $getRoot().append(p);
+      p.select();
+      $insertNodes($generateNodesFromDOM(editor, doc));
+    },
+    // Päivitetään synkronisesti!
+    { discrete: true }
+  );
+  return editor.getEditorState();
+};
+
+// TODO: Tarvii varmistua ettei rikota HTML:ää tässä!
+const unwrapSpans = html => html.replace(new RegExp('</?span.*?>', 'gm'), '');
+
+export const serializeEditorState = (value: EditorState): string => {
+  const editor = createEditor(editorConfig);
+  editor.setEditorState(value);
+
+  let html = '';
+
+  editor.update(
+    () => {
+      html = $generateHtmlFromNodes(editor, null);
+    },
+    // Päivitetään synkronisesti!
+    { discrete: true }
+  );
+
+  return unwrapSpans(html);
+};
+
+export const isEditorState = (value: unknown): value is EditorState => {
+  return _.isFunction(value?.isEmpty);
+  //return value && typeof value === 'object' && value.hasOwnProperty('htmlStr');
+};
+
+export const isEmptyEditorState = (state: EditorState) => {
+  if (!isEditorState) return false;
+  return state.isEmpty();
 };

04386f6ac364f87da3b7f1626135ed7ec06309ac
Heidi Lammi-Mihaljov <heidi.lammi-mihaljov@gofore.com>
2023-05-04 08:21:27 +0300

    OPHJOD-102: Korjataan tyypitys

--- src/main/app/src/utils/koulutus/isTohtorikoulutus.tsx
+++ src/main/app/src/utils/koulutus/isTohtorikoulutus.tsx
@@ -1,14 +1,17 @@
 import _ from 'lodash';
 
+import { koodiUriWithoutVersion } from '#/src/utils/koodi/koodiUriWithoutVersion';
+
 export const isTohtorikoulutus = (
   koulutusKoodiurit: Array<string> = [],
-  tohtorikoulutukset: Array<string> = []
+  tohtorikoulutukset: Array<Koodi> = []
 ) => {
   const tohtorikoulutuskoodiurit = tohtorikoulutukset.map(
     koulutus => koulutus.koodiUri
   );
-  const koulutuskoodiuritWithoutVersion = koulutusKoodiurit.flatMap(koodiuri =>
-    koodiuri.split('#')
+
+  const koulutuskoodiuritWithoutVersion = koulutusKoodiurit.map(koodiuri =>
+    koodiUriWithoutVersion(koodiuri)
   );
 
   return !_.isEmpty(

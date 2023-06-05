31679484707f410f0fd465c30a97b29acf6da16c
Santeri Korri <santeri.korri@knowit.fi>
2023-06-05 13:41:09 +0300

    OY-4247 Suodatetaan koodiston koulutuksista pois väliotsikot

--- src/main/app/src/utils/koodi/isValidKoulutusKoodi.ts
+++ src/main/app/src/utils/koodi/isValidKoulutusKoodi.ts
@@ -1,7 +1,12 @@
 import { isBefore, parseISO, endOfToday } from 'date-fns';
 import _ from 'lodash';
 
-export const isValidKoulutusKoodi = ({ koodisto, voimassaLoppuPvm }) =>
+export const isValidKoulutusKoodi = ({
+  koodiArvo,
+  koodisto,
+  voimassaLoppuPvm,
+}) =>
   koodisto?.koodistoUri === 'koulutus' &&
+  !koodiArvo.endsWith('00') && // suodatetaan pois väliotsikot
   (_.isNil(voimassaLoppuPvm) ||
     isBefore(endOfToday(), parseISO(voimassaLoppuPvm)));

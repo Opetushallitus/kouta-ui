20226466f7dd6f533a8e45c7e8d7d50245fce1f1
Heidi Lammi-Mihaljov <heidi.lammi-mihaljov@gofore.com>
2023-06-15 16:25:01 +0300

    OY-4344: Disabloidaan immutable- ja serializable-checkit

--- src/main/app/src/state/store.ts
+++ src/main/app/src/state/store.ts
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { reducer as formReducer } from 'redux-form';
-import { persistStore } from 'redux-persist';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import { isDev } from '#/src/utils';
 
@@ -11,7 +12,27 @@ export const store = () => {
     form: formReducer,
   });
 
-  const store = configureStore({ reducer: rootReducer, devTools: isDev });
+  const persistConfig = {
+    key: 'root',
+    storage,
+  };
+  const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+  const store = configureStore({
+    reducer: persistedReducer,
+    middleware: getDefaultMiddleware =>
+      getDefaultMiddleware({
+        // Browser slows down and becomes unresponsive because of state and immutable-state-invariant
+        // https://github.com/reduxjs/redux-toolkit/issues/415#issuecomment-596812204
+        immutableCheck: false,
+        // Non-serializable data should not be put in redux state:
+        // https://redux.js.org/faq/organizing-state#can-i-put-functions-promises-or-other-non-serializable-items-in-my-store-state
+        // However, we are using draft.js and its EditorState (Record type) is put in state which is why serializableCheck is disabled for now
+        // https://stackoverflow.com/a/63244831
+        serializableCheck: false,
+      }),
+    devTools: isDev,
+  });
 
   const persistor = persistStore(store);
 

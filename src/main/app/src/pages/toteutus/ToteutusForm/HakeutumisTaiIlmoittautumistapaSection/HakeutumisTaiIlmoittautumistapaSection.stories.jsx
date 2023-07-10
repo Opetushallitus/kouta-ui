cdae21bd0a06cfd9a29435810a495dfe0547064e
Marja Kari <marja.kari@amban.fi>
2023-07-10 22:33:40 +0300

    OY-3972 rebase ja käliviilaus

--- src/main/app/src/pages/toteutus/ToteutusForm/HakeutumisTaiIlmoittautumistapaSection/HakeutumisTaiIlmoittautumistapaSection.stories.jsx
+++ src/main/app/src/pages/toteutus/ToteutusForm/HakeutumisTaiIlmoittautumistapaSection/HakeutumisTaiIlmoittautumistapaSection.stories.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 import ReduxForm from '#/src/components/ReduxForm';
-import { ENTITY, FormMode } from '#/src/constants';
+import { ENTITY, FormMode, KOULUTUSTYYPPI } from '#/src/constants';
 
-import HakeutumisTaiIlmoittautumistapaSection from './HakeutumisTaiIlmoittautumistapaSection';
+import { HakeutumisTaiIlmoittautumistapaSection } from './HakeutumisTaiIlmoittautumistapaSection';
 
 export default {
   title: 'HakeutumisTaiIlmoittautumisTapaSection',
@@ -11,6 +11,9 @@ export default {
 
 export const Basic = () => (
   <ReduxForm form={ENTITY.TOTEUTUS} initialValues={{}} mode={FormMode.CREATE}>
-    <HakeutumisTaiIlmoittautumistapaSection language="fi" />
+    <HakeutumisTaiIlmoittautumistapaSection
+      language="fi"
+      koulutustyyppi={KOULUTUSTYYPPI.ERIKOISTUMISKOULUTUS}
+    />
   </ReduxForm>
 );

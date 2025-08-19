c964e33db9c5cbbea6dbc9ba3ac8ee623743895d
Topias Heinonen <topias.heinonen@gofore.com>
2025-08-19 09:31:19 +0300

    OY-5048 Playwright päivitetty
    Aiemmin Playwrightin sisäinen snapshottien polunlaskentafunktio
    ylikirjoitettiin. Playwright ei enää kutsu tätä funktiota hakiessaan
    snapshotia.  Sen sijaan asetetaan snapshotPathTemplate konfiguraatioon.
    Snapshotien tiedostonimet muuttuvat vähän tämän seurauksena.

--- src/main/app/playwright/playwright-helpers.ts
+++ src/main/app/playwright/playwright-helpers.ts
@@ -1,5 +1,3 @@
-import path from 'path';
-
 import {
   BrowserContext,
   Locator,
@@ -7,7 +5,7 @@ import {
   TestInfo,
   expect,
 } from '@playwright/test';
-import { deburr, includes, last, split, toLower } from 'lodash';
+import { includes, last, toLower } from 'lodash';
 
 import { Alkamiskausityyppi, ENTITY } from '#/src/constants';
 
@@ -26,12 +24,6 @@ export const wrapMutationTest =
   async (args: { page: Page; testInfo: TestInfo }, run: () => Promise<any>) => {
     const { page, testInfo } = args;
     const entityLower = toLower(entityName);
-
-    // Tallennetaan snapshot tiedostoon ./snapshots/<testitiedosto>/<testin-nimi>.json
-    testInfo.snapshotPath = (name: string) =>
-      `${path.resolve(testInfo.file, '../..')}/snapshots/${
-        split(testInfo.titlePath[0], '.')?.[0]
-      }/${deburr(toLower(name))}`;
 
     const requestPromise = page.waitForRequest(req => {
       const method = req.method();

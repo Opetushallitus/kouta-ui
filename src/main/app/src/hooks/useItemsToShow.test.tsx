9738702e4bff9f6006c83dfc8f86b569e46adb30
Heidi Lammi-Mihaljov <heidi.lammi-mihaljov@gofore.com>
2025-08-25 09:04:30 +0300

    OPHJOD-1486: Päivitetään testing-library-kirjastoja

--- src/main/app/src/hooks/useItemsToShow.test.tsx
+++ src/main/app/src/hooks/useItemsToShow.test.tsx
@@ -1,4 +1,4 @@
-import { renderHook } from '@testing-library/react-hooks';
+import { renderHook } from '@testing-library/react';
 
 import { ORGANISAATIOTYYPPI } from '#/src/constants';
 import { OrganisaatioModel } from '#/src/types/domainTypes';
--- src/main/app/src/setupTests.ts
+++ src/main/app/src/setupTests.ts
@@ -1,11 +1,28 @@
 import 'jest-styled-components';
 import '@testing-library/jest-dom';
-import { expect } from 'vitest';
+import { expect, vi } from 'vitest';
 
 import {
   isEditorState,
   serializeEditorState,
 } from '#/src/components/LexicalEditorUI/utils';
+
+// https://react.i18next.com/misc/testing
+vi.mock('react-i18next', () => ({
+  // this mock makes sure any components using the translate hook can use it without a warning being shown
+  useTranslation: () => {
+    return {
+      t: (i18nKey: string) => i18nKey,
+      i18n: {
+        changeLanguage: () => new Promise(() => {}),
+      },
+    };
+  },
+  initReactI18next: {
+    type: '3rdParty',
+    init: () => {},
+  },
+}));
 
 // Serialize Lexical editor
 expect.addSnapshotSerializer({

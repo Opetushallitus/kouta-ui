6b207da8435a1d9ef173d090d4a93383adf4ccf2
Petteri Tolonen <petteri.tolonen@knowit.fi>
2025-09-19 10:36:47 +0300

    Siistitty apufunktioita
    - Poistettu käyttämättömiä funktioita
    - Vähennettu lodash-käyttöä
    - Poistettu lodash/fp käyttöä (tai korvattu tavallisella lodashilla)

--- src/main/app/src/hooks/useCurrentUserHasRole.ts
+++ src/main/app/src/hooks/useCurrentUserHasRole.ts
@@ -1,6 +1,6 @@
 import { useCallback, useMemo } from 'react';
 
-import _fp from 'lodash/fp';
+import { upperFirst } from 'lodash';
 
 import { ENTITY_ROLES, CRUD_ROLES } from '#/src/constants';
 import useAuthorizedUserRoleBuilder from '#/src/hooks/useAuthorizedUserRoleBuilder';
@@ -11,7 +11,7 @@ export const useGetCurrentUserHasRole = (entity, role = CRUD_ROLES.READ) => {
 
   return useCallback(
     organisaatio =>
-      roleBuilder[`has${_fp.upperFirst(role)}`](
+      roleBuilder[`has${upperFirst(role)}`](
         ENTITY_ROLES[entity],
         organisaatio
       ).result(),

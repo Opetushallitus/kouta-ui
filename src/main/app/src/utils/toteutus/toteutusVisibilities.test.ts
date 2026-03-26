8230e11f0f3477eeec9369829322ecadc050d182
Heidi Lammi-Mihaljov <heidi.lammi-mihaljov@gofore.com>
2026-03-26 09:27:16 +0200

    OY-5501: Lisätään lukuvuosimaksu_amm_lk-vaihtoehto isApurahaVisible-testiin

--- src/main/app/src/utils/toteutus/toteutusVisibilities.test.ts
+++ src/main/app/src/utils/toteutus/toteutusVisibilities.test.ts
@@ -4,6 +4,7 @@ test.each([
   ['maksuton', false],
   ['maksullinen', false],
   ['lukuvuosimaksu', true],
+  ['lukuvuosimaksu_amm_lk', false],
 ])('isApurahaVisible', (maksullisuustyyppi, expected) =>
   expect(isApurahaVisible(maksullisuustyyppi)).toEqual(expected)
 );

144648cce26764aba591b9c9872ab1c74980c2e1
Heidi Lammi-Mihaljov <heidi.lammi-mihaljov@gofore.com>
2026-05-08 15:32:19 +0300

    OY-5501: Näytetään puuttuvien maksunMaarien virhetekstit lomakkeella

--- src/main/app/src/utils/toteutus/toteutusRemoteErrorsToFormErrors.ts
+++ src/main/app/src/utils/toteutus/toteutusRemoteErrorsToFormErrors.ts
@@ -265,6 +265,23 @@ export const toteutusRemoteErrorsToFormErrors: RemoteErrorsToFormErrors = (
     };
   }
 
+  if (path === 'metadata.opetus.maksunMaara' && errorType === 'missingMsg') {
+    return {
+      field: 'jarjestamistiedot.maksunMaara',
+      errorKey: `validointivirheet.pakollinen`,
+    };
+  }
+
+  if (
+    path === 'metadata.opetus.lukuvuosimaksunMaara' &&
+    errorType === 'missingMsg'
+  ) {
+    return {
+      field: 'jarjestamistiedot.lukuvuosimaksunMaara',
+      errorKey: `validointivirheet.pakollinen`,
+    };
+  }
+
   if (
     path === 'metadata.tila' &&
     errorType === 'invalidStateChangeForLiitetty'

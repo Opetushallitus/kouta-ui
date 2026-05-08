43de3e7befe23e6fa52c13ad3b8f4f4db484650a
Petteri Tolonen <petteri.tolonen@knowit.fi>
2026-05-08 16:38:41 +0300

    Siivottu pois turhia API-funktioita

--- src/main/app/src/utils/haku/getHakuByOid.ts
+++ src/main/app/src/utils/haku/getHakuByOid.ts
@@ -1,27 +1,6 @@
 import { ENTITY } from '#/src/constants';
-import { HttpClient } from '#/src/httpClient';
-import { HakuModel } from '#/src/types/domainTypes';
-import { ApiUrls } from '#/src/urls';
 
-import { getEntityByOid, useEntityByOid } from '../api/getEntityByOid';
-
-export const getHakuByOid = async ({
-  oid,
-  httpClient,
-  apiUrls,
-}: {
-  oid: string;
-  httpClient: HttpClient;
-  apiUrls: ApiUrls;
-}) =>
-  getEntityByOid<HakuModel>({
-    entityType: ENTITY.HAKU,
-    oid,
-    httpClient,
-    apiUrls,
-  });
+import { useEntityByOid } from '../api/getEntityByOid';
 
 export const useHakuByOid = (oid, options = {}) =>
-  useEntityByOid<HakuModel>(ENTITY.HAKU, { oid }, options);
-
-export default getHakuByOid;
+  useEntityByOid(ENTITY.HAKU, { oid }, options);
--- src/main/app/src/utils/toteutus/getToteutusByOid.ts
+++ src/main/app/src/utils/toteutus/getToteutusByOid.ts
@@ -1,26 +1,5 @@
 import { ENTITY } from '#/src/constants';
-import { HttpClient } from '#/src/httpClient';
-import { ToteutusModel } from '#/src/types/domainTypes';
-import { ApiUrls } from '#/src/urls';
-import { getEntityByOid, useEntityByOid } from '#/src/utils/api/getEntityByOid';
-
-const getToteutusByOid = async ({
-  oid,
-  apiUrls,
-  httpClient,
-}: {
-  oid: string;
-  apiUrls: ApiUrls;
-  httpClient: HttpClient;
-}) =>
-  getEntityByOid<ToteutusModel>({
-    entityType: ENTITY.TOTEUTUS,
-    oid,
-    apiUrls,
-    httpClient,
-  });
+import { useEntityByOid } from '#/src/utils/api/getEntityByOid';
 
 export const useToteutusByOid = (oid?: string | null, options = {}) =>
-  useEntityByOid<ToteutusModel>(ENTITY.TOTEUTUS, { oid }, options);
-
-export default getToteutusByOid;
+  useEntityByOid(ENTITY.TOTEUTUS, { oid }, options);

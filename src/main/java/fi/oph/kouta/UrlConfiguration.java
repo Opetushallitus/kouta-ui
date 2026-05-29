3bca6ea7d7ec0615365c3e534c681cfe7306de3b
Petteri Tolonen <petteri.tolonen@knowit.fi>
2026-05-29 09:29:35 +0300

    OY-3891: Lisätty eperuste-amosaa URL:t Java-wrapperiin

--- src/main/java/fi/oph/kouta/UrlConfiguration.java
+++ src/main/java/fi/oph/kouta/UrlConfiguration.java
@@ -81,6 +81,15 @@ public class UrlConfiguration extends OphProperties {
     this.frontProperties.setProperty(
         "kouta-backend.valintaperuste-list", this.require("kouta-backend.valintaperuste-list"));
     this.frontProperties.setProperty(
+        "kouta-backend.eperuste-amosaa-opetussuunnitelmat",
+        this.require("kouta-backend.eperuste-amosaa-opetussuunnitelmat"));
+    this.frontProperties.setProperty(
+        "kouta-backend.eperuste-amosaa-opetussuunnitelma",
+        this.require("kouta-backend.eperuste-amosaa-opetussuunnitelma"));
+    this.frontProperties.setProperty(
+        "kouta-backend.eperuste-amosaa-paikalliset-tutkinnonosat",
+        this.require("kouta-backend.eperuste-amosaa-paikalliset-tutkinnonosat"));
+    this.frontProperties.setProperty(
         "kouta-backend.ammattinimike-search", this.require("kouta-backend.ammattinimike-search"));
     this.frontProperties.setProperty(
         "kouta-backend.asiasana-search", this.require("kouta-backend.asiasana-search"));
@@ -156,6 +165,9 @@ public class UrlConfiguration extends OphProperties {
     this.frontProperties.setProperty("eperusteet.sisalto", this.require("eperusteet.sisalto"));
     this.frontProperties.setProperty(
         "eperusteet.tutkinnonosat", this.require("eperusteet.tutkinnonosat"));
+    this.frontProperties.setProperty(
+        "eperusteet.paikallinen-tutkinnonosa",
+        this.require("eperusteet.paikallinen-tutkinnonosa"));
     this.frontProperties.setProperty(
         "eperusteet.osaamismerkit", this.require("eperusteet.osaamismerkit"));
 
